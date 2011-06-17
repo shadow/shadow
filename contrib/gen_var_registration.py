7e513c51cef6aeaa9123e29a80c1a3be4576d341
Rob Jansen <jansen@cs.umn.edu>
2011-06-17 18:21:39 -0400

    Added license to source files.
    Used GPLv3 similar to in Shadow. Included preamble in C and Python
    source files. Closes #1.

--- contrib/gen_var_registration.py
+++ contrib/gen_var_registration.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python
+
+##
+# Scallion - plug-in for The Shadow Simulator
+#
+# Copyright (c) 2010-2011 Rob Jansen <jansen@cs.umn.edu>
+#
+# This file is part of Scallion.
+#
+# Scallion is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Scallion is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Scallion.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 """
 This script takes the var_definitions.c file that is generated when compiling Tor
 with the --disable-static-vars flag, and parses the file for static variables.
--- src/tor_includes.h
+++ src/tor_includes.h
@@ -1,3 +1,24 @@
+/**
+ * Scallion - plug-in for The Shadow Simulator
+ *
+ * Copyright (c) 2010-2011 Rob Jansen <jansen@cs.umn.edu>
+ *
+ * This file is part of Scallion.
+ *
+ * Scallion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scallion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scallion.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE 1
 #endif
