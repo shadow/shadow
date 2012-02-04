ec863fa66de2abd9244f7494276354e7b1f367f4
Rob G. Jansen <jansen@cs.umn.edu>
2012-02-04 12:50:17 -0500

    Working 5-node scallion test in resource, updated readme

--- contrib/installdeps.sh
+++ contrib/installdeps.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+## This script downloads OpenSSL and Libevent2, build them, and installs them
+## to ~/.shadow. This must be done to use the Scallion plugin, since we need
+## to pass special flags so that linking to Tor and running in Shadow both
+## work properly.
+
 D=`pwd`
 mkdir -p build
 cd build
@@ -7,8 +12,15 @@ cd build
 wget http://www.openssl.org/source/openssl-1.0.0e.tar.gz
 tar xvzf openssl-1.0.0e.tar.gz
 cd openssl-1.0.0e/
+
+## use ONE of the following:
+
+## for debugging and profiling
 #./config --prefix=${HOME}/.shadow -fPIC -g -pg -DPURIFY -Bsymbolic shared
+
+## for normal use
 ./config --prefix=${HOME}/.shadow -fPIC shared
+
 make
 make install
 
@@ -17,9 +29,15 @@ cd ../
 wget https://github.com/downloads/libevent/libevent/libevent-2.0.16-stable.tar.gz
 tar xvzf libevent-2.0.16-stable.tar.gz
 cd libevent-2.0.16-stable/
-## CPPFLAGS="-DUSE_DEBUG"
-#./configure --prefix=${HOME}/.shadow CFLAGS="-fPIC -I${HOME}/.shadow -g -pg" LDFLAGS="-L${HOME}/.shadow"
+
+## use ONE of the following (you probably want :
+
+## for debugging and profiling
+#./configure --prefix=${HOME}/.shadow CFLAGS="-fPIC -I${HOME}/.shadow -g -pg" LDFLAGS="-L${HOME}/.shadow" CPPFLAGS="-DUSE_DEBUG"
+
+## for normal use
 ./configure --prefix=${HOME}/.shadow CFLAGS="-fPIC -I${HOME}/.shadow" LDFLAGS="-L${HOME}/.shadow"
+
 make
 make install
 
