3537a3a51e37bbcdb360da36b68f31375bb26206
Rob G. Jansen <jansen@cs.umn.edu>
2012-02-18 00:40:52 -0500

    new bandwidth and link speed estimations using server descriptors and extra infos

--- contrib/generate.py
+++ contrib/generate.py
@@ -44,36 +44,117 @@ DOCHURN=False
 class Relay():
     def __init__(self, ip, bw, isExit=False):
         self.ip = ip
-        self.bw = int(bw) # in bytes
+        self.bwconsensus = int(bw) # in bytes, from consensus
         self.isExit = isExit
         self.code = None
-        self.bwrate = None # in bytes
-        self.bwburst = None # in bytes
-        self.bwhistory = None # in bytes
         
-        self.ispbandwidth = 0 # in KiB
-        kbw = bw / 1024
-        # select correct bandwidth tier
-        if kbw <= 512: self.ispbandwidth = 512
-        elif kbw <= 1024: self.ispbandwidth = 1024 # 1 MiB
-        elif kbw <= 10240: self.ispbandwidth = 10240 # 10 MiB
-        elif kbw <= 25600: self.ispbandwidth = 25600 # 25 MiB
-        elif kbw <= 51200: self.ispbandwidth = 51200 # 50 MiB
-        elif kbw <= 76800: self.ispbandwidth = 76800 # 75 MiB
-        elif kbw <= 102400: self.ispbandwidth = 102400 # 100 MiB
-        elif kbw <= 153600: self.ispbandwidth = 153600 # 150 MiB
-        else: self.ispbandwidth = 204800
+        self.bwrate = 0 # in bytes
+        self.bwburst = 0 # in bytes
+        self.bwtstamp = 0
         
-    def setTokenBucketBW(self, bwrate, bwburst, bwhistory):
-        self.bwrate = int(bwrate)
-        self.bwburst = int(bwburst)
-        self.bwhistory = int(bwhistory)
+        self.maxobserved = 0 # in bytes
+        self.maxread = 0 # in bytes
+        self.maxwrite = 0 # in bytes
+        
+        self.upload = 0 # in KiB
+        self.download = 0 # in KiB
         
     def setRegionCode(self, code):
         self.code = code
+        
+    def setLimits(self, bwrate, bwburst, bwtstamp):
+        # defaults are 5MiB rate (5120000), 10MiB Burst (10240000)
+        # minimums are 20KiB rate (20480)
+        if bwtstamp > self.bwtstamp:
+            self.bwtstamp = bwtstamp
+            self.bwrate = int(bwrate)
+            if self.bwrate < 20480: self.bwrate = 20480 # the min required Tor config
+            self.bwburst = int(bwburst)
+        
+    # max observed from server descriptor (min of max-read and max-write over 10 second intervals)
+    def setMaxObserved(self, observed):
+        self.maxobserved = max(self.maxobserved, observed)
+        
+    # max read and write over 15 minute intervals from extra-infos
+    def setMaxSpeeds(self, read, write):
+        self.maxread = max(self.maxread, read)
+        self.maxwrite = max(self.maxwrite, write)
+        
+    def computeSpeeds(self):
+        '''
+        compute the link speeds to the ISP
+        
+        we prefer relay measurements over the consensus, because the measurement
+        is generally more accurate and unlikely malicious
+        
+        we can estimate the link speed as the maximum bandwidth we've ever seen.
+        this is usually the observed bandwidth from the server descriptor since
+        its computed over 10 second intervals rather than the read/write histories
+        from the extra-info which are averaged over 15 minutes.
+        
+        since the observed bandwidth reported in the server descriptor is the minimum
+        of the 10 second interval reads and writes, we use the extra-infos
+        to determine which of read or write this observed bandwidth likely
+        corresponds to. then we compute the missing observed value (the max of the
+        10 second interval reads and writes) using the ratio of read/write from the
+        extra-info.
+        
+        in the absence of historical data we fall back to the consensus bandwidth 
+        and hope that TorFlow accurately measured in this case
+        '''
+        if self.maxobserved > 0:
+            if self.maxread > 0 and self.maxwrite > 0:
+                # yay, best case as we have all the data
+                readToWriteRatio = float(self.maxread)/float(self.maxwrite)
+                bw = int(self.maxobserved / 1024.0)
+                if readToWriteRatio > 1.0:
+                    # write is the min and therefore the 'observed' value
+                    self.upload = bw # the observed min
+                    self.download = int(bw*readToWriteRatio) # the scaled up max
+                else:
+                    # read is the min and therefore the 'observed' value
+                    self.download = bw # the observed min
+                    self.upload = int(bw*(1.0/readToWriteRatio)) # the scaled up max
+            else:
+                # ok, at least use our observed
+                bw = int(self.maxobserved / 1024.0)
+                self.download = bw
+                self.upload = bw                
+        elif self.maxread > 0 and self.maxwrite > 0:
+            # use read/write directly
+            self.download = int(self.maxread / 1024.0)
+            self.upload = int(self.maxwrite / 1024.0)               
+        else:
+            # pity...
+            bw = int(self.bwconsensus / 1024.0)
+            self.download = bw
+            self.upload = bw
+            
+        # the 'tiered' approach, not currently used
+        '''
+        if self.ispbandwidth <= 512: self.ispbandwidth = 512
+        elif self.ispbandwidth <= 1024: self.ispbandwidth = 1024 # 1 MiB
+        elif self.ispbandwidth <= 10240: self.ispbandwidth = 10240 # 10 MiB
+        elif self.ispbandwidth <= 25600: self.ispbandwidth = 25600 # 25 MiB
+        elif self.ispbandwidth <= 51200: self.ispbandwidth = 51200 # 50 MiB
+        elif self.ispbandwidth <= 76800: self.ispbandwidth = 76800 # 75 MiB
+        elif self.ispbandwidth <= 102400: self.ispbandwidth = 102400 # 100 MiB
+        elif self.ispbandwidth <= 153600: self.ispbandwidth = 153600 # 150 MiB
+        else: self.ispbandwidth = 204800
+        '''
+        
+    CSVHEADER = "IP,CCode,IsExit,Consensus(KiB/s),Rate(KiB/s),Burst(KiB/s),MaxObserved(KiB/s),MaxRead(KiB/s),MaxWrite(KiB/s),LinkDown(KiB/s),LinkUp(KiB/s)"
 
     def toCSV(self):
-        return ",".join([self.ip, self.code, str(self.isExit), str(self.bw), str(self.bwrate), str(self.bwburst), str(self.bwhistory)])
+        c = str(int(self.bwconsensus/1024.0))
+        r = str(int(self.bwrate/1024.0))
+        b = str(int(self.bwburst/1024.0))
+        mo = str(int(self.maxobserved/1024.0))
+        mr = str(int(self.maxread/1024.0))
+        mw = str(int(self.maxwrite/1024.0))
+        ldown = str(int(self.download))
+        lup = str(int(self.upload))
+        return ",".join([self.ip, self.code, str(self.isExit), c, r, b, mo, mr, mw, ldown, lup])
 
 class GeoIPEntry():
     def __init__(self, lownum, highnum, countrycode):
@@ -97,8 +178,9 @@ def main():
     ap.add_argument('alexa', action="store", type=str, help="path to an ALEXA file (produced with contrib/parsealexa.py)", metavar='ALEXA', default=None)
     ap.add_argument('consensus', action="store", type=str, help="path to a current Tor CONSENSUS file", metavar='CONSENSUS', default=None)
     ap.add_argument('descriptors', action="store", type=str, help="path to top-level directory containing current Tor server-descriptors", metavar='DESCRIPTORS', default=None)
+    ap.add_argument('extrainfos', action="store", type=str, help="path to top-level directory containing current Tor extra-infos", metavar='EXTRAINFOS', default=None)
     ap.add_argument('connectingusers', action="store", type=str, help="path to csv containing Tor directly connecting user country data", metavar='CONNECTINGUSERS', default=None)
-    
+     
     # get arguments, accessible with args.value
     args = ap.parse_args()
     
@@ -107,13 +189,14 @@ def main():
     args.consensus = os.path.abspath(os.path.expanduser(args.consensus))
     args.alexa = os.path.abspath(os.path.expanduser(args.alexa))
     args.descriptors = os.path.abspath(os.path.expanduser(args.descriptors))
+    args.extrainfos = os.path.abspath(os.path.expanduser(args.extrainfos))
     args.connectingusers = os.path.abspath(os.path.expanduser(args.connectingusers))
     
     # we'll need to convert IPs to cluster codes
     args.geoippath = os.path.abspath(args.prefix+"/share/geoip")
     
     generate(args)
-    log("finished generating:\n{0}/hosts.xml\n{0}/web.dl\n{0}/bulk.dl\n{0}/think.dat".format(os.getcwd()))
+    log("finished generating:\n{0}/relays.csv\n{0}/hosts.xml\n{0}/web.dl\n{0}/bulk.dl\n{0}/think.dat".format(os.getcwd()))
 
 def generate(args):
     # get list of relays, sorted by increasing bandwidth
@@ -129,12 +212,20 @@ def generate(args):
 
     # sample for the exits and nonexits we'll use for our nodes
     nexits = int(args.exitfrac * args.nrelays)
-    exitnodes = getRelays(exits, nexits, geoentries, args.descriptors)
+    exitnodes = getRelays(exits, nexits, geoentries, args.descriptors, args.extrainfos)
     nnonexits = args.nrelays - nexits
-    nonexitnodes = getRelays(nonexits, nnonexits, geoentries, args.descriptors)
+    nonexitnodes = getRelays(nonexits, nnonexits, geoentries, args.descriptors, args.extrainfos)
     
     servers = getServers(args.alexa)
     clientCountryCodes = getClientCountryChoices(args.connectingusers)
+    
+    # output choices
+    with open("relays.csv", "wb") as f:
+        print >>f, Relay.CSVHEADER
+        for r in exitnodes:
+            print >>f, r.toCSV()
+        for r in nonexitnodes:
+            print >>f, r.toCSV()
     
     # build the XML
     root = etree.Element("hosts")
@@ -194,8 +285,8 @@ def generate(args):
     name = "4uthority"
     soft = "{0}soft".format(name)
     starttime = "2"
-    softargs = "dirauth {0} {1} {2} ./authority.torrc ./data/authoritydata {3}share/geoip".format(authority.bw, authority.bwrate, authority.bwburst, INSTALLPREFIX) # in bytes
-    addRelayToXML(root, soft, starttime, softargs, name, authority.ispbandwidth, authority.ip, authority.code)
+    softargs = "dirauth {0} {1} {2} ./authority.torrc ./data/authoritydata {3}share/geoip".format(authority.bwconsensus, authority.bwrate, authority.bwburst, INSTALLPREFIX) # in bytes
+    addRelayToXML(root, soft, starttime, softargs, name, authority.download, authority.upload, authority.ip, authority.code)
     
     # node boot-up rates
     relaysPerSecond = 5
@@ -210,9 +301,9 @@ def generate(args):
         name = "exit{0}".format(i)
         soft = "{0}soft".format(name)
         starttime = "{0}".format(timecounter)
-        softargs = "exitrelay {0} {1} {2} ./exit.torrc ./data/exitdata {3}share/geoip".format(exit.bw, exit.bwrate, exit.bwburst, INSTALLPREFIX) # in bytes
+        softargs = "exitrelay {0} {1} {2} ./exit.torrc ./data/exitdata {3}share/geoip".format(exit.bwconsensus, exit.bwrate, exit.bwburst, INSTALLPREFIX) # in bytes
         
-        addRelayToXML(root, soft, starttime, softargs, name, exit.ispbandwidth, exit.ip, exit.code)
+        addRelayToXML(root, soft, starttime, softargs, name, exit.download, exit.upload, exit.ip, exit.code)
         
         if i % relaysPerSecond == 0: timecounter += 1 # x nodes every second
         i += 1
@@ -227,9 +318,9 @@ def generate(args):
         name = "nonexit{0}".format(i)
         soft = "{0}soft".format(name)
         starttime = "{0}".format(timecounter)
-        softargs = "relay {0} {1} {2} ./relay.torrc ./data/relaydata {3}share/geoip".format(relay.bw, relay.bwrate, relay.bwburst, INSTALLPREFIX) # in bytes
+        softargs = "relay {0} {1} {2} ./relay.torrc ./data/relaydata {3}share/geoip".format(relay.bwconsensus, relay.bwrate, relay.bwburst, INSTALLPREFIX) # in bytes
         
-        addRelayToXML(root, soft, starttime, softargs, name, relay.ispbandwidth, relay.ip, relay.code)
+        addRelayToXML(root, soft, starttime, softargs, name, relay.download, relay.upload, relay.ip, relay.code)
     
         if i % relaysPerSecond == 0: timecounter += 1 # x nodes every second
         i += 1
@@ -309,7 +400,7 @@ def generate(args):
         # all our hosts
         print >>fhosts, (etree.tostring(root, pretty_print=True, xml_declaration=False))
 
-def addRelayToXML(root, soft, starttime, softargs, name, ispbandwidth=0, ip=None, code=None): # bandwidth in KiB
+def addRelayToXML(root, soft, starttime, softargs, name, download=0, upload=0, ip=None, code=None): # bandwidth in KiB
     # software
     e = etree.SubElement(root, "software")
     e.set("id", soft)
@@ -323,9 +414,11 @@ def addRelayToXML(root, soft, starttime, softargs, name, ispbandwidth=0, ip=None
     if ip is not None: e.set("ip", ip)
     if code is not None: e.set("cluster", code)
     e.set("software", soft)
-    if ispbandwidth > 0: # bandwidth is optional in XML, will be assigned based on cluster if not given
-        e.set("bandwidthup", "{0}".format(ispbandwidth)) # in KiB
-        e.set("bandwidthdown", "{0}".format(ispbandwidth)) # in KiB
+    
+    # bandwidth is optional in XML, will be assigned based on cluster if not given
+    if download > 0: e.set("bandwidthdown", "{0}".format(download)) # in KiB
+    if upload > 0: e.set("bandwidthup", "{0}".format(upload)) # in KiB
+    
     e.set("quantity", "1")
     e.set("cpufrequency", choice(CPUFREQS))
 
@@ -409,16 +502,14 @@ def getServers(alexapath):
             ips.append(ip)
     return ips
 
-def getRelays(relays, k, geoentries, descriptorpath):
+def getRelays(relays, k, geoentries, descriptorpath, extrainfopath):
     sample = sample_relays(relays, k)
     
-    # get a map for easy lookup while parsing descriptors
-    ipmap = dict()
-    timemap = dict()
+    # maps for easy relay lookup while parsing descriptors
+    ipmap, fpmap = dict(), dict()
     for s in sample:
         if s.ip not in ipmap: 
             ipmap[s.ip] = s
-            timemap[s.ip] = 0.0
         
     # go through all the descriptors and find the bandwidth rate, burst, and
     # history from the most recent descriptor of each relay in our sample
@@ -426,8 +517,9 @@ def getRelays(relays, k, geoentries, descriptorpath):
         for filename in files: 
             fullpath = os.path.join(root, filename)
             with open(fullpath, 'rb') as f:
-                rate, burst, hist = 0, 0, 0
+                rate, burst, observed = 0, 0, 0
                 ip = ""
+                fingerprint = None
                 published = None
                 
                 for line in f:
@@ -439,27 +531,70 @@ def getRelays(relays, k, geoentries, descriptorpath):
                     elif parts[0] == "published":
                         published = "{0} {1}".format(parts[1], parts[2])
                     elif parts[0] == "bandwidth":
-                        rate, burst, hist = int(parts[1]), int(parts[2]), int(parts[3])
-                        
-                if published is not None and ip in ipmap:
+                        rate, burst, observed = int(parts[1]), int(parts[2]), int(parts[3])
+                    elif parts[0] == "opt" and parts[1] == "fingerprint":
+                        fingerprint = "".join(parts[2:])
+                
+                if ip not in ipmap: continue
+                
+                relay = ipmap[ip]
+                # we want to know about every fingerprint
+                if fingerprint is not None: fpmap[fingerprint] = relay
+                # we want to know every observed bandwidth to est. link speed
+                relay.setMaxObserved(observed)
+                # we only want the latest rate and burst settings 
+                if published is not None:
                     datet = datetime.strptime(published, "%Y-%m-%d %H:%M:%S")
                     unixt = time.mktime(datet.timetuple())
-                    if unixt >= timemap[ip]:
-                        timemap[ip] = unixt
-                        relay = ipmap[ip]
-                        relay.setTokenBucketBW(rate, burst, hist)
-    
+                    relay.setLimits(rate, burst, unixt)
+                        
+    # now check for extra info docs for our chosen relays, so we get read and write histories
+    # here the published time doesnt matter b/c we are trying to estimate the
+    # relay's ISP link speed
+    for root, dirs, files in os.walk(extrainfopath):
+        for filename in files: 
+            fullpath = os.path.join(root, filename)
+            with open(fullpath, 'rb') as f:
+                maxwrite, maxread = 0, 0
+                fingerprint = None
+                
+                for line in f:
+                    parts = line.strip().split()
+                    if len(parts) == 0: continue
+                    if parts[0] == "extra-info":
+                        if len(parts) < 3: break # cant continue if we dont know the relay
+                        fingerprint = parts[2]
+                        if fingerprint not in fpmap: break
+                    elif parts[0] == "write-history":
+                        if len(parts) < 6: continue # see if we can get other info from this doc
+                        seconds = float(int(parts[3][1:]))
+                        speeds = parts[5]
+                        maxwrite = int(max([int(i) for i in speeds.split(',')]) / seconds)
+                    elif parts[0] == "read-history":
+                        if len(parts) < 6: continue # see if we can get other info from this doc
+                        seconds = float(int(parts[3][1:]))
+                        speeds = parts[5]
+                        maxread = int(max([int(i) for i in speeds.split(',')]) / seconds)
+                        
+                if fingerprint is not None and fingerprint in fpmap:
+                    relay = fpmap[fingerprint]
+                    relay.setMaxSpeeds(maxread, maxwrite)
+                        
     # make sure we found some info for all of them, otherwise use defaults
     for s in sample:
-        if s.bwrate <= 0 or s.bwburst <= 0: s.setTokenBucketBW(5120000, 10240000, 0) # 5MB rate, 10MB Burst
-        if s.bwrate < 20480: s.bwrate = 20480 # the min required setting
         s.setRegionCode(getClusterCode(geoentries, s.ip))
+        s.computeSpeeds()
     
     return sample
     
-# relays should be sorted by bandwidth
 def sample_relays(relays, k):
-    # sample k of n relays to best fit the original list
+    """
+    sample k of n relays
+    split list into k buckts, take the median from each bucket
+    this provides the statistically best fit to the original list
+    
+    relays list should be sorted by bandwidth
+    """
     n = len(relays)
     assert k < n
     
@@ -499,7 +634,7 @@ def parse_consensus(consensus_path):
             elif line[0:2] == "w ":
                 bw = float(line.strip().split()[1].split("=")[1]) * 1024.0 # KiB to bytes
     
-    return sorted(relays, key=lambda relay: relay.bw)
+    return sorted(relays, key=lambda relay: relay.bwconsensus)
 
 def log(msg):
     color_start_code = "\033[94m" # red: \033[91m"
