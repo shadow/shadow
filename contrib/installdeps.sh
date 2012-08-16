9a5e4056f68a6b46ee56e936cd45d3c37ba33634
Arne <caffeine@parttimegeeks.net>
2012-08-16 17:40:29 +0200

    Import keys from keyserver (If the user wants to) in installdeps.sh

--- contrib/installdeps.sh
+++ contrib/installdeps.sh
@@ -5,25 +5,39 @@
 ## to pass special flags so that linking to Tor and running in Shadow both
 ## work properly.
 
+check_signature() {
+  local pkgname=`echo $1 | cut -d '-' -f1`
+  local keyring=./signing_keys
+  read -r -p "do you want me to import the public key to check the signature of $pkgname? [Y/n] " response
+
+  if [[ $response =~ ^([nn][oo]|[nn])$ ]]; then
+    gpg --verify $1.asc
+  else
+    gpg --no-default-keyring --keyring $keyring --keyserver pgp.mit.edu --recv $2
+    gpg --keyring $keyring --verify $1.asc
+  fi
+
+  if [ $? -eq 0 ]
+  then
+    echo Signature is well.
+  else
+    echo "Problem with $pkgname signature. Edit the installdeps.sh script if you want to avoid checking the signature."
+    exit -1
+  fi
+}
+
+
 PREFIX=${PREFIX-${HOME}/.shadow}
 echo "Installing to $PREFIX"
 
 D=`pwd`
-KEYRING=$( cd "$( dirname "$0" )" && pwd )/deps_keyring.gpg
 mkdir -p build
 cd build
 
 wget https://www.openssl.org/source/openssl-1.0.1c.tar.gz
 wget https://www.openssl.org/source/openssl-1.0.1c.tar.gz.asc
-gpg --keyring $KEYRING --verify openssl-1.0.1c.tar.gz.asc
 
-if [ $? -eq 0 ]
-then
-    echo Signature is well.
-else
-    echo "Problem with openssl signature. Edit the installdeps.sh script if you want to avoid checking the signature."
-    exit -1
-fi
+check_signature "openssl-1.0.1c.tar.gz" "F295C759"
 
 tar xaf openssl-1.0.1c.tar.gz
 cd openssl-1.0.1c/
@@ -44,15 +58,7 @@ cd ../
 wget https://github.com/downloads/libevent/libevent/libevent-2.0.19-stable.tar.gz
 wget https://github.com/downloads/libevent/libevent/libevent-2.0.19-stable.tar.gz.asc
 
-gpg --keyring $KEYRING --verify libevent-2.0.19-stable.tar.gz.asc
-
-if [ $? -eq 0 ]
-then
-    echo Signature is well.
-else
-    echo "Problem with libevent signature. Edit the installdeps.sh script if you want to avoid checking the signature."
-    exit -1
-fi
+check_signature "libevent-2.0.19-stable.tar.gz" "8D29319A"
 
 tar xaf libevent-2.0.19-stable.tar.gz
 cd libevent-2.0.19-stable/
