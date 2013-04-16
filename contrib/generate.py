c56d9b4489710b002772bbb18db632d8e15fede7
Rob Jansen <jansen@cs.umn.edu>
2013-04-16 18:42:07 -0400

    Remove old copyright notices

--- contrib/generate.py
+++ contrib/generate.py
@@ -1,26 +1,5 @@
 #!/usr/bin/env python2.7
 
-##
-# Scallion - Tor plug-in for The Shadow Simulator
-#
-# Copyright (c) 2010-2012 Rob Jansen <jansen@cs.umn.edu>
-#
-# This file is part of Scallion.
-#
-# Scallion is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Scallion is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Scallion.  If not, see <http://www.gnu.org/licenses/>.
-#
-
 import os, sys, subprocess, argparse, socket, time, math
 from random import choice
 from datetime import datetime
