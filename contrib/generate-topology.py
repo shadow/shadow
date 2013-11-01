4cb799a45d49ea312eb03a84ca94a37f06aadf99
Rob Jansen <jansen@cs.umn.edu>
2013-11-01 10:23:36 -0400

    more topology script updates

--- contrib/generate-topology.py
+++ contrib/generate-topology.py
@@ -6,13 +6,12 @@ import networkx as nx
 BW_FILENAME="aggregate_mean_netspeeds.csv"
 PLOSS_FILENAME="aggregate_mean_netquality.csv"
 MAP_FILENAME="full_tor_map.xml"
+GEO_FILENAME="geoip"
 OUTPUT_FILENAME="topology.graphml.xml"
 
 def main():
-    # KiB/s
-    bwup, bwdown = get_bandwidth(BW_FILENAME)
-    # fraction between 0 and 1
-    loss = get_packet_loss(PLOSS_FILENAME)
+    bwup, bwdown = get_bandwidth() # KiB/s
+    loss = get_packet_loss() # fraction between 0 and 1
     meanloss = numpy.mean(loss.values())
     keys = sorted(bwup.keys())
     for k in keys:
@@ -22,66 +21,73 @@ def main():
     bdownstr = ','.join(["{0}={1}".format(k, bwdown[k]) for k in keys])
     plossstr = ','.join(["{0}={1}".format(k, loss[k]) for k in keys])
 
-    geo = getGeoEntries()
+    geo = get_geo()
 
     # now get the graph and mod it as needed for shadow
     Gin = nx.read_graphml(MAP_FILENAME)
 
-    G = nx.DiGraph()
+    G = nx.Graph()
     G.graph['bandwidthup'] = bupstr
     G.graph['bandwidthdown'] = bdownstr
     G.graph['packetloss'] = plossstr
     # hack until we use a version of igraph that supports graph attributes
     G.add_node("dummynode", bandwidthup=bupstr, bandwidthdown=bdownstr, packetloss=plossstr)
 
+    empty = set()
+
     for id in Gin.nodes():
         n = Gin.node[id]
-        if len(n) == 0 or 'nodetype' not in n: continue
 
-        nodetypestr = get_node_type_string(n)
-        geocodesstr = 'US'
-        if 'countries' in n: geocodesstr = n['countries']
-        elif 'country' in n: geocodesstr = n['country']
-        elif 'relay' in nodetypestr: geocodesstr = getClusterCode(geo, n['relay_ip'])
-        asnstr = n['asn'].split()[0][2:] if 'asn' in n else '0'
-        idstr = get_id_string(n)
+        if 'nodetype' not in n:
+            empty.add(id)
+            continue
 
-#        if 'relay' in nodetypestr:
-#            G.add_node(idstr, nodetype=nodetypestr, geocodes=geocodesstr, asn=asnstr, relay_ip=n['relay_ip'], relay_fingerprint=n['fp'], relay_nickname=n['nick'])
-#        else:
-        G.add_node(idstr, nodetype=nodetypestr, geocodes=geocodesstr, asn=asnstr)
+        elif 'pop' in n['nodetype']:
+            popid = str(n['nodeid'])
+            gc = n['countries'] if 'countries' in n else 'US'
+            #asn = n['asn'].split()[0][2:]
+            G.add_node(popid, nodetype='pop', geocodes=gc)
 
-    for (srcid,dstid) in Gin.edges():
+        elif 'relay' in n['nodetype']:
+            ip = n['relay_ip']
+            #fingerprint = n['fp']
+            #nickname = n['nick']
+            #asn = n['asn'].split()[0][2:]
+            #pop = n['pop']
+            #poiip = n['ip']
+            gc = get_geo_code(geo, ip)
+            G.add_node(ip, nodetype='relay', geocodes=gc)
+
+        elif 'dest' in n['nodetype']:
+            ip = '.'.join(n['nodeid'].split('_')[1:])
+            gc = n['country']
+            G.add_node(ip, nodetype='server', geocodes=gc)
+
+    for (srcid, dstid) in Gin.edges():
         e = Gin.edge[srcid][dstid]
-        srcn = Gin.node[srcid]
-        dstn = Gin.node[dstid]
-        if len(srcn) == 0 or 'nodetype' not in n: continue
-        if len(dstn) == 0 or 'nodetype' not in n: continue
-        srcidstr = get_id_string(srcn)
-        dstidstr = get_id_string(dstn)
-        if srcidstr in G.node and dstidstr in G.node:
-            l = e['latency']
-            G.add_edge(srcidstr, dstidstr, latencies=l)
+        s = '.'.join(n['nodeid'].split('_')[1:]) if 'dest' in srcid else srcid
+        d = '.'.join(n['nodeid'].split('_')[1:]) if 'dest' in dstid else dstid
+        if s in G.node and d in G.node: G.add_edge(s, d, latencies=e['latency'])
+        else: print "skipped edge: {0} -- {1}".format(s, d)
 
-    # make sure the dummy node is connected
+    # connect the dummy node that stores our bandwidth information
     dummy_connect_id = G.nodes()[0]
     G.add_edge("dummynode", dummy_connect_id, latencies="10000.0")
     G.add_edge(dummy_connect_id, "dummynode", latencies="10000.0")
 
+    # undirected graphs
+    assert is_connected(G)
+    assert number_connected_components(G) == 1
+
+    # directed graphs
+    #assert is_strongly_connected(G)
+    #assert number_strongly_connected_components(G) == 1
+
     nx.write_graphml(G, OUTPUT_FILENAME)
 
-def get_id_string(n):
-    t = get_node_type_string(n)
-    if 'server' in t: return '.'.join(n['nodeid'].split('_')[1:])
-    elif 'relay' in t: return n['relay_ip']
-    else: return str(n['nodeid'])
-
-def get_node_type_string(n):
-    return 'server' if 'dest' in n['nodetype'] else n['nodetype']
-
-def get_packet_loss(filename):
+def get_packet_loss():
     loss = {}
-    with open(filename, 'rb') as f:
+    with open(PLOSS_FILENAME, 'rb') as f:
         r = csv.reader(f) # country, region, jitter, packetloss, latency
         for row in r:
             country, region, jitter, packetloss, latency = row[0], row[1], float(row[2]), float(row[3]), float(row[4])
@@ -91,9 +97,9 @@ def get_packet_loss(filename):
             if code not in loss: loss[code] = packetloss
     return loss
 
-def get_bandwidth(filename):
+def get_bandwidth():
     bwdown, bwup = {}, {}
-    with open(filename, 'rb') as f:
+    with open(BW_FILENAME, 'rb') as f:
         r = csv.reader(f) # country, region, bwdown, bwup
         for row in r:
             country, region = row[0], row[1]
@@ -105,29 +111,29 @@ def get_bandwidth(filename):
             if code not in bwup: bwup[code] = up
     return bwdown, bwup
 
-def getClusterCode(geoentries, ip):
-    ip_array = ip.split('.')
-    ipnum = long(ip_array[0]) * 16777216 + long(ip_array[1]) * 65536 + long(ip_array[2]) * 256 + long(ip_array[3])
-    for entry in geoentries:
-        if ipnum >= entry.lownum and ipnum <= entry.highnum: 
-            return "{0}".format(entry.countrycode)
-    return "US"
-
-def getGeoEntries():
-    entries = []
-    with open("geoip", "rb") as f:
-        for line in f:
-            if line[0] == "#": continue
-            parts = line.strip().split(',')
-            entry = GeoIPEntry(parts[0], parts[1], parts[2])
-            entries.append(entry)
-    return entries
-
 def get_code(country, region):
     if ('US' in country and 'US' not in region) or ('CA' in country and 'CA' not in region):
         return "{0}{1}".format(country, region)
     else:
         return "{0}".format(country)
+
+def get_geo_code(geo, ip):
+    ip_array = ip.split('.')
+    ipnum = long(ip_array[0]) * 16777216 + long(ip_array[1]) * 65536 + long(ip_array[2]) * 256 + long(ip_array[3])
+    for entry in geo:
+        if ipnum >= entry[0] and ipnum <= entry[1]: 
+            return "{0}".format(entry[2])
+    return "US"
+
+def get_geo():
+    entries = []
+    with open(GEO_FILENAME, "rb") as f:
+        for line in f:
+            if line[0] == "#": continue
+            parts = line.strip().split(',')
+            entry = (parts[0], parts[1], parts[2]) # lownum, highnum, countrycode
+            entries.append(entry)
+    return entries
 
 def make_test_graph():
     G = nx.DiGraph()
@@ -149,11 +155,5 @@ def make_test_graph():
     G.add_edge("137.150.145.240", "2", latencies="2.0,2.1,2.2,2.2,2.2,2.3,2.6,2.8,3.0,3.5")
 
     nx.write_graphml(G, "test.graphml.xml")
-
-class GeoIPEntry():
-    def __init__(self, lownum, highnum, countrycode):
-        self.lownum = int(lownum)
-        self.highnum = int(highnum)
-        self.countrycode = countrycode
 
 if __name__ == '__main__': sys.exit(main())
