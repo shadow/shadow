1fa663baedc16046752bbe1b16bb20559a1b22d2
Rob Jansen <jansen@cs.umn.edu>
2013-11-23 23:36:49 -0500

    more tweaks to topology script
    refs #27

--- contrib/compute-trimmed-topology.py
+++ contrib/compute-trimmed-topology.py
@@ -4,47 +4,67 @@ import time, sys
 import networkx as nx
 from random import sample
 from multiprocessing import Process, Queue, cpu_count
-from threading import Thread
+from threading import Thread, Lock
 
-INPUT_GRAPH="topology.test.graphml.xml"
+INPUT_GRAPH="topology.full.graphml.xml"
 OUTPUT_GRAPH="topology.trimmed.graphml.xml"
 CLIENT_SAMPLE_SIZE=10000
 
-def worker(taskq, resultq, G):
+def worker(taskq, resultq, G, pois):
     for srcid in iter(taskq.get, 'STOP'):
         dists = nx.single_source_dijkstra_path_length(G, srcid)
-        resultq.put((srcid, dists))
+        wantd = {}
+        for d in dists: 
+            if d in pois: wantd[d] = dists[d]
+        resultq.put((srcid, wantd))
+
+def thread(resultq, d, dlock, doneq):
+    for r in iter(resultq.get, 'STOP'):
+        dlock.acquire()
+        d[r[0]] = r[1]
+        dlock.release()
+        doneq.put(True)
 
 def distribute(num_processes, G, pois):
-    taskq, resultq = Queue(), Queue()
-
     # each task is to do a single source shortest path for a node
-    for i in pois:
-        taskq.put(i)
+    taskq = Queue()
+    for i in pois: taskq.put(i)
+
+    d = {}
+    dlock = Lock()
 
     # start the worker processes
+    workers = []
+    doneq = Queue()
     for i in range(num_processes):
-        Process(target=worker, args=(taskq, resultq, G)).start()
+        resultq = Queue()
+        p = Process(target=worker, args=(taskq, resultq, G, pois))
+        t = Thread(target=thread, args=(resultq, d, dlock, doneq))
+        t.setDaemon(True)
+        workers.append([p, t, resultq])
+    for w in workers: w[0].start();w[1].start()
 
     # we will collect the distances from the workers
-    d = {}
     npois = len(pois)
     start = time.time()
     count = 0
     while count < npois:
-        result = resultq.get()
-        srcid, dists = result[0], result[1]
-        d[srcid] = dists
+        result = doneq.get()
+#        srcid, dists = result[0], result[1]
+#        d[srcid] = dists
         count += 1
         est = (((time.time() - start) / count) * (npois - count)) / 3600.0
-        msg = "\rfinished {0}/{1}, estimated hours remaining: {2}".format(count, npois, est)
+        msg = "\rfinished {0}/{1}, {3} waiting, estimated hours remaining: {2}".format(count, npois, est, doneq.qsize())
         sys.stdout.write(msg)
         sys.stdout.flush()
 
     # tell workesr to stop
-    for i in range(num_processes):
+    for w in workers:
         taskq.put('STOP')
-
+        w[2].put('STOP')
+#        w[0].join()
+#        w[1].join()
+    
     return d
 
 def main():
