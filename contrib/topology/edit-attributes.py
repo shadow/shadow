f42809409ea33848c1e3a22eff85c5d012c33de8
Rob Jansen <jansen@cs.umn.edu>
2014-01-21 14:40:28 -0500

    code to edit packet loss rates

--- contrib/topology/edit-attributes.py
+++ contrib/topology/edit-attributes.py
@@ -2,7 +2,8 @@
 
 import networkx as nx
 
-GRAPH="topology.graphml.xml"
+INGRAPH="topology.graphml.xml"
+OUTGRAPH="topology.edited.graphml.xml"
 
 def ensure_nonzero_latency(G):
     lintra, linter, zeros = [], [], []
@@ -22,17 +23,28 @@ def ensure_nonzero_latency(G):
 
     return G
 
+def set_node_packet_loss(G, val):
+    for n in G.nodes():
+        if 'packetloss' in G.node[n]:
+            G.node[n]['packetloss'] = val
+    return G
+
+def set_edge_packet_loss(G, val):
+    for (s, d) in G.edges():
+        if 'packetloss' in G.edge[s][d]:
+            G.edge[s][d]['packetloss'] = val
+    return G
+
 def main():
     print "reading graph..."
-    G = nx.read_graphml(GRAPH)
+    G = nx.read_graphml(INGRAPH)
 
     print "editing attributes..."
-    G = ensure_nonzero_latency(G)
-#    for (s, d) in G.edges():
-#        if float(G.edge[s][d]['latency']) == 0.0:
-#            G.edge[s][d]['latency'] = 5.0
+#    G = ensure_nonzero_latency(G)
+    G = set_node_packet_loss(G, 0.0)
+    G = set_edge_packet_loss(G, 0.005)
 
     print "writing graph..."
-    nx.write_graphml(G, GRAPH)
+    nx.write_graphml(G, OUTGRAPH)
 
 if __name__ == '__main__': main()
