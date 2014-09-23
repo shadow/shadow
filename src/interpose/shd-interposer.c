46e0eec725f440abe823ae1e2ff752edca00990a
Rob Jansen <jansen@cs.umn.edu>
2014-09-23 14:32:13 -0400

    set return value when reading random bytes
    refs #237

--- src/interpose/shd-interposer.c
+++ src/interpose/shd-interposer.c
@@ -1482,6 +1482,7 @@ ssize_t read(int fd, void *buff, size_t numbytes) {
     } else if(host_isRandomHandle(host, fd)) {
 		Random* random = host_getRandom(host);
 		random_nextNBytes(random, (guchar*)buff, numbytes);
+		ret = (ssize_t) numbytes;
     } else {
         gint osfd = host_getOSHandle(host, fd);
         if(osfd >= 0) {
