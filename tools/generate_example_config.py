1f3b4eb663bc253f21d30ec698967f868534568e
Rob Jansen <jansen@cs.umn.edu>
2014-11-22 07:19:29 -0500

    generate more example client configs

--- tools/generate_example_config.py
+++ tools/generate_example_config.py
@@ -8,6 +8,10 @@ def main():
     generate_shadow()
     generate_tgen_server()
     generate_tgen_client()
+    #generate_tgen_filetransfer_clients()
+    #generate_tgen_perf_clients(size="50 KiB", name="tgen.perfclient50k.graphml.xml")
+    #generate_tgen_perf_clients(size="1 MiB", name="tgen.perfclient1m.graphml.xml")
+    #generate_tgen_perf_clients(size="5 MiB", name="tgen.perfclient5m.graphml.xml")
 
 def generate_shadow():
     root = etree.Element("shadow")
@@ -82,6 +86,44 @@ def generate_tgen_client():
     G.add_edge("pause", "start")
 
     nx.write_graphml(G, "tgen.client.graphml.xml")
+
+def generate_tgen_filetransfer_clients(servers="server1:8888,server2:8888"):
+    # webclients
+    G = nx.DiGraph()
+
+    G.add_node("start", serverport="8888", peers=servers)
+    G.add_node("transfer", type="get", protocol="tcp", size="320 KiB")
+    G.add_node("pause", time="1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60")
+
+    G.add_edge("start", "transfer")
+    G.add_edge("transfer", "pause")
+    G.add_edge("pause", "start")
+
+    nx.write_graphml(G, "tgen.filetransferwebclient.graphml.xml")
+
+    # bulkclients
+    G = nx.DiGraph()
+
+    G.add_node("start", serverport="8888", peers=servers)
+    G.add_node("transfer", type="get", protocol="tcp", size="5 MiB")
+
+    G.add_edge("start", "transfer")
+    G.add_edge("transfer", "start")
+
+    nx.write_graphml(G, "tgen.filetransferbulkclient.graphml.xml")
+
+def generate_tgen_perf_clients(servers="server1:8888,server2:8888", size="50 KiB", name="tgen.perfclient50k.graphml.xml"):
+    G = nx.DiGraph()
+
+    G.add_node("start", serverport="8888", peers=servers)
+    G.add_node("transfer", type="get", protocol="tcp", size=size)
+    G.add_node("pause", time="60")
+
+    G.add_edge("start", "transfer")
+    G.add_edge("transfer", "pause")
+    G.add_edge("pause", "start")
+
+    nx.write_graphml(G, name)
 
 def get_topology():
     G = nx.Graph()
