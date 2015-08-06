78edbaf0c24cd89f2544364709e5fb333b40ba0d
MileB <ByersJR.Michael@gmail.com>
2015-08-06 15:41:32 -0400

    added more complete tests to sleep

--- src/test/sleep/shd-test-sleep.c
+++ src/test/sleep/shd-test-sleep.c
@@ -6,6 +6,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <stdlib.h>
+
+#define S_TO_NS 1000000000L
 
 static int _test_sleep() {
     struct timespec start, end;
@@ -26,22 +29,81 @@ static int _test_sleep() {
         return -1;
     }
 
-    /* lets make sure its within a small range of 3 seconds */
-    // TODO
+    /* let's make sure its within a small range (1ms) of 3 seconds */
+    long diff = end.tv_nsec - start.tv_nsec;
+    diff += (end.tv_sec - start.tv_sec)*S_TO_NS;
+    diff -= 3*S_TO_NS;
+    if( llabs(diff) > 1000000) {
+        fprintf(stdout, "sleep failed. diff: %li\n", diff);
+        return -1;
+    }
 
     /* success! */
     return 0;
 }
 
 static int _test_usleep() {
-    // TODO
+    struct timespec start, end;
+
+    /* get the start time */
+    if(clock_gettime(CLOCK_MONOTONIC, &start) < 0) {
+        return -1;
+    }
+
+    /* go to sleep for 30 microseconds */
+    if(usleep(30) != 0) {
+        /* was interrupted */
+        return -1;
+    }
+
+    /* get the end time */
+    if(clock_gettime(CLOCK_MONOTONIC, &end) < 0) {
+        return -1;
+    }
+
+    /* let's make sure its within a small range (1ms) of 3 microseconds */
+    long diff = end.tv_nsec - start.tv_nsec;
+    diff += (end.tv_sec - start.tv_sec)*S_TO_NS;
+    diff -= 30000L;
+    if( llabs(diff)> 1000000) {
+        fprintf(stdout, "usleep failed. diff: %li", diff);
+        return -1;
+    }
 
     /* success! */
     return 0;
 }
 
 static int _test_nanosleep() {
-    // TODO
+    struct timespec start, stop, end;
+
+    /* get the start time */
+    if(clock_gettime(CLOCK_MONOTONIC, &start) < 0) {
+        return -1;
+    }
+
+    /* set the stop time to 300 nanoseconds ahead */
+    stop.tv_sec = 0;
+    stop.tv_nsec = 300L;
+
+    /* go to sleep for 300 nanoseconds */
+    if(nanosleep(&stop, NULL) != 0) {
+        /* was interrupted */
+        return -1;
+    }
+
+    /* get the end time */
+    if(clock_gettime(CLOCK_MONOTONIC, &end) < 0) {
+        return -1;
+    }
+
+    /* let's make sure its within a small range (5ns) of 300 nanoseconds */
+    long diff = end.tv_nsec - start.tv_nsec;
+    diff += (end.tv_sec - start.tv_sec)*S_TO_NS;
+    diff -= 300L;
+    if(labs(diff) < 5) {
+        return -1;
+    }
 
     /* success! */
     return 0;
