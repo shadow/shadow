cc01710ac55d27ef7ee228b8c07bf17e26f5073c
Rob Jansen <jansen@cs.umn.edu>
2016-02-23 17:14:48 -0500

    Use default and fallback options when searching for LLVM
    refs #298
    refs #301

--- cmake/FindLLVM.cmake
+++ cmake/FindLLVM.cmake
@@ -14,7 +14,7 @@ find_path (LLVM_CMAKE_DIR LLVM-Config.cmake
 
 if(NOT LLVM_CMAKE_DIR)
   find_path (LLVM_CMAKE_DIR LLVM-Config.cmake
-    PATHS $ENV{HOME}/.shadow/share/llvm /usr/local/share/llvm /usr/share/llvm /usr/share/llvm-3.5 /usr/share/llvm-3.4 /usr/share/llvm-3.3 /usr/share/llvm-3.2 PATH_SUFFIXES cmake
+    PATHS $ENV{HOME}/.shadow/share/llvm /usr/local/share/llvm /usr/share/llvm /usr/share/llvm-3.7 /usr/share/llvm-3.6 /usr/share/llvm-3.5 /usr/share/llvm-3.4 /usr/share/llvm-3.3 /usr/share/llvm-3.2 PATH_SUFFIXES cmake
   )
 endif()
 
