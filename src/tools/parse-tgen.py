ebfa3cd45384c37b40dc5717ce523cb67f06d694
Rob Jansen <jansen@cs.umn.edu>
2017-12-04 15:30:01 -0500

    check list len before indexing it

--- src/tools/parse-tgen.py
+++ src/tools/parse-tgen.py
@@ -125,12 +125,12 @@ def process_tgen_log(filename):
                 name = line.strip().split()[11]
             elif re.search("transfer-complete", line) is not None or re.search("transfer-error", line) is not None:
                 parts = line.strip().split()
+                if len(parts) < 26: continue
 
                 if re.search('GETPUT', parts[10]) is not None:
                     # Ignore GETPUT transfer results
                     continue
 
-                if len(parts) < 26: continue
                 sim_seconds = timestamp_to_seconds(parts[2])
                 second = int(sim_seconds)
 
