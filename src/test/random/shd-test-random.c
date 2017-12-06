72b7ec009b7acdf2c3f5a1ea1eac33f50789fc57
Rob Jansen <jansen@cs.umn.edu>
2017-12-06 15:20:31 -0500

    clean up random test

--- src/test/random/shd-test-random.c
+++ src/test/random/shd-test-random.c
@@ -19,21 +19,21 @@ static int _test_dev_urandom() {
     uint numLow = 0, numHigh = 0;
 
     int fd = open(path, O_RDONLY);
-    for(int i = 0; i < 100; i++) {
+    for (int i = 0; i < 100; i++) {
         uint randomValue = 0;
 
-        if(read(fd, &randomValue, sizeof(uint)) < 0) {
+        if (read(fd, &randomValue, sizeof(uint)) < 0) {
             fprintf(stdout, "error: unable to read random data from %s\n", path);
             close(fd);
-            return -1;
+            return EXIT_FAILURE;
         }
 
-        double randomFrac = ((double)randomValue) / ((double)UINT_MAX);
+        double randomFrac = ((double) randomValue) / ((double) UINT_MAX);
         //fprintf(stdout, "########## random_frac is %f\n", randomFrac);
 
-        if(randomFrac < LOW_THRESH) {
+        if (randomFrac < LOW_THRESH) {
             numLow++;
-        } else if(randomFrac > HIGH_THRESH) {
+        } else if (randomFrac > HIGH_THRESH) {
             numHigh++;
         }
     }
@@ -41,36 +41,36 @@ static int _test_dev_urandom() {
 
     fprintf(stdout, "got %u low and %u high values from %s\n", numLow, numHigh, path);
 
-    if(numLow > 0 && numHigh > 0) {
+    if (numLow > 0 && numHigh > 0) {
         /* success! */
-        return 0;
-    } else{
-        return -1;
+        return EXIT_SUCCESS;
+    } else {
+        return EXIT_FAILURE;
     }
 }
 
 static int _test_rand() {
-    for(int i = 0; i < 100; i++) {
+    for (int i = 0; i < 100; i++) {
         int random_value = rand();
-        if(random_value < 0 || random_value > RAND_MAX) {
-            return -1;
+        if (random_value < 0 || random_value > RAND_MAX) {
+            return EXIT_FAILURE;
         }
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 int main(int argc, char* argv[]) {
     fprintf(stdout, "########## random test starting ##########\n");
 
-    if(_test_dev_urandom() < 0) {
+    if (_test_dev_urandom() < 0) {
         fprintf(stdout, "########## _test_dev_urandom() failed\n");
-        return -1;
+        return EXIT_FAILURE;
     }
-    if(_test_rand() < 0) {
+    if (_test_rand() < 0) {
         fprintf(stdout, "########## _test_rand() failed\n");
-        return -1;
+        return EXIT_FAILURE;
     }
 
     fprintf(stdout, "########## random test passed! ##########\n");
-    return 0;
+    return EXIT_SUCCESS;
 }
