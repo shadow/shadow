5840f2668d70a6142ddac6fc7de4ed70402f1522
Justin Tracey <j2tracey@gmail.com>
2018-01-01 13:27:22 -0500

    redefine memcpy and memset Similar to how GCC will insert calls to memcpy if tree-loop-distribute-patterns is enabled, Clang will (at all optimization levels) insert PLT references to memcpy and memset if it finds any code equivalent to them. To get around this, we explicitly define our implementations with the same name/prototype, so Clang knows to use it instead.
    Unfortunately, while these problems are related, they are distinct, so we have
    to have seperate fixes for the GCC and Clang versions of the problem.

--- src/external/elf-loader/vdl-mem.h
+++ src/external/elf-loader/vdl-mem.h
@@ -7,9 +7,16 @@
 extern "C" {
 #endif
 
-void vdl_memcpy (void *dst, const void *src, size_t len);
-void vdl_memmove (void *dst, const void *src, size_t len);
+// in order to prevent clang from replacing our memcpy and memset definitions
+// with a plt lookup, we have to redeclare the c std lib prototypes
+void *memcpy(void *d, const void *s, size_t len);
+void *memset (void *s, int c, size_t n);
+// the compiler will just optimize these to the above,
+// we define them for breakpoints specific to elf-loader
+void vdl_memcpy (void *d, const void *s, size_t len);
 void vdl_memset (void *s, int c, size_t n);
+
+void vdl_memmove (void *dst, const void *src, size_t len);
 int vdl_memcmp (void *a, void *b, size_t n);
 
 #ifdef __cplusplus
