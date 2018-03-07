5e8e74766ebbbc332893a2befe7092f982ef35c6
Justin Tracey <j2tracey@gmail.com>
2018-03-07 08:30:19 -0500

    use /usr/bin/env in scripts Some distros, e.g. Ubuntu, don't put crucial executables like bash in /usr/bin.

--- src/test/leakcheck.sh
+++ src/test/leakcheck.sh
@@ -1,1 +1,1 @@
-/usr/bin/grep "ObjectCounter: counter diffs:" ../../Testing/Temporary/LastTest.log.tmp | head -n 20 | /usr/bin/cut -d' ' -f8- > leakcheck.log
+/usr/bin/env grep "ObjectCounter: counter diffs:" ../../Testing/Temporary/LastTest.log.tmp | head -n 20 | /usr/bin/env cut -d' ' -f8- > leakcheck.log
