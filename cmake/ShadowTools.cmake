9b14bd01ef4900c77f5060df048a2cdd62f1a9ab
Jim Newsome <jnewsome@torproject.org>
2020-02-20 10:03:46 -0800

    Pass link flags only to link steps.
    * Adds a macro add_ldflags analagous to add_cflags.
    * Uses that macro to fix a "unused-command-line-argument" warning in
      clang, where link flags were being passed to compile steps.
    
    Progress on GH-711.

--- cmake/ShadowTools.cmake
+++ cmake/ShadowTools.cmake
@@ -41,3 +41,14 @@ macro(add_cflags)
 endmacro(add_cflags)
 
 ######################################################################################################
+## ADD_LDFLAGS                                                                                       ##
+######################################################################################################
+
+macro(add_ldflags)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${ARGN}")
+    set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${ARGN}")
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${ARGN}")
+    set(CMAKE_STATIC_LINKER_FLAGS "${CMAKE_STATIC_LINKER_FLAGS} ${ARGN}")
+endmacro(add_ldflags)
+
+######################################################################################################
