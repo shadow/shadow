750c6ac4ad34949d1cff81ba0543024a3cf45604
vuille_f <florian.vuillemot@posteo.net>
2020-03-29 17:36:06 +0200

    Allow strip log script to run with python2 and 3

--- src/tools/strip_log_for_compare.py
+++ src/tools/strip_log_for_compare.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#!/usr/bin/python
 
 '''
 Make cleaner diffs of shadow logfiles by stripping the parts of the logs that
@@ -7,25 +7,26 @@ we can determine if two repeated experiments produced the same results based on
 their log files.
 '''
 
+from __future__ import print_function
 import sys
 
 if len(sys.argv) < 3:
-    print >>sys.stderr, "USAGE: {0} logfile outputfile".format(sys.argv[0])
+    print("USAGE: {0} logfile outputfile".format(sys.argv[0]), file=sys.stderr)
     exit()
 
-inf = open(sys.argv[1], 'rb')
-outf = open(sys.argv[2], 'wb')
+inf = open(sys.argv[1], 'r')
+outf = open(sys.argv[2], 'w')
 
 n = 0
 for line in inf:
     parts = line.strip().split()
-    for part in parts[1:]: # skip the first timer column
-        if part.startswith("0x"): continue # skip printing memory addresses
-        print >>outf, part,
-    print >>outf, ""
+    parts = parts[1:] # skip the first timer column
+    parts = [p + ' ' for p in parts if not p.startswith("0x")] # skip printing memory addresses
+    parts.append("\n")
+    outf.writelines(parts)
     n += 1
     
 inf.close()
 outf.close()
 
-print >>sys.stderr, "Done! Processed {0} lines.".format(n)
+print("Done! Processed {0} lines.".format(n), file=sys.stderr)
