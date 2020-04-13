0af2997799bf92de6671a6136b3733178042f2b8
Jim Newsome <jnewsome@torproject.org>
2020-04-13 09:01:26 -0500

    Add error-checking and print explicit success

--- src/test/epoll/test_epoll_writeable.c
+++ src/test/epoll/test_epoll_writeable.c
@@ -2,7 +2,7 @@
  * The Shadow Simulator
  * See LICENSE for licensing information
  */
-#define _POSIX_C_SOURCE 199309L
+//#define _POSIX_C_SOURCE 199309L
 #include <errno.h>
 #include <fcntl.h>
 #include <netdb.h>
@@ -18,6 +18,7 @@
 #define WRITE_SZ 65535
 
 static void _client(void) {
+    printf("Start");
    struct addrinfo *client_info, hints;
 
    memset(&hints, 0, sizeof(hints));
@@ -25,19 +26,39 @@ static void _client(void) {
    hints.ai_socktype = SOCK_STREAM;
 
    const char *port = PORT_STR;
-   getaddrinfo("testnode", port, &hints, &client_info);
+   int rv;
+   if ((rv = getaddrinfo("testnode", port, &hints, &client_info)) < 0) {
+       fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+       exit(EXIT_FAILURE);
+   }
 
    int client_socket = socket(client_info->ai_family,
                               client_info->ai_socktype,
                               client_info->ai_protocol);
 
-   connect(client_socket, client_info->ai_addr, client_info->ai_addrlen);
+   if (connect(client_socket, client_info->ai_addr, client_info->ai_addrlen) <
+       0) {
+       perror("connect");
+       exit(EXIT_FAILURE);
+   }
 
-   for (;;) { sleep(10); }
+   char *buf = malloc(WRITE_SZ);
+   int recvd = 0;
+   while (recvd < 30 * WRITE_SZ) {
+       int rc;
+       if ((rc = recv(client_socket, buf, WRITE_SZ, 0)) < 0) {
+           perror("recv");
+           exit(EXIT_FAILURE);
+       };
+       printf("Recvd %d\n", rc);
+       recvd += rc;
+   }
+   printf("Exit");
 }
 
 static int _server(void) {
    struct addrinfo *info, hints;
+   int rv;
 
    memset(&hints, 0, sizeof(hints));
    hints.ai_family = AF_INET;
@@ -45,29 +66,52 @@ static int _server(void) {
    hints.ai_flags = AI_PASSIVE;
 
    const char *port = PORT_STR;
-   getaddrinfo(NULL, port, &hints, &info);
+   if ((rv = getaddrinfo(NULL, port, &hints, &info)) < 0) {
+       fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+       return EXIT_FAILURE;
+   }
 
    int server_socket = socket(info->ai_family,
                               info->ai_socktype,
                               info->ai_protocol);
 
-   bind(server_socket, info->ai_addr, info->ai_addrlen);
-   listen(server_socket, 10);
+   if (bind(server_socket, info->ai_addr, info->ai_addrlen) < 0) {
+       perror("bind");
+       return EXIT_FAILURE;
+   }
+   if (listen(server_socket, 10) < 0) {
+       perror("listen");
+       return EXIT_FAILURE;
+   }
 
    struct sockaddr_storage client_addr;
    socklen_t addr_size = sizeof(client_addr);
 
    int client_socket =
       accept(server_socket, (struct sockaddr *)&client_addr, &addr_size);
+   if (client_socket < 0) {
+       perror("accept");
+       return EXIT_FAILURE;
+   }
 
-   fcntl(client_socket, F_SETFL, fcntl(client_socket, F_GETFL, 0) | O_NONBLOCK);
+   if (fcntl(client_socket, F_SETFL, fcntl(client_socket, F_GETFL, 0) | O_NONBLOCK) < 0) {
+       perror("fcntl");
+       return EXIT_FAILURE;
+   }
 
    int epoll_fd = epoll_create1(0);
+   if (epoll_fd < 0) {
+       perror("epoll_create1");
+       return EXIT_FAILURE;
+   }
    struct epoll_event ev;
    memset(&ev, 0, sizeof(ev));
    ev.data.fd = client_socket;
    ev.events = EPOLLOUT;
-   epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_socket, &ev);
+   if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_socket, &ev) < 0) {
+       perror("epoll_ctl");
+       return EXIT_FAILURE;
+   }
 
    struct epoll_event events[5]; // Events to be populated by epoll_wait
 
@@ -77,9 +121,17 @@ static int _server(void) {
       return EXIT_FAILURE;
    }
 
-   for (int idx = 0; idx < 30; ++idx) {
+   int sent = 0;
+   while (sent < 30 * WRITE_SZ) {
       int num_events = epoll_wait(epoll_fd, events, 5, -1);
-
+      if (num_events < 0) {
+          perror("epoll_wait");
+          return EXIT_FAILURE;
+      } 
+      if (num_events == 0) {
+          fprintf(stderr, "epoll_wait unexpectedly returned 0 events with inf timeout\n");
+          return EXIT_FAILURE;
+      }
       if (num_events > 0) {
          // Sanity check to ensure epoll only returned one event for the socket
          // we are monitoring.
@@ -99,9 +151,12 @@ static int _server(void) {
          // Let's try to send some data.
          int rc = send(client_socket, buf, WRITE_SZ, 0);
          if (rc <= 0) {
-            printf("epoll reported client_socket is writeable but send() failed with %s.\n", strerror(errno));
-            return EXIT_FAILURE;
+             printf("epoll reported client_socket is writeable but send() "
+                    "failed with %s.\n",
+                    strerror(errno));
+             return EXIT_FAILURE;
          }
+         sent += rc;
       }
    }
 
