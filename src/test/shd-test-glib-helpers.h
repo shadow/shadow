6b3698e4476c42f34b776e0cadaa19d5f4cb54ae
Jim Newsome <jnewsome@torproject.org>
2020-04-15 14:43:45 -0500

    Beginning of UDP test
    This is mostly getting in place a framework for UDP tests; particularly
    for using dynamically assigned ports when not running under Shadow and
    communicating them via a fifo(7). I'll flesh out some more tests in a
    follow-up.
    
    Initially I tried doing the same under Shadow as well, but it doesn't
    deal well with blocking on an `open` call. I got it to sort of work if I
    opened the fifo in nonblocking mode and looped, but decided it'd be
    cleaner to just use hard-coded ports under Shadow for now. I expect that
    the blocking `open` call won't be an issue under Phantom, so we can
    perhaps switch it over there.

--- src/test/shd-test-glib-helpers.h
+++ src/test/shd-test-glib-helpers.h
@@ -4,13 +4,15 @@
 #include <glib.h>
 #include <unistd.h>
 
-// Similar to g_assert_true, but include stringified errno on failure.
-#define assert_true_errno(c)                                                   \
+#define assert_true_errstring(c, s)                                            \
     if (!(c)) {                                                                \
-        g_error("!(%s): %s", #c, strerror(errno));                             \
+        g_error("!(%s): %s", #c, (s));                                         \
         g_test_fail();                                                         \
+        exit(EXIT_FAILURE);                                                    \
     }
 
+// Similar to g_assert_true, but include stringified errno on failure.
+#define assert_true_errno(c) assert_true_errstring(c, strerror(errno))
 #define assert_nonnull_errno(p) assert_true_errno((p) != NULL)
 #define assert_nonneg_errno(p) assert_true_errno((p) >= 0)
 
@@ -23,7 +25,9 @@
             g_error("Got errno %d (%s) instead of %d (%s)", _errno,            \
                     strerror(_errno), _e, strerror(_e));                       \
             g_test_fail();                                                     \
+            exit(EXIT_FAILURE);                                                \
         }                                                                      \
     }
+
 
 #endif
