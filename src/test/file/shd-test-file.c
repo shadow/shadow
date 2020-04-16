cf592af17773038ba20cc0dab5b0a726cece8350
Jim Newsome <jnewsome@torproject.org>
2020-04-16 16:04:18 -0500

    move glib helper macros to a common header

--- src/test/file/shd-test-file.c
+++ src/test/file/shd-test-file.c
@@ -17,27 +17,7 @@
 #include <sys/uio.h>
 #include <errno.h>
 
-// Similar to g_assert_true, but include stringified errno on failure.
-#define assert_true_errno(c)                                                   \
-    if (!(c)) {                                                                \
-        g_error("!(%s): %s", #c, strerror(errno));                             \
-        g_test_fail();                                                         \
-    }
-
-#define assert_nonnull_errno(p) assert_true_errno((p) != NULL)
-#define assert_nonneg_errno(p) assert_true_errno((p) >= 0)
-
-// Assert that errno is the expected value.
-#define assert_errno_is(e)                                                     \
-    {                                                                          \
-        int _errno = errno;                                                    \
-        int _e = e;                                                            \
-        if (_e != _errno) {                                                    \
-            g_error("Got errno %d (%s) instead of %d (%s)", _errno,            \
-                    strerror(_errno), _e, strerror(_e));                       \
-            g_test_fail();                                                     \
-        }                                                                      \
-    }
+#include "test/shd-test-glib-helpers.h"
 
 // For use in conjunction with g_auto for a file that will delete itself on
 // function exit.
