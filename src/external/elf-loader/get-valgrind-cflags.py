754356d6448bb55de87a15c62065b03c2fde876a
Vuillemot Florian <florian.vuillemot@posteo.net>
2020-04-25 18:24:09 +0200

    Make script executable by python2 and python3

--- src/external/elf-loader/get-valgrind-cflags.py
+++ src/external/elf-loader/get-valgrind-cflags.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import os
 
-if os.path.exists ('/usr/include/valgrind/valgrind.h'):
-    print '-DHAVE_VALGRIND_H -I/usr/include'
+if os.path.exists('/usr/include/valgrind/valgrind.h'):
+    print('-DHAVE_VALGRIND_H -I/usr/include')
