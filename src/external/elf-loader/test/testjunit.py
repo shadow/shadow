5c14a30b87f0f49851cc64350f8db678daca91b5
Vuillemot Florian <florian.vuillemot@posteo.net>
2020-04-25 19:11:02 +0200

    Make script testjunit ,executable by python2 and python3

--- src/external/elf-loader/test/testjunit.py
+++ src/external/elf-loader/test/testjunit.py
@@ -1,15 +1,26 @@
 #! /usr/bin/env python
 ## -*- Mode: python; py-indent-offset: 4; indent-tabs-mode: nil; coding: utf-8; -*-
+
+from __future__ import print_function
+
 import sys
 import os
 import subprocess
 import junit_xml_output
 
+ENCODING = 'UTF-8'
+
 test_cases = []
+
+
+def get_subprocess_output(proc):
+    proc_stdout, proc_stderr = proc.communicate()
+    return proc_stdout.decode(ENCODING), proc_stderr.decode(ENCODING)
+
 
 for arg in sys.argv[1:]:
     if '12' in arg:
-        val = subprocess.Popen(['rm' ,'-f', 'libl.so'])
+        val = subprocess.Popen(['rm', '-f', 'libl.so'])
         val.wait ()
 
     cmd = ['./'+arg+'-ldso']
@@ -19,14 +30,14 @@ for arg in sys.argv[1:]:
                            stdout = subprocess.PIPE,
                            stderr = subprocess.PIPE,
                            env = test_env)
-    (stdout, stderr) = val.communicate()
-    f = open ('output/' + arg, 'w')
-    f.write (stdout)
-    f.close ()
+    stdout, stderr = get_subprocess_output(val)
+    f = open('output/' + arg, 'w')
+    f.write(stdout)
+    f.close()
     if val.returncode != 0:
-        test_cases.append(junit_xml_output.TestCase(arg, '(crash)\n '+ stderr,
+        test_cases.append(junit_xml_output.TestCase(arg, '(crash)\n ' + stderr,
                                                     "failure"))
-        print 'CRASH ' + arg + '  -- LD_LIBRARY_PATH=.:../ ./' + arg + '-ldso'
+        print('CRASH ' + arg + '  -- LD_LIBRARY_PATH=.:../ ./' + arg + '-ldso')
     else:
         cmd = ['diff', '-q',
                'output/' + arg,
@@ -34,17 +45,15 @@ for arg in sys.argv[1:]:
         val = subprocess.Popen(cmd,
                                stdout = subprocess.PIPE,
                                stderr = subprocess.PIPE)
-        (stdout, stderr) = val.communicate()
-        #print stdout
-        #print stderr
+        stdout, stderr = get_subprocess_output(val)
         if val.returncode != 0:
             test_cases.append(junit_xml_output.TestCase(arg, stderr,
                                                         "failure"))
-            print 'FAIL ' + arg + '  -- LD_LIBRARY_PATH=.:../ ./' + arg + '-ldso'
+            print('FAIL ' + arg + '  -- LD_LIBRARY_PATH=.:../ ./' + arg + '-ldso')
         else:
             test_cases.append(junit_xml_output.TestCase(arg, stdout,
                                                         "success"))
-            print 'PASS ' + arg
+            print('PASS ' + arg)
 
 junit_xml = junit_xml_output.JunitXml("elf-loader-tests", test_cases)
 f = open ('elf-loader-tests.xml', 'w')
