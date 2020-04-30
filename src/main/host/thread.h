f286f85b532b4afcf0754f8effc08c6a836d1b50
Jim Newsome <jnewsome@torproject.org>
2020-04-30 17:08:00 -0500

    Delete thread_new declaration. (Implementation no longer exists)

--- src/main/host/thread.h
+++ src/main/host/thread.h
@@ -15,7 +15,6 @@ typedef struct _Thread Thread;
 #include "main/host/syscall_handler.h"
 #include "main/host/syscall_types.h"
 
-Thread* thread_new(gint threadID, SysCallHandler* sys);
 void thread_ref(Thread* thread);
 void thread_unref(Thread* thread);
 
