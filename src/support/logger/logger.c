aa7b260dab67b4d84ca11246c4864d4fa0eae0dd
Jim Newsome <jnewsome@torproject.org>
2020-05-14 18:06:19 -0500

    Write to stderr directly instead of using glib's logger
    glib's default logger has some default behaviors I don't think we want:
    
    It normally suppresses 'debug' and 'info' level messages, unless you use
    the environment variable G_MESSAGES_DEBUG to enable them for a given
    domain.
    
    It writes some messages to stdout and some to stderr, depending on
    level.
    
    It prefixes with the process name and pid unless overridden with the
    G_MESSAGES_PREFIXED environmant variable.
    
    https://developer.gnome.org/glib/stable/glib-Message-Logging.html#g-log-default-handler
    
    All of these behaviors could be overridden by installing our own default
    log handler, but at that point using glib's logger wouldn't really be
    buying us anything over writing to stderr directly.

--- src/support/logger/logger.c
+++ src/support/logger/logger.c
@@ -1,6 +1,7 @@
 #include "support/logger/logger.h"
 
 #include <pthread.h>
+#include <stdio.h>
 #include <sys/time.h>
 
 static Logger* defaultLogger = NULL;
@@ -13,19 +14,6 @@ void logger_setDefault(Logger* logger) {
 }
 
 Logger* logger_getDefault() { return defaultLogger; }
-
-static GLogLevelFlags _gloglevelflags(LogLevel level) {
-    switch (level) {
-        case LOGLEVEL_CRITICAL: return G_LOG_LEVEL_CRITICAL;
-        case LOGLEVEL_WARNING: return G_LOG_LEVEL_WARNING;
-        case LOGLEVEL_MESSAGE: return G_LOG_LEVEL_MESSAGE;
-        case LOGLEVEL_INFO: return G_LOG_LEVEL_INFO;
-        case LOGLEVEL_DEBUG: return G_LOG_LEVEL_DEBUG;
-        case LOGLEVEL_UNSET:
-        case LOGLEVEL_ERROR:
-        default: return G_LOG_LEVEL_ERROR;
-    }
-}
 
 // Process start time, initialized on first use.
 // TODO: Parse out of /proc/$$/stat instead to get the true process start time.
@@ -58,8 +46,8 @@ static void _logger_default_log(LogLevel level, const gchar* fileName,
     gmtime_r(&tv.tv_sec, &tm);
     gchar* timeString = g_strdup_printf("%02d:%02d:%02d.%06ld", tm.tm_hour,
                                         tm.tm_min, tm.tm_sec, tv.tv_usec);
-    g_log("shadow", _gloglevelflags(level), "%s [%s:%i] [%s] %s", timeString,
-          baseName, lineNumber, functionName, message);
+    g_print("%s %s [%s:%i] [%s] %s\n", timeString, loglevel_toStr(level),
+            baseName, lineNumber, functionName, message);
     g_free(message);
     g_free(timeString);
     g_free(baseName);
