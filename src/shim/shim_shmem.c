cdef7c573e296ccf193c6c808fe4bb20f6fe2de8
Jim Newsome <jnewsome@torproject.org>
2020-06-03 16:32:47 -0500

    bugfix: was asserting wrong event in _shim_shmemHandleCloneString

--- src/shim/shim_shmem.c
+++ src/shim/shim_shmem.c
@@ -19,7 +19,7 @@ static void _shim_shmemHandleClone(const ShimEvent* ev) {
 }
 
 static void _shim_shmemHandleCloneString(const ShimEvent* ev) {
-    assert(ev && ev->event_id == SHD_SHIM_EVENT_CLONE_REQ);
+    assert(ev && ev->event_id == SHD_SHIM_EVENT_CLONE_STRING_REQ);
 
     ShMemBlock blk = shmemserializer_globalBlockDeserialize(
         &ev->event_data.shmem_blk.serial);
