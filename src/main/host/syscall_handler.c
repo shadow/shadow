b7771585e7aa1763a3b008d2b500a16834203534
Jim Newsome <jnewsome@torproject.org>
2020-06-12 12:39:16 -0500

    Revert to simplify merge

--- src/main/host/syscall_handler.c
+++ src/main/host/syscall_handler.c
@@ -183,177 +183,21 @@ SysCallReturn syscallhandler_make_syscall(SysCallHandler* sys,
         NATIVE(openat);
 
         // **************************************
-        // Needed for tor, but not handled yet:
-        // **************************************
-        // Whitelisted in tor/src/lib/sandbox/sandbox.c
-        NATIVE(chown);
-        // Found in strace of shadow-plugin-tor integration test.
-        NATIVE(clone);
-        // Whitelisted in tor/src/lib/sandbox/sandbox.c
-        NATIVE(chmod);
-        // Found in strace of shadow-plugin-tor integration test.
-        NATIVE(eventfd2);
-        // Found in strace of shadow-plugin-tor integration test.
-        NATIVE(fallocate);
-        // Found in strace of shadow-plugin-tor integration test.
-        NATIVE(fchmod);
-        // Manipulate file descriptor.
-        //
-        // Called from tor(tor_fopen_cloexec) -> libpthread(__fcntl)
-        NATIVE(fcntl);
-#ifdef SYS_fcntl64
-        // Whitelisted in tor/src/lib/sandbox/sandbox.c
-        NATIVE(fcntl64);
-#endif
-        // Apply or remove advisory lock on an open file.
-        //
-        // Called from tor(tor_init) -> ... tor(set_options) -> ...
-        // tor(tor_lockfile_lock)
-        NATIVE(flock);
-        // Found in strace of shadow-plugin-tor integration test.
-        NATIVE(fstatfs);
-        // fast user-space locking. Basically handles blocking until a lock is
-        // released, and conversely waking up blocked threads when it is
-        // released.
-        //
-        // Called from CRYPTO_THREAD_run_once -> __pthread_once_slow
-        NATIVE(futex);
-        // get directory entries.
-        // We'll need handle file descriptor remapping.
-        NATIVE(getdents);
-        // Called a few places in libcrypto.
-        NATIVE(getrandom);
-        // Found in strace of shadow-plugin-tor integration test.
-        NATIVE(getrlimit);
-        // Whitelisted in tor/src/lib/sandbox/sandbox.c
-        NATIVE(getsockopt);
-        // Found in strace of shadow-plugin-tor integration test.
-        NATIVE(gettimeofday);
-        // Whitelisted in tor/src/lib/sandbox/sandbox.c
-        NATIVE(ioctl);
-        // Whitelisted in tor/src/lib/sandbox/sandbox.c.
-        // It looks like it's configured to only work with signal=0; i.e.
-        // check for process existence, but not actually send a signal.
-        NATIVE(kill);
-        NATIVE(lseek);
-        NATIVE(mmap);
-#ifdef SYS_mmap2
-        // Whitelisted in tor/src/lib/sandbox/sandbox.c
-        NATIVE(mmap2);
-#endif
-        // Only deals in address and offset, so might not need for correctness.
-        // Might want it to GC any bookkeeping from corresponding mmap calls,
-        // though.
-        NATIVE(munmap);
-        // Whitelisted in tor/src/lib/sandbox/sandbox.c
-        NATIVE(open);
-        // Whitelisted in tor/src/lib/sandbox/sandbox.c
-        NATIVE(prctl);
-        // Surprisingly, gets called while building the list of local
-        // interfaces, using sa_family=AF_NETLINK.
-        //
-        // Called from tor(get_interface_addresses_raw) -> libc(getifaddrs) ->
-        // libc(if_indextoname)
-        NATIVE(recvmsg);
-        // Found in strace of shadow-plugin-tor integration test.
-        NATIVE(setrlimit);
-        // get/set list of robust futexes. The  purpose  of  the  robust futex
-        // list is to ensure that if a thread accidentally fails to unlock a
-        // futex before terminating or calling execve.
-        //
-        // Might be able to get away without emulating, but should take a closer
-        // look when supporting threading, futex, etc.
-        //
-        // Called from pthread(__pthread_initialize_minimal)
-        NATIVE(set_robust_list);
-        // Whitelisted in tor/src/lib/sandbox/sandbox.c
-        NATIVE(setsockopt);
-        // Set pointer to thread ID.
-        //
-        // Might be able to get away without emulating, but should take a closer
-        // look when supporting threading, futex, etc.
-        //
-        // Called from pthread(__pthread_initialize_minimal)
-        NATIVE(set_tid_address);
-        // Whitelisted in tor/src/lib/sandbox/sandbox.c
-        NATIVE(socketpair);
-        // Whitelisted in tor/src/lib/sandbox/sandbox.c
-        NATIVE(time);
-
-        // **************************************
         // Not handled (yet):
         // **************************************
-        NATIVE(execve);
-
-        // **************************************
-        // Called from tor, but probably can continue to let plugin execute
-        // natively.
-        // **************************************
-        // Checks if a file is accessible.
         NATIVE(access);
-        // Change data segment size. Not needed for correctness, though might
-        // be useful to intercept this later to put the heap in a pre-shared
-        // memory region.
-        NATIVE(brk);
-        // Gets or sets segment registers.
         NATIVE(arch_prctl);
-        // get real group id of calling process.
-        NATIVE(getgid);
-        // Returns effective user ID of the calling process.
-        NATIVE(geteuid);
-        // get effective group id of calling process.
-        NATIVE(getegid);
-        // get real user ID of calling process.
-        NATIVE(getuid);
-        // Found in strace of shadow-plugin-tor integration test.
-        NATIVE(lstat);
-        // give advice to kernel about address range... to improve system or
-        // application performance.
-        NATIVE(madvise);
-        // Found in strace of shadow-plugin-tor integration test.
-        NATIVE(mkdir);
-        // Change access protections of calling process's memory pages.
+        NATIVE(brk);
+        NATIVE(execve);
+        NATIVE(mmap);
         NATIVE(mprotect);
-        // Expands, shrinks, and/or moves a memory mapping.
-        NATIVE(mremap);
-        // Get and set resource limits for a specified process. Takes a pid, so
-        // *may* need to remap if anything tries to set limits for other
-        // processes. In the strace calls I see in Tor though, pid 0 is used,
-        // meaning "this" process.
-        //
-        // Called from tor(set_max_file_descriptors)
+        NATIVE(munmap);
         NATIVE(prlimit64);
-        // Rename a file. Takes paths rather than file descriptors, so can
-        // probably allow natively.
-        //
-        // Called from tor(save_state_callback) -> ... tor(finish_writing_to_file_impl)
-        NATIVE(rename);
-        // Change action taken by a process on receipt of a signal. Can probably
-        // get away without this, assuming signals aren't used for non-error
-        // cases.
-        //
-        // Called from:
-        // - tor(handle_signals) -> ...
         NATIVE(rt_sigaction);
-        // Fetch and/or change signal mask. As with rt_sigaction, we can
-        // probably get away without it for now.
         NATIVE(rt_sigprocmask);
-        // Deals in paths rather than file descriptors. Should be ok to allow
-        // natively.
+        NATIVE(set_robust_list);
+        NATIVE(set_tid_address);
         NATIVE(stat);
-#ifdef SYS_stat64
-        // Whitelisted in tor/src/lib/sandbox/sandbox.c
-        NATIVE(stat64);
-#endif
-        // Returns stats on memory, swap, load average.
-        //
-        // Might be needed for determinism, but the only call I see is actually
-        // in libc's qsort.
-        //
-        // Called from libssl(SSL_rstate_string) -> libc(qsort_r)
-        NATIVE(sysinfo);
-        // unlinks (deletes) a file by name.
-        NATIVE(unlink);
         default:
             info("unhandled syscall %ld", args->number);
             scr = (SysCallReturn){.state = SYSCALL_NATIVE};
