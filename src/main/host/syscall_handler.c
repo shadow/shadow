975d8acb0da39e283be736e343d4e824a9da01b5
Jim Newsome <jnewsome@torproject.org>
2020-06-23 23:30:38 +0000

    Make unhandled syscalls a bit louder
    At this point we want to know about any syscalls being executed that we both
    aren't handling and haven't explicitly noted that we're not handling.

--- src/main/host/syscall_handler.c
+++ src/main/host/syscall_handler.c
@@ -303,6 +303,7 @@ SysCallReturn syscallhandler_make_syscall(SysCallHandler* sys,
         // (because the plugin can natively):
         // ***************************************
         NATIVE(access);
+        NATIVE(getcwd);
         NATIVE(geteuid);
         NATIVE(getegid);
         NATIVE(getgid);
@@ -324,7 +325,7 @@ SysCallReturn syscallhandler_make_syscall(SysCallHandler* sys,
         NATIVE(unlink);
 
         default:
-            info("unhandled syscall %ld", args->number);
+            warning("unhandled syscall %ld", args->number);
             scr = (SysCallReturn){.state = SYSCALL_NATIVE};
             break;
     }
