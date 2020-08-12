f7ea8112f5069683b8970273ab63088080771902
Jim Newsome <jnewsome@torproject.org>
2020-08-12 16:53:14 -0500

    Add more type conversions for syscall types

--- src/main/host/syscall_types.rs
+++ src/main/host/syscall_types.rs
@@ -1,15 +1,27 @@
 use crate::cbindings as c;
 use std::convert::From;
 
+#[derive(Copy, Clone)]
+pub struct PluginPtr {
+    ptr: c::PluginPtr,
+}
+
 impl From<PluginPtr> for c::PluginPtr {
     fn from(v: PluginPtr) -> c::PluginPtr {
         v.ptr
     }
 }
 
-#[derive(Copy, Clone)]
-pub struct PluginPtr {
-    ptr: c::PluginPtr,
+impl From<c::PluginPtr> for PluginPtr {
+    fn from(v: c::PluginPtr) -> PluginPtr {
+        PluginPtr { ptr: v }
+    }
+}
+
+impl From<PluginPtr> for usize {
+    fn from(v: PluginPtr) -> usize {
+        v.ptr.val as usize
+    }
 }
 
 impl From<usize> for PluginPtr {
@@ -28,17 +40,9 @@ impl From<u64> for PluginPtr {
     }
 }
 
-impl From<c::PluginPtr> for PluginPtr {
-    fn from(v: c::PluginPtr) -> PluginPtr {
-        PluginPtr { ptr: v }
-    }
-}
-
-impl From<SysCallReg> for PluginPtr {
-    fn from(v: SysCallReg) -> PluginPtr {
-        PluginPtr {
-            ptr: unsafe { v.reg.as_ptr },
-        }
+impl From<PluginPtr> for u64 {
+    fn from(v: PluginPtr) -> u64 {
+        v.ptr.val
     }
 }
 
@@ -61,11 +65,23 @@ impl From<u64> for SysCallReg {
     }
 }
 
+impl From<SysCallReg> for u64 {
+    fn from(v: SysCallReg) -> u64 {
+        unsafe { v.reg.as_u64 }
+    }
+}
+
 impl From<usize> for SysCallReg {
     fn from(v: usize) -> SysCallReg {
         SysCallReg {
             reg: c::SysCallReg { as_u64: v as u64 },
         }
+    }
+}
+
+impl From<SysCallReg> for usize {
+    fn from(v: SysCallReg) -> usize {
+        unsafe { v.reg.as_u64 as usize }
     }
 }
 
@@ -77,6 +93,12 @@ impl From<i64> for SysCallReg {
     }
 }
 
+impl From<SysCallReg> for i64 {
+    fn from(v: SysCallReg) -> i64 {
+        unsafe { v.reg.as_i64 }
+    }
+}
+
 impl From<i32> for SysCallReg {
     fn from(v: i32) -> SysCallReg {
         SysCallReg {
@@ -85,10 +107,24 @@ impl From<i32> for SysCallReg {
     }
 }
 
+impl From<SysCallReg> for i32 {
+    fn from(v: SysCallReg) -> i32 {
+        unsafe { v.reg.as_i64 as i32 }
+    }
+}
+
 impl From<PluginPtr> for SysCallReg {
     fn from(v: PluginPtr) -> SysCallReg {
         SysCallReg {
             reg: c::SysCallReg { as_ptr: v.into() },
+        }
+    }
+}
+
+impl From<SysCallReg> for PluginPtr {
+    fn from(v: SysCallReg) -> PluginPtr {
+        PluginPtr {
+            ptr: unsafe { v.reg.as_ptr },
         }
     }
 }
