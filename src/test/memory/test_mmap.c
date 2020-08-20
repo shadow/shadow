fcb248b14368eb4d87600e2c7b88883ffeb04517
Jim Newsome <jnewsome@torproject.org>
2020-08-19 19:13:08 -0500

    Extend mmap tests
    * Add test for mremap clobbering part of an existing mapping
    * Validate that shadow can access the effected memory

--- src/test/memory/test_mmap.c
+++ src/test/memory/test_mmap.c
@@ -13,6 +13,31 @@
 
 #define MAPLEN 16
 
+// Writes data to a file and reads it back in, to validate that Shadow accesses
+// the given memory correctly.
+static void _validate_shadow_access(unsigned char* buf, size_t size) {
+    GError* error = NULL;
+    char* name = NULL;
+    int fd = g_file_open_tmp("test_mmap.XXXXXX", &name, &error);
+    g_assert_no_error(error);
+    close(fd);
+
+    g_file_set_contents(name, (gchar*)buf, size, &error);
+    g_assert_no_error(error);
+
+    gchar* file_contents;
+    gsize file_size;
+    g_file_get_contents(name, &file_contents, &file_size, &error);
+    g_assert_no_error(error);
+
+    g_assert_cmpmem(file_contents, file_size, buf, size);
+
+    unlink(name);
+
+    g_free(name);
+    g_free(file_contents);
+}
+
 static void _test_mmap_file() {
     /* Get a file that we can mmap and write into. */
     FILE* temp = NULL;
@@ -51,17 +76,26 @@ static void _test_mmap_file() {
     assert_nonneg_errno(fclose(temp));
 }
 
-static void _test_mmap_anon() {
-    unsigned char* buf;
-    // Don't worry about the system page size, but make it big enough to span multiple pages.
-    size_t initial_size = 100 * 1024;
-    assert_true_errno((buf = mmap(NULL, initial_size, PROT_READ | PROT_WRITE,
-                                  MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)) != MAP_FAILED);
+static size_t page_size() { return sysconf(_SC_PAGE_SIZE); }
 
-    // Set each byte to its KB offset.
-    for (int i = 0; i < initial_size; ++i) {
-        buf[i] = i / 1024;
+static void init_buf(unsigned char* buf, size_t size) {
+    for (int i = 0; i < size; ++i) {
+        buf[i] = i / page_size();
     }
+}
+
+static unsigned char* mmap_and_init_buf(size_t size) {
+    unsigned char* buf;
+    assert_true_errno((buf = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS,
+                                  -1, 0)) != MAP_FAILED);
+    init_buf(buf, size);
+    return buf;
+}
+
+static void _test_mmap_anon() {
+    size_t initial_size = 2 * page_size();
+    unsigned char* buf = mmap_and_init_buf(initial_size);
+    _validate_shadow_access(buf, initial_size);
 
     // Grow the buffer.
     size_t grown_size = 2 * initial_size;
@@ -71,15 +105,22 @@ static void _test_mmap_anon() {
                           MAP_FAILED);
     }
 
-    // Fill the new portion of the buffer.
-    for (int i = initial_size; i < grown_size; ++i) {
-        buf[i] = i / 1024;
+    // Validate that initial contents are still there.
+    for (int i = 0; i < initial_size; ++i) {
+        g_assert_cmpint(buf[i], ==, i / page_size());
     }
+
+    _validate_shadow_access(buf, initial_size);
+
+    // Fill the new portion of the buffer.
+    init_buf(buf, grown_size);
 
     // Validate the whole contents of the buffer.
     for (int i = 0; i < grown_size; ++i) {
-        g_assert_cmpint(buf[i], ==, i / 1024);
+        g_assert_cmpint(buf[i], ==, i / page_size());
     }
+
+    _validate_shadow_access(buf, grown_size);
 
     // Shrink the buffer.
     size_t shrunk_size = initial_size / 2;
@@ -92,14 +133,55 @@ static void _test_mmap_anon() {
 
     // Validate the whole contents of the buffer.
     for (int i = 0; i < shrunk_size; ++i) {
-        g_assert_cmpint(buf[i], ==, i / 1024);
+        g_assert_cmpint(buf[i], ==, i / page_size());
     }
+
+    _validate_shadow_access(buf, shrunk_size);
+}
+
+static void _test_mremap_clobber() {
+    unsigned char* bigbuf = mmap_and_init_buf(3 * page_size());
+
+    unsigned char* smallbuf = mmap_and_init_buf(page_size());
+
+    // mremap smallbuf into the middle of bigbuf, clobbering it.
+    {
+        unsigned char* requested_new_address = &bigbuf[page_size()];
+        unsigned char* actual_new_address;
+
+        assert_true_errno((actual_new_address = mremap(smallbuf, page_size(), page_size(),
+                                                       MREMAP_MAYMOVE | MREMAP_FIXED,
+                                                       requested_new_address)) != MAP_FAILED);
+        g_assert_cmpint((int64_t)actual_new_address, ==, (int64_t)requested_new_address);
+        smallbuf = actual_new_address;
+    }
+    // First page of bigbuf should be untouched.
+    for (int i = 0; i < page_size(); ++i) {
+        g_assert_cmpint(bigbuf[i], ==, 0);
+    }
+    _validate_shadow_access(&bigbuf[0], page_size());
+
+    // Next page should have been overwritten by smallbuf
+    for (int i = page_size(); i < (2 * page_size()); ++i) {
+        g_assert_cmpint(bigbuf[i], ==, 0);
+    }
+    _validate_shadow_access(&bigbuf[page_size()], page_size());
+
+    // Last page should be untouched.
+    for (int i = (2 * page_size()); i < (3 * page_size()); ++i) {
+        g_assert_cmpint(bigbuf[i], ==, 2);
+    }
+    _validate_shadow_access(&bigbuf[2 * page_size()], page_size());
+
+    // Validate Shadow access of the whole buffer (which crosses mmap'd regions) at once.
+    _validate_shadow_access(bigbuf, 3 * page_size());
 }
 
 int main(int argc, char* argv[]) {
     g_test_init(&argc, &argv, NULL);
     g_test_add_func("/memory/mmap_file", _test_mmap_file);
     g_test_add_func("/memory/mmap_anon", _test_mmap_anon);
+    g_test_add_func("/memory/mremap_clobber", _test_mremap_clobber);
     g_test_run();
     return 0;
 }
