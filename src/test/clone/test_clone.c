802c3fba89f3b8149b4ee472ba6489d74034010e
Jim Newsome <jnewsome@torproject.org>
2020-08-28 12:42:44 -0500

    Add test for a child thread exiting after the leader

--- src/test/clone/test_clone.c
+++ src/test/clone/test_clone.c
@@ -19,11 +19,19 @@
 
 #define CLONE_TEST_STACK_NBYTES 4096
 
-static volatile int _clone_test_acc = 0;
+#define CLONE_FLAGS                                                                                \
+    (CLONE_VM         /* Share process memory */                                                   \
+     | CLONE_FS       /* Share file attributes */                                                  \
+     | CLONE_FILES    /* Share open files */                                                       \
+     | CLONE_SIGHAND  /* Share signal dispositions */                                              \
+     | CLONE_THREAD   /* Share thread-group */                                                     \
+     | CLONE_SYSVSEM) /* Share semaphore values */
+
+static volatile int _clone_minimal_acc = 0;
 
 // _clone_testCloneStandardFlags calls this upon cloning
-static int _inc_clone_test_acc(void* args) {
-    ++_clone_test_acc;
+static int _clone_minimal_thread(void* args) {
+    ++_clone_minimal_acc;
     return 0;
 }
 
@@ -36,16 +44,7 @@ static void _clone_minimal() {
     // Head, shoulders, knees, and toes...
     uint8_t* stack_top = stack + CLONE_TEST_STACK_NBYTES;
 
-    // Some of the standard flags used in a pthread_create() call:
-    int flags = 0;
-    flags |= CLONE_VM;      // Share process memory
-    flags |= CLONE_FS;      // Share file attributes
-    flags |= CLONE_FILES;   // Share open files
-    flags |= CLONE_SIGHAND; // Share signal dispositions
-    flags |= CLONE_THREAD;  // Share thread-group
-    flags |= CLONE_SYSVSEM; // Share semaphore values
-
-    int child_tid = clone(_inc_clone_test_acc, stack_top, flags, NULL, NULL, NULL);
+    int child_tid = clone(_clone_minimal_thread, stack_top, CLONE_FLAGS, NULL, NULL, NULL);
     g_assert_cmpint(child_tid, >, 0);
 
     // The conventional way to wait for a child is futex, but we don't want this
@@ -55,10 +54,49 @@ static void _clone_minimal() {
     // *this process's parent*, not this process. We might be able to work around
     // this by forking first so that we can wait in the parent of the threaded process
     // (using __WCLONE), but we don't want this test rely on fork, either.
-    while (!_clone_test_acc) {
+    while (!_clone_minimal_acc) {
         usleep(1);
     }
-    g_assert_cmpint(_clone_test_acc, ==, 1);
+    g_assert_cmpint(_clone_minimal_acc, ==, 1);
+
+    free(stack);
+}
+
+static volatile int _clone_child_exits_after_leader_acc = 0;
+
+static int _clone_child_exits_after_leader_thread(void* args) {
+    ++_clone_child_exits_after_leader_acc;
+    // Racy when executed natively (but test will still pass). In Shadow this
+    // should deterministically ensure that this thread exits after the leader
+    // thread.
+    usleep(100);
+    return 0;
+}
+
+static void _clone_child_exits_after_leader() {
+    // allocate some memory for the cloned thread.
+    uint8_t* stack = calloc(CLONE_TEST_STACK_NBYTES, 1);
+
+    // Heap grows up,
+    // Stack grows down,
+    // Head, shoulders, knees, and toes...
+    uint8_t* stack_top = stack + CLONE_TEST_STACK_NBYTES;
+
+    int child_tid =
+        clone(_clone_child_exits_after_leader_thread, stack_top, CLONE_FLAGS, NULL, NULL, NULL);
+    g_assert_cmpint(child_tid, >, 0);
+
+    // The conventional way to wait for a child is futex, but we don't want this
+    // test to rely on it.
+    //
+    // We can't use `wait` etc, because the child "thread" process's parent is
+    // *this process's parent*, not this process. We might be able to work around
+    // this by forking first so that we can wait in the parent of the threaded process
+    // (using __WCLONE), but we don't want this test rely on fork, either.
+    while (!_clone_child_exits_after_leader_acc) {
+        usleep(1);
+    }
+    g_assert_cmpint(_clone_child_exits_after_leader_acc, ==, 1);
 
     free(stack);
 }
@@ -67,6 +105,13 @@ int main(int argc, char** argv) {
     g_test_init(&argc, &argv, NULL);
 
     g_test_add("/clone/clone_minimal", void, NULL, NULL, _clone_minimal, NULL);
+    // FIXME: currently hangs under shadow. The exit_group sycall is executed
+    // natively from the thread leader, which does cause all threads to exit.
+    // In Shadow, however, the corresponding Shadow thread remains blocked by
+    // its nanosleep syscall. We probably want to add a handler for the
+    // exit_group syscall to wake-up/terminate the other threads in the
+    // process.
+    //g_test_add("/clone/clone_child_exits_after_leader", void, NULL, NULL, _clone_child_exits_after_leader, NULL);
 
     return g_test_run();
 }
