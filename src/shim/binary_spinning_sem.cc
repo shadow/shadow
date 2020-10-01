f1ab2439328687a5633afe13fd03d406c7089bcc
Jim Newsome <jnewsome@torproject.org>
2020-10-01 09:28:31 -0500

    Undo massive spin-max. Now it's fast!

--- src/shim/binary_spinning_sem.cc
+++ src/shim/binary_spinning_sem.cc
@@ -8,7 +8,8 @@
 // (rwails) The number times we should increment the counter and
 // check the atomic bool before we fall back to the semaphore.
 // TODO: Move to an environment variable?
-#define SHD_GATE_SPIN_MAX 8096000
+// FIXME: in hybrid mode, handle if this ends up blocking 
+#define SHD_GATE_SPIN_MAX 8096
 
 void BinarySpinningSem::init() {
     _x.store(false);
