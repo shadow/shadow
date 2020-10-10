a47caa108acbc46c74db1a26af0a55cca504211c
Vuillemot Florian <florian.vuillemot@posteo.net>
2020-10-10 19:20:41 +0200

    Add test on pid

--- src/test/unistd/test_unistd.rs
+++ src/test/unistd/test_unistd.rs
@@ -3,7 +3,19 @@
  * See LICENSE for licensing information
  */
 
+use std::process;
 
-fn main() -> Result<(), String> {
-    Ok(())
+
+fn main() {
+    test_getpid_nodeps()
 }
+
+
+// Tests that the results are plausible, but can't really validate that it's our
+// pid without depending on other functionality.
+fn test_getpid_nodeps() {
+    let pid = process::id();
+    assert!(pid > 0);
+    assert_eq!(pid, process::id());
+}
+
