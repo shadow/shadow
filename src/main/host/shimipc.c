dc3ca80c1be85f1340a3f0202207aa312445b3f6
Jim Newsome <jnewsome@torproject.org>
2020-11-20 16:16:16 -0600

    Set higher spin max

--- src/main/host/shimipc.c
+++ src/main/host/shimipc.c
@@ -17,10 +17,19 @@ OPTION_EXPERIMENTAL_ENTRY(
 
 bool shimipc_sendExplicitBlockMessageEnabled() { return _sendExplicitBlockMessage; }
 
-static gint _spinMax = 8096;
+// Setting this too low will cause us to block too quickly, resulting in extra
+// overhead from context switching.
+//
+// In principle setting this too high could result in hogging CPU cores when
+// it'd be better to give them up.  However since Shadow explicitly tells
+// plugins to stop spinning as soon as they make a blocking syscall, this is
+// unlikely to happen. One scenario in which it *could* happen is if a plugin
+// goes a long time without making any syscalls e.g. doing some very long
+// pure-CPU-operation.
+static gint _spinMax = 1000000000;
 OPTION_EXPERIMENTAL_ENTRY("preload-spin-max", 0, 0, G_OPTION_ARG_INT, &_spinMax,
                           "Max number of iterations to busy-wait on ICP sempahore before blocking. "
-                          "-1 for unlimited. [8096]",
+                          "-1 for unlimited. [1000000000]",
                           "N")
 
 ssize_t shimipc_spinMax() { return _spinMax; }
