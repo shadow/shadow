fedf0c6f0c85a1e135c9c5ee17f7cc65faa799d9
hjkim17 <ouiqdmw17@gmail.com>
2020-11-23 15:48:10 +0900

    start to add structures to new bitcoin dat sharing

--- src/main/bleep_addon/bitcoin_storage_share/storage_share.cpp
+++ src/main/bleep_addon/bitcoin_storage_share/storage_share.cpp
@@ -1,19 +1,37 @@
 #include "storage_share.h"
 
 #include <vector>
+#include <unordered_map>
 
 class blockdat_hash_entry {
 
 };
 
-
-
 std::vector<blockdat_hash_entry> blockdat_tbl_head;
 
-#include <unordered_set>
+void AddHashData(int fileno, char* actual_path, char* lastBlockHash){
 
-/* bitcoin coinflip validation */
-std::unordered_set<std::string> _bitcoin_coinflip_validation_table;
+    // list entry 생성
+    Hashlist* elem = (Hashlist*)malloc(sizeof(Hashlist));
+    elem->fileno = fileno;
+    elem->actual_path = actual_path;
+    elem->lastBlockHashMerkleRoot = (char*)malloc(sizeof(char)*32);
+    memcpy(elem->lastBlockHashMerkleRoot, lastBlockHash, 32);
+    elem->refCnt=0;
+
+    // put elem to list header
+    Hashlist* cursor = hashtable->ents[fileno].list;
+    hashtable->ents[fileno].list = elem;
+    elem->next = cursor;
+    elem->prev = NULL;
+    if (cursor)
+        cursor->prev = elem;
+
+    hashtable->ents[fileno].listcnt++;
+}
+
+
+
 
 extern "C"
 {
