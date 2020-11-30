193cdf9ec1d36f7451e7ec311cc8ea30d23988ac
hjkim17 <ouiqdmw17@gmail.com>
2020-11-30 17:58:01 +0900

    use mutex to make safe unordered_set usage

--- src/main/bleep_addon/bleep_addon.cpp
+++ src/main/bleep_addon/bleep_addon.cpp
@@ -1,15 +1,19 @@
 #include "bleep_addon.h"
 
 #include <unordered_set>
+#include <mutex>
 
 /* bitcoin coinflip validation */
 std::unordered_set<std::string> _bitcoin_coinflip_validation_table;
+std::mutex _bitcoin_coinflip_validation_table_m;
 
 extern "C"
 {
 
 void shadow_bitcoin_register_hash(const char hash[]) {
+    _bitcoin_coinflip_validation_table_m.lock();
     _bitcoin_coinflip_validation_table.insert(hash);
+    _bitcoin_coinflip_validation_table_m.unlock();
     return;
 }
 int shadow_bitcoin_check_hash(const char hash[]) {
