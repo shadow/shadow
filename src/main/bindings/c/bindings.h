5c3fd5d896305cd5e15e371723fd855d817235b5
Steven Engler <opara@cs.georgetown.edu>
2020-12-01 12:20:46 -0500

    Move opaque types in C bindings into their own header file
    This helps to prevent circular dependencies between C headers.

--- src/main/bindings/c/bindings.h
+++ src/main/bindings/c/bindings.h
@@ -14,28 +14,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include "main/bindings/c/bindings-opaque.h"
 #include "main/host/syscall_types.h"
 #include "main/host/thread.h"
-
-// A queue of byte chunks.
-typedef struct ByteQueue ByteQueue;
-
-// Manages the address-space for a plugin process.
-//
-// The MemoryManager's primary purpose is to make plugin process's memory directly accessible to
-// Shadow. It does this by tracking what regions of program memory in the plugin are mapped to
-// what (analagous to /proc/<pid>/maps), and *remapping* parts of the plugin's address space into
-// a shared memory-file, which is also mapped into Shadow.
-//
-// Shadow provides several methods for allowing Shadow to access the plugin's memory, such as
-// `get_readable_ptr`. If the corresponding region of plugin memory is mapped into the shared
-// memory file, the corresponding Shadow pointer is returned. If not, then, it'll fall back to
-// (generally slower) Thread APIs.
-//
-// For the MemoryManager to maintain consistent state, and to remap regions of memory it knows how
-// to remap, Shadow must delegate handling of mman-related syscalls (such as `mmap`) to the
-// MemoryManager via its `handle_*` methods.
-typedef struct MemoryManager MemoryManager;
 
 void rust_logging_init(void);
 
