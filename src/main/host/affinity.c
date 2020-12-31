fc0c77caf752ee1da761a85f9a74d193adc9d85d
Ryan Wails <ryanswails@gmail.com>
2020-12-31 14:33:54 -0500

    Cleaning up setProcessAffinity routine, adding command line argument

--- src/main/host/affinity.c
+++ src/main/host/affinity.c
@@ -5,65 +5,70 @@
 #endif              // _GNU_SOURCE
 
 #include <assert.h>
+#include <glib.h>
 #include <sched.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <sys/sysinfo.h>
 
+#include "main/core/support/options.h"
 #include "support/logger/logger.h"
 
 // Compute once and cache this value. Apparently fetching is expensive and
 // requires parsing procfs.
-static int _NCPUS = -1; // Starts uninitialized.
+static int _ncpus = -1; // Starts uninitialized.
 
-__attribute__((constructor)) static void _init_ncpus() { _NCPUS = get_nprocs(); }
+static gint _affinity_enabled = 0;
+
+OPTION_EXPERIMENTAL_ENTRY(
+    "cpu-pin", 0, 0, G_OPTION_ARG_INT, &_affinity_enabled,
+    "Pin workers to fixed CPU cores, pin plugins to the worker's core", "[0|1]")
+
+__attribute__((constructor)) static void _init_ncpus() { _ncpus = get_nprocs(); }
 
 int affinity_getGoodWorkerAffinity(guint worker_thread_id) {
-    assert(_NCPUS > 0);                    // Our host better have at least one CPU.
-    return (int)worker_thread_id % _NCPUS; // Uniform distribution of workers over CPUs
+    assert(_ncpus > 0);                    // Our host better have at least one CPU.
+    return (int)worker_thread_id % _ncpus; // Uniform distribution of workers over CPUs
 }
 
 int affinity_setProcessAffinity(pid_t pid, int old_cpu_num, int new_cpu_num) {
+    assert(_ncpus > 0 && pid >= 0);
 
-    return old_cpu_num;
-
-    if (new_cpu_num == AFFINITY_UNINIT) {
-      return old_cpu_num;
+    // We can short-circuit if there's no work to do.
+    if (!_affinity_enabled || new_cpu_num == AFFINITY_UNINIT || new_cpu_num == old_cpu_num) {
+        return old_cpu_num;
     }
 
     cpu_set_t* cpu_set = NULL;
+    bool set_affinity_suceeded = false;
+    int retval = new_cpu_num;
 
-    if (new_cpu_num >= _NCPUS) {
-        goto FAIL;
+    if (new_cpu_num < _ncpus) { // Good, we are trying to set to a valid CPU
+        cpu_set = CPU_ALLOC(_ncpus);
+
+        if (cpu_set) { // Good, the CPU set allocation succeeded
+
+            size_t cpu_set_size = CPU_ALLOC_SIZE(_ncpus);
+
+            // Clear the CPU set
+            CPU_ZERO_S(cpu_set_size, cpu_set);
+            // And then add the new_cpu_num as the only element of the set
+            CPU_SET_S(new_cpu_num, cpu_set_size, cpu_set);
+
+            int rc = sched_setaffinity(pid, cpu_set_size, cpu_set);
+
+            set_affinity_suceeded = (rc == 0);
+        }
     }
 
-    cpu_set = CPU_ALLOC(_NCPUS);
-
-    if (!cpu_set) {
-        goto FAIL;
-    }
-
-    size_t cpu_set_size = CPU_ALLOC_SIZE(_NCPUS);
-
-    // Clear the CPU set
-    CPU_ZERO_S(cpu_set_size, cpu_set);
-    // And then add the new_cpu_num as the only element of the set.
-    CPU_SET_S(new_cpu_num, cpu_set_size, cpu_set);
-
-    int rc = sched_setaffinity(pid, cpu_set_size, cpu_set);
-    if (rc == -1) {
-        goto FAIL;
+    if (!set_affinity_suceeded) {
+        warning("Could not set CPU affinity for PID %d to %d", (int)pid, new_cpu_num);
+        retval = old_cpu_num;
     }
 
     if (cpu_set) {
         CPU_FREE(cpu_set);
     }
 
-    return new_cpu_num;
-
-FAIL: // So sue me.
-    warning("Could not set CPU affinity for PID %d to %d", (int)pid, new_cpu_num);
-    if (cpu_set) {
-        CPU_FREE(cpu_set);
-    }
-    return old_cpu_num;
+    return retval;
 }
