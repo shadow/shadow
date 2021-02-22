58b4699f2edd24e1e6b17a4238dc6e42435b8125
Steven Engler <opara@cs.georgetown.edu>
2021-02-22 10:23:59 -0500

    Default logger should flush the stderr, not stdout

--- src/support/logger/logger.c
+++ src/support/logger/logger.c
@@ -121,7 +121,7 @@ static void _logger_default_log(LogLevel level, const char* fileName, const char
 
 #ifdef DEBUG
     if (level == LOGLEVEL_ERROR) {
-        fflush(stdout);
+        fflush(stderr);
         abort();
     }
 #endif
