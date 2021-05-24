7a9a935192a494057f252351b03f99b2b118fbc2
Rob Jansen <jansen@cs.umn.edu>
2021-05-24 14:47:04 -0400

    Allow script to add useful labels to nodes and edges

--- src/tools/convert_legacy_topology.py
+++ src/tools/convert_legacy_topology.py
@@ -32,7 +32,7 @@ ATTR_CONVERSIONS = {
 }
 
 
-def convert_topology(xml_root, out_file):
+def convert_topology(xml_root, out_file, set_labels):
     graphml_ns = 'http://graphml.graphdrawing.org/xmlns'
     graphml_ns_prefix = '{' + graphml_ns + '}'
 
@@ -95,6 +95,16 @@ def convert_topology(xml_root, out_file):
         assert len(graph.graph['edge_default']) == 0
         del graph.graph['edge_default']
 
+    # Only change node and edge labels if the label option was given.
+    # Our custom labels help avoid cross-ref from an edge to a node via the node's
+    # numeric id, which can be a pain especially on large graphs.
+    if set_labels:
+        graph = nx.relabel_nodes(graph, lambda x: f"node at {graph.nodes[x]['ip_address']}")
+        for (source, target) in graph.edges:
+            src_ip = graph.nodes[source]['ip_address']
+            tgt_ip = graph.nodes[target]['ip_address']
+            graph[source][target]['label'] = f"path from {src_ip} to {tgt_ip}"
+
     # generate gml from the graph
     try:
         nx.write_gml(graph, out_file)
@@ -113,6 +123,8 @@ if __name__ == '__main__':
         description=('Convert Shadow topology files from the version 1.x format '
                      'to the 2.x format, and write the output to stdout'))
     parser.add_argument('filename', help='Filename to convert')
+    parser.add_argument('--set-labels', help='Add custom labels to nodes and edges',
+                        action="store_true", default=False, required=False)
     args = parser.parse_args()
 
     filename = args.filename
@@ -129,7 +141,7 @@ if __name__ == '__main__':
     # the option to 'use_shortest_path' in 'convert_legacy_config.py' without needing to
     # raise an error.
 
-    removed_graph_data = convert_topology(tree.getroot(), sys.stdout.buffer)
+    removed_graph_data = convert_topology(tree.getroot(), sys.stdout.buffer, args.set_labels)
 
     for x in removed_graph_data:
         print('Removed the deprecated graph attribute \'{}\': {}'.format(
