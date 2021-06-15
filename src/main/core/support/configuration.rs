569d54d500647d077ad0e5446ea28089488cc7e2
Jim Newsome <jnewsome@torproject.org>
2021-06-14 21:14:22 -0500

    configuration: use quantity when calculating number of hosts
    When auto-configuring the number of worker threads to create, we were
    counting the number of host *entries* instead of the number of hosts.
    Each entry has a *quantity* field that we must account for.

--- src/main/core/support/configuration.rs
+++ src/main/core/support/configuration.rs
@@ -8,7 +8,6 @@ use merge::Merge;
 use once_cell::sync::Lazy;
 use schemars::{schema_for, JsonSchema};
 use serde::{Deserialize, Serialize};
-use std::convert::TryInto;
 use std::num::NonZeroU32;
 
 use super::simulation_time::{SIMTIME_ONE_NANOSECOND, SIMTIME_ONE_SECOND};
@@ -1202,7 +1201,7 @@ mod export {
             Some(w) => *w,
             None => {
                 // By default use 1 worker per host.
-                NonZeroU32::new(config.hosts.len().try_into().unwrap()).unwrap()
+                NonZeroU32::new(config_getNHosts(config)).unwrap()
             }
         }
     }
