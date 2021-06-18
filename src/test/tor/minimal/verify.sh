55864d62ac28d53616e133734e52fa6c8575d4fd
Rob Jansen <jansen@cs.umn.edu>
2021-06-18 11:06:28 -0400

    Clarify Tor test verification result

--- src/test/tor/minimal/verify.sh
+++ src/test/tor/minimal/verify.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 bootstrapped_count="$(grep -r --include="*.tor.*.stdout" "Bootstrapped 100" | wc -l)"
-echo "Bootstrapped count: ${bootstrapped_count}"
+printf "Bootstrapped count: ${bootstrapped_count}/9\n"
 
 if [ "${bootstrapped_count}" != "9" ]; then
-    echo "Not all tor processes bootstrapped"
+    printf "Verification ${RED}failed${NC}: Not all tor processes bootstrapped :(\n"
     exit 1
 fi
 
 stream_count="$(grep -r --include="*.tgen.*.stdout" "stream-success" | wc -l)"
-echo "Successful tgen stream count: ${stream_count}"
+printf "Successful tgen stream count: ${stream_count}/80\n"
 
 if [ "${stream_count}" != "80" ]; then
-    echo "Not all tgen streams were successful"
+    printf "Verification ${RED}failed${NC}: Not all tgen streams were successful :(\n"
     exit 1
 fi
+
+printf "Verification ${GREEN}suceeded${NC}: Yay :)\n"
