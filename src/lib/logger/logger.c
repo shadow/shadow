shadow/shadow

commit d167ccd6ee39bc7bdbe9e0d7020f79ab08aeaf6f

logger: log everything by default
Most logging frameworks log little/nothing unless explicitly enabled.
That probably makes sense for a framework used across independent
libraries and apps, but in our case verbose is a useful default,
particularly in test programs.

src/lib/logger/logger.c
@@ -152,11 +152,11 @@ void logger_setLevel(Logger* logger, LogLevel level) {
 }
 
 bool logger_isEnabled(Logger* logger, LogLevel level) {
-    if (!logger) {
-        return level > LOGLEVEL_WARNING;
-    } else {
-        return logger->isEnabled(logger, level);
-    }
+    // Most logging frameworks log little/nothing unless explicitly enabled.
+    // That probably makes sense for a framework used across independent
+    // libraries and apps, but in our case verbose is a useful default,
+    // particularly in test programs.
+    return true;
 }
 
 void logger_flush(Logger* logger) {

commit bad931cf7d88c2e1ac996a49c9c7170ee7589dd3

logger bugfix: Enforce log levels
Fixes bug introduced in d167ccd6ee39bc7bdbe9e0d7020f79ab08aeaf6f

src/lib/logger/logger.c
@@ -152,11 +152,15 @@ void logger_setLevel(Logger* logger, LogLevel level) {
 }
 
 bool logger_isEnabled(Logger* logger, LogLevel level) {
-    // Most logging frameworks log little/nothing unless explicitly enabled.
-    // That probably makes sense for a framework used across independent
-    // libraries and apps, but in our case verbose is a useful default,
-    // particularly in test programs.
-    return true;
+    if (!logger) {
+        // Most logging frameworks log little/nothing unless explicitly enabled.
+        // That probably makes sense for a framework used across independent
+        // libraries and apps, but in our case verbose is a useful default,
+        // particularly in test programs.
+        return true;
+    } else {
+        return logger->isEnabled(logger, level);
+    }
 }
 
 void logger_flush(Logger* logger) {