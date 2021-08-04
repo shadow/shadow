f9063764089e0b9e5e18accfa1bf262ed41880d4
Jim Newsome <jnewsome@torproject.org>
2021-08-04 10:25:22 -0500

    Always make 'shadow' syscalls directly
    In particular avoid logging while trying to disable interposition so
    that we can log.

--- src/lib/shim/shim_event.c
+++ src/lib/shim/shim_event.c
@@ -1,23 +1,25 @@
 #include "lib/shim/shim_event.h"
+
+#include "lib/shim/preload_syscall.h"
 #include "main/host/syscall_numbers.h"
 
 #include <unistd.h>
 
 int shadow_set_ptrace_allow_native_syscalls(bool val) {
-    return syscall(SYS_shadow_set_ptrace_allow_native_syscalls, val);
+    return shadow_real_raw_syscall(SYS_shadow_set_ptrace_allow_native_syscalls, val);
 }
 
 int shadow_get_ipc_blk(ShMemBlockSerialized* ipc_blk_serialized) {
-    return syscall(SYS_shadow_get_ipc_blk, ipc_blk_serialized);
+    return shadow_real_raw_syscall(SYS_shadow_get_ipc_blk, ipc_blk_serialized);
 }
 
 int shadow_get_shm_blk(ShMemBlockSerialized* shm_blk_serialized) {
-    return syscall(SYS_shadow_get_shm_blk, shm_blk_serialized);
+    return shadow_real_raw_syscall(SYS_shadow_get_shm_blk, shm_blk_serialized);
 }
 
 int shadow_hostname_to_addr_ipv4(const char* name, size_t name_len, uint32_t* addr,
                                  size_t addr_len) {
-    return syscall(SYS_shadow_hostname_to_addr_ipv4, name, name_len, addr, addr_len);
+    return shadow_real_raw_syscall(SYS_shadow_hostname_to_addr_ipv4, name, name_len, addr, addr_len);
 }
 
 static inline void shim_determinedSend(int sock_fd, const void* ptr,
