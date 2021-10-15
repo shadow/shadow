9f0363bb13be2e25ef332cc0c66932c791c21e71
Jim Newsome <jnewsome@torproject.org>
2021-10-15 11:55:29 -0500

    rdtsc signal handler: use thread-local to detect recursion

--- src/lib/shim/shim_rdtsc.c
+++ src/lib/shim/shim_rdtsc.c
@@ -15,31 +15,41 @@
 #include "lib/shim/shim_logger.h"
 #include "lib/shim/shim_sys.h"
 #include "lib/shim/shim_syscall.h"
+#include "lib/shim/shim_tls.h"
 #include "lib/tsc/tsc.h"
 
-static void _shim_rdtsc_handle_sigsegv(int sig, siginfo_t* info, void* voidUcontext);
-
-static void _shim_rdtsc_install_oneshot_sigsegv_handler() {
-    if (sigaction(SIGSEGV,
-                  &(struct sigaction){
-                      .sa_sigaction = _shim_rdtsc_handle_sigsegv,
-                      // SA_NODEFER: Handle recursive SIGSEGVs, so that it can
-                      // "rethrow" the SIGSEGV and in case of a bug in the
-                      // handler.
-                      // SA_RESETHAND: Restore the default action before
-                      // invoking the handler, so that a SIGSEGV in the handler
-                      // itself crashes instead of recursing.
-                      // SA_SIGINFO: Required because we're specifying
-                      // sa_sigaction.
-                      .sa_flags = SA_SIGINFO | SA_NODEFER | SA_RESETHAND,
-                  },
-                  NULL) < 0) {
+_Noreturn static void _shim_rdtsc_die_with_sigsegv() {
+    shim_disableInterposition();
+    if (sigaction(SIGSEGV, &(struct sigaction){.sa_handler = SIG_DFL}, NULL) != 0) {
         panic("sigaction: %s", strerror(errno));
     }
+    raise(SIGSEGV);
+    panic("Unexpectedly survived SIGSEGV");
 }
 
 static void _shim_rdtsc_handle_sigsegv(int sig, siginfo_t* info, void* voidUcontext) {
     shim_disableInterposition();
+
+    // Detect recursion. Unfortunately there's still potential for infinite
+    // recursion here if there's a SEGV in this handling itself. We could avoid
+    // that by using SA_RESETHAND with `sigaction` to have the system reset the
+    // signal disposition every time this handler is invoked, but that doesn't
+    // work well with threads when `CLONE_SIGHAND` is in use. OTOH the infinite
+    // recursion should still ultimately result in a SIGSEGV - just with an
+    // annoyingly large stacktrace.
+    //
+    // Maybe something to revisit when we fully virtualize and implement signal
+    // handling; e.g. we could always clear CLONE_SIGHAND in `clone` calls, so
+    // that we can control signal dispositions independently for each thread
+    // (while emulating the CLONE_SIGHAND behavior for the managed process
+    // itself).
+    static ShimTlsVar in_handler_storage = {0};
+    bool* in_handler = shimtlsvar_ptr(&in_handler_storage, sizeof(*in_handler));
+    if (*in_handler) {
+        error("Recursive sigsegv");
+        _shim_rdtsc_die_with_sigsegv();
+    }
+    *in_handler = true;
 
     trace("Trapped sigsegv");
     static bool tsc_initd = false;
@@ -77,10 +87,9 @@ static void _shim_rdtsc_handle_sigsegv(int sig, siginfo_t* info, void* voidUcont
         regs[REG_RIP] = rip;
     } else {
         error("Unhandled sigsegv");
-        raise(SIGSEGV);
-        panic("Unexpectedly survived SIGSEGV");
+        _shim_rdtsc_die_with_sigsegv();
     }
-    _shim_rdtsc_install_oneshot_sigsegv_handler();
+    *in_handler = false;
     shim_enableInterposition();
 }
 
@@ -91,5 +100,17 @@ void shim_rdtsc_init() {
     }
 
     // Install our own handler to emulate.
-    _shim_rdtsc_install_oneshot_sigsegv_handler();
+    if (sigaction(SIGSEGV,
+                  &(struct sigaction){
+                      .sa_sigaction = _shim_rdtsc_handle_sigsegv,
+                      // SA_NODEFER: Handle recursive SIGSEGVs, so that it can
+                      // "rethrow" the SIGSEGV and in case of a bug in the
+                      // handler.
+                      // SA_SIGINFO: Required because we're specifying
+                      // sa_sigaction.
+                      .sa_flags = SA_SIGINFO | SA_NODEFER,
+                  },
+                  NULL) < 0) {
+        panic("sigaction: %s", strerror(errno));
+    }
 }
