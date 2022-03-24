dba4a1dd9437e037daf45c164068c00b139a6909
Steven Engler <opara@cs.georgetown.edu>
2022-03-24 14:35:56 -0400

    Get the `SysCallCondition` from the rust `Thread`

--- src/main/host/syscall_condition.rs
+++ src/main/host/syscall_condition.rs
@@ -1,19 +1,74 @@
 use crate::cshadow;
 use crate::host::syscall::Trigger;
 
-/// Wrapper
+use std::marker::PhantomData;
+
+/// An immutable reference to a syscall condition.
+#[derive(Debug, PartialEq, Eq)]
+pub struct SysCallConditionRef<'a> {
+    c_ptr: *mut cshadow::SysCallCondition,
+    _phantom: PhantomData<&'a ()>,
+}
+
+// do not define any mutable methods for this type
+impl<'a> SysCallConditionRef<'a> {
+    /// Borrows from a C pointer. i.e. doesn't increase the ref count, nor decrease the ref count
+    /// when dropped.
+    /// `ptr` must point to a valid object that will not be accessed by other threads
+    /// for the lifetime of this object.
+    pub fn borrow_from_c(ptr: *mut cshadow::SysCallCondition) -> Self {
+        assert!(!ptr.is_null());
+        Self {
+            c_ptr: ptr,
+            _phantom: PhantomData::default(),
+        }
+    }
+}
+
+/// A mutable reference to a syscall condition.
+#[derive(Debug, PartialEq, Eq)]
+pub struct SysCallConditionRefMut<'a> {
+    condition: SysCallConditionRef<'a>,
+}
+
+// any immutable methods should be implemented on SysCallConditionRef instead
+impl<'a> SysCallConditionRefMut<'a> {
+    /// Borrows from a C pointer. i.e. doesn't increase the ref count, nor decrease the ref count
+    /// when dropped.
+    /// `ptr` must point to a valid object that will not be accessed by other threads
+    /// for the lifetime of this object.
+    pub fn borrow_from_c(ptr: *mut cshadow::SysCallCondition) -> Self {
+        assert!(!ptr.is_null());
+        Self {
+            condition: SysCallConditionRef::borrow_from_c(ptr),
+        }
+    }
+}
+
+impl<'a> std::ops::Deref for SysCallConditionRefMut<'a> {
+    type Target = SysCallConditionRef<'a>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.condition
+    }
+}
+
+/// An owned syscall condition.
 #[derive(Debug, PartialEq, Eq)]
 pub struct SysCallCondition {
-    c_ptr: *mut cshadow::SysCallCondition,
+    condition: Option<SysCallConditionRefMut<'static>>,
 }
 
 impl SysCallCondition {
-    /// "Steal" from a C pointer. i.e. doesn't increase ref count.
+    /// "Steal" from a C pointer. i.e. doesn't increase ref count, but will decrease the ref count
+    /// when dropped.
     /// `ptr` must point to a valid object that will not be accessed by other threads
     /// for the lifetime of this object.
     pub unsafe fn consume_from_c(ptr: *mut cshadow::SysCallCondition) -> Self {
         assert!(!ptr.is_null());
-        Self { c_ptr: ptr }
+        Self {
+            condition: Some(SysCallConditionRefMut::borrow_from_c(ptr)),
+        }
     }
 
     /// Constructor.
@@ -21,23 +76,39 @@ impl SysCallCondition {
     // implementation or wrapper.
     pub fn new(trigger: Trigger) -> Self {
         SysCallCondition {
-            c_ptr: unsafe { cshadow::syscallcondition_new(trigger.into()) },
+            condition: Some(SysCallConditionRefMut::borrow_from_c(unsafe {
+                cshadow::syscallcondition_new(trigger.into())
+            })),
         }
     }
 
     /// "Steal" the inner pointer without unref'ing it.
     pub fn into_inner(mut self) -> *mut cshadow::SysCallCondition {
-        let ptr = self.c_ptr;
-        // We *don't* want Drop to deref
-        self.c_ptr = std::ptr::null_mut() as *mut cshadow::SysCallCondition;
-        ptr
+        let condition = self.condition.take().unwrap();
+        condition.c_ptr
     }
 }
 
 impl Drop for SysCallCondition {
     fn drop(&mut self) {
-        if !self.c_ptr.is_null() {
-            unsafe { cshadow::syscallcondition_unref(self.c_ptr) }
+        if let Some(condition) = &self.condition {
+            if !condition.c_ptr.is_null() {
+                unsafe { cshadow::syscallcondition_unref(condition.c_ptr) }
+            }
         }
+    }
+}
+
+impl std::ops::Deref for SysCallCondition {
+    type Target = SysCallConditionRefMut<'static>;
+
+    fn deref(&self) -> &Self::Target {
+        self.condition.as_ref().unwrap()
+    }
+}
+
+impl std::ops::DerefMut for SysCallCondition {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.condition.as_mut().unwrap()
     }
 }
