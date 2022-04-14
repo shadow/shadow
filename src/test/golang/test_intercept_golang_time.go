3985202a72497609100dcf9a383f1e2fd8f9dc11
Jim Newsome <jnewsome@torproject.org>
2022-04-14 15:58:28 -0500

    Update tests to handle syscall latency being enabled by default

--- src/test/golang/test_intercept_golang_time.go
+++ src/test/golang/test_intercept_golang_time.go
@@ -6,12 +6,13 @@ import (
 )
 
 // Validate that time is being intercepted.
-// Won't pass natively.
+// Won't pass natively. (At least without resetting the system clock)
 func main() {
-	var t0 = time.Now()
-	var t1 = time.Now()
-
-	if t0 != t1 {
-		panic(fmt.Sprint(t0, " != ", t1))
+	var shadowSimStartTime = time.Date(2000, time.January, 1, 1, 0, 0, 0, time.UTC)
+	var now = time.Now()
+	var dt = now.Sub(shadowSimStartTime)
+	var maxDelta, _ = time.ParseDuration("5m")
+	if dt > maxDelta {
+		panic(fmt.Sprint("Time is now ", now, "; ", dt, " since sim start of ", shadowSimStartTime))
 	}
 }
