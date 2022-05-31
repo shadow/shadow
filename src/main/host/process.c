shadow/shadow

commit ba800b837fa0b77961a3907c7f6719fc05d11411

Distinguish between sources of death by signal 9
* Treat death by signal 9 as an error if Shadow didn't
  intentionally use it to stop the process.

* Write an empty exitcode file when Shadow does kill the process (via
  signal 9), which is a more intuitive result for a process that didn't
  exit within the simulation itself.

src/main/host/process.c
@@ -127,6 +127,7 @@ struct _Process {
 
     gint returnCode;
     gboolean didLogReturnCode;
+    gboolean killedByShadow;
 
     // int thread_id -> Thread*.
     GHashTable* threads;
@@ -315,9 +316,11 @@ static void _process_handleProcessExit(Process* proc) {
     _process_check(proc);
 }
 
-static void _process_terminate_threads(Process* proc) {
-    trace("Terminating threads");
+static void _process_terminate(Process* proc) {
+    trace("Terminating");
     if (process_isRunning(proc)) {
+        proc->killedByShadow = true;
+
         if (kill(proc->nativePid, SIGKILL)) {
             warning("kill(pid=%d) error %d: %s", proc->nativePid, errno, g_strerror(errno));
         }
@@ -374,16 +377,30 @@ static void _process_getAndLogReturnCode(Process* proc) {
 
     // don't change the formatting of this string since some integration tests depend on it
     GString* mainResultString = g_string_new(NULL);
-    g_string_printf(mainResultString, "main %s code '%i' for process '%s'",
-                    ((proc->returnCode == 0) ? "success" : "error"), proc->returnCode,
-                    process_getName(proc));
+    g_string_printf(mainResultString, "process '%s'", process_getName(proc));
+    if (proc->killedByShadow) {
+        g_string_append_printf(mainResultString, " killed by Shadow");
+    } else {
+        g_string_append_printf(mainResultString, " exited with code %d", proc->returnCode);
+        if (proc->returnCode == 0) {
+            g_string_append_printf(mainResultString, " (success)");
+        } else {
+            g_string_append_printf(mainResultString, " (error)");
+        }
+    }
 
     gchar* fileName = _process_outputFileName(proc, "exitcode");
     FILE* exitcodeFile = fopen(fileName, "we");
     g_free(fileName);
 
     if (exitcodeFile != NULL) {
-        fprintf(exitcodeFile, "%d", proc->returnCode);
+        if (proc->killedByShadow) {
+            // Process never died during the simulation; shadow chose to kill it;
+            // typically because the simulation end time was reached.
+            // Write out an empty exitcode file.
+        } else {
+            fprintf(exitcodeFile, "%d", proc->returnCode);
+        }
         fclose(exitcodeFile);
     } else {
         warning("Could not open '%s' for writing: %s", mainResultString->str, strerror(errno));
@@ -392,7 +409,7 @@ static void _process_getAndLogReturnCode(Process* proc) {
     // if there was no error or was intentionally killed
     // TODO: once we've implemented clean shutdown via SIGTERM,
     //       treat death by SIGKILL as a plugin error
-    if (proc->returnCode == 0 || proc->returnCode == return_code_for_signal(SIGKILL)) {
+    if (proc->returnCode == 0 || proc->killedByShadow) {
         info("%s", mainResultString->str);
     } else {
         warning("%s", mainResultString->str);
@@ -698,7 +715,7 @@ void process_stop(Process* proc) {
 #endif
 
     proc->plugin.isExecuting = TRUE;
-    _process_terminate_threads(proc);
+    _process_terminate(proc);
     proc->plugin.isExecuting = FALSE;
 
 #ifdef USE_PERF_TIMERS
@@ -879,7 +896,7 @@ static void _process_free(Process* proc) {
     process_freePtrsWithoutFlushing(proc);
     g_array_free(proc->memoryRefs, false);
 
-    _process_terminate_threads(proc);
+    _process_terminate(proc);
     if (proc->threads) {
         g_hash_table_destroy(proc->threads);
         proc->threads = NULL;

commit 6475eea3576a163ff03915448ffec82df255e779

shadow tests: check retval files instead of shadow log messages
I accidentally broke the regex in
ba800b837fa0b77961a3907c7f6719fc05d11411.

Checking the exitcode files should be less prone to getting broken.

src/main/host/process.c
@@ -390,7 +390,6 @@ static void _process_getAndLogReturnCode(Process* proc) {
         }
     }
 
-    // don't change the formatting of this string since some integration tests depend on it
     GString* mainResultString = g_string_new(NULL);
     g_string_printf(mainResultString, "process '%s'", process_getName(proc));
     if (proc->killedByShadow) {

src/test/CMakeLists.txt
@@ -126,6 +126,28 @@ macro(add_shadow_tests)
          set(POST_CMD "true")
       endif()
 
+      if(SHADOW_TEST_CHECK_RETVAL)
+         # Validate that all processes exited with code 0.  Processes that don't
+         # exit have an empty exitcode file, which will cause this check to
+         # fail.
+         # We *don't* fail in the case that nothing matched the glob, which is
+         # why we need to explicitly check whether the file exists.
+         set(CHECK_RETVAL_CMD "
+             for f in ${SHADOW_TEST_NAME}.data/hosts/*/*.exitcode
+             do
+               # Check if glob actually matched
+               if [ ! -f $f ]\; then continue\; fi
+               # Check if the file contains 0
+               if [ \"`cat $f`\" != 0 ]
+               then
+                 echo FAIL: $f isn\\'t 0
+                 exit 1
+               fi
+            done")
+      else()
+         set(CHECK_RETVAL_CMD "true")
+      endif()
+
       set(SHADOW_TEST_COMMAND sh -c "\
          rm -rf ${SHADOW_TEST_NAME}.data \
          && ${INVERT_ERROR_CODE} ${CMAKE_BINARY_DIR}/src/main/shadow \
@@ -134,6 +156,7 @@ macro(add_shadow_tests)
          --log-level=${SHADOW_TEST_LOGLEVEL} \
          ${SHADOW_TEST_ARGS} \
          ${SHADOW_TEST_SHADOW_CONFIG} \
+         && (${CHECK_RETVAL_CMD}) \
          && (${POST_CMD}) \
          "
       )
@@ -154,10 +177,6 @@ macro(add_shadow_tests)
       # check that we didn't leak any reference-counted objects
       set(FAIL_REGEX "Memory leak detected")
 
-      # need to check the test's return code, not just shadow's (see shadow/shadow#902)
-      if(SHADOW_TEST_CHECK_RETVAL)
-         list(APPEND FAIL_REGEX "main error code '.*' for process")
-      endif()
       set_property(TEST ${SHADOW_TEST_NAME} PROPERTY ENVIRONMENT "RUST_BACKTRACE=1;G_DEBUG=fatal-criticals")
 
       set_property(TEST ${SHADOW_TEST_NAME} PROPERTY FAIL_REGULAR_EXPRESSION ${FAIL_REGEX})

src/test/regression/CMakeLists.txt
@@ -19,6 +19,7 @@ add_shadow_tests(
   POST_CMD "test `cat hosts/*/*.exitcode` -eq 137"
   # Shadow should return failure
   EXPECT_ERROR TRUE
+  CHECK_RETVAL FALSE
   # ptrace doesn't support signals well enough
   SKIP_METHODS ptrace)
 