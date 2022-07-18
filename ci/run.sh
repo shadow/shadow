8411fd21d57efeeeb2dc485cd98f1e14ff27047a
Jim Newsome <jnewsome@torproject.org>
2022-07-18 18:33:50 -0500

    ci/run.sh: only run one configuration

--- ci/run.sh
+++ ci/run.sh
@@ -4,98 +4,50 @@
 
 set -euo pipefail
 
-BUILD_IMAGES=0
+BUILD_IMAGE=0
 PUSH=0
 NOCACHE=
 REPO=shadowsim/shadow-ci
 
-CONTAINERS=(
-    ubuntu:18.04
-    ubuntu:20.04
-    ubuntu:22.04
-    debian:10-slim
-    debian:11-slim
-    fedora:34
-    fedora:35
-    fedora:36
-    quay.io/centos/centos:stream8
-    )
-
-CCS=(
-    gcc
-    clang
-    )
-
-BUILDTYPES=(
-    debug
-    release
-    )
-
-EXTRAS=("ubuntu:18.04;clang;coverage")
-
-NOCACHE=
+CONTAINER=ubuntu:18.04
+CC=gcc
+BUILDTYPE=debug
 
 show_help () {
     cat <<EOF
 Usage: $0 ...
   -h
   -?             Show help
-  -i             Build images
-  -c CONTAINERS  Set containers used in test matrix
-  -C CCS         Set C compilers used in test matrix
-  -b BUILDTYPES  Set build-types used in test matrix
-  -e EXTRAS      Set extra configurations to run
-  -o             Set only configurations to run
+  -i             Build image
+  -c CONTAINER   Set container
+  -C CC          Set C compiler
+  -b BUILDTYPE   Set build-types
   -n             nocache when building Docker images
-  -p             pushes images to dockerhub if set
+  -p             push image to dockerhub
   -r             set Docker repository
 
-On the first run, you should use the '-i' flag to build the images.
+On the first run, you should use the '-i' flag to build the image.
 
-Run all default configurations:
+Run default configuration:
 
   $0
 
-Run all default configurations, but restrict C compilers to gcc:
-
-  $0 -C gcc
-
-Run all default configurations, but restrict C compilers to gcc,
-and containers to ubuntu:18.04 and fedora:35:
-
-  $0 -C gcc -c "ubuntu:18.04 fedora:35"
-
-Set "extra" configurations to ubuntu:18.04;clang;coverage
-and debian:11-slim;gcc;coverage
-
-  $0 -e "ubuntu:18.04;clang;coverage debian:11-slim;gcc;coverage"
-
-Set *only* configurations to run:
-
-  $0 -o "ubuntu:18.04;clang;coverage debian:11-slim;gcc;coverage"
 EOF
 }
 
-while getopts "h?ipc:C:b:e:o:nr:" opt; do
+while getopts "h?ipc:C:b:nr:" opt; do
     case "$opt" in
     h|\?)
         show_help
         exit 0
         ;;
-    i)  BUILD_IMAGES=1
+    i)  BUILD_IMAGE=1
         ;;
-    c)  read -ra CONTAINERS <<< "$OPTARG"
+    c)  CONTAINER="$OPTARG"
         ;;
-    C)  read -ra CCS <<< "$OPTARG"
+    C)  CC="$OPTARG"
         ;;
-    b)  read -ra BUILDTYPES <<< "$OPTARG"
-        ;;
-    e)  read -ra EXTRAS <<< "$OPTARG"
-        ;;
-    o)  CONTAINERS=()
-        CCS=()
-        BUILDTYPES=()
-        read -ra EXTRAS <<< "$OPTARG"
+    b)  BUILDTYPE="$OPTARG"
         ;;
     n)  NOCACHE=--no-cache
         ;;
@@ -106,10 +58,6 @@ while getopts "h?ipc:C:b:e:o:nr:" opt; do
 done
 
 run_one () {
-    CONTAINER="$1"
-    CC="$2"
-    BUILDTYPE="$3"
-
     # Replace the single ':' with a '-'
     CONTAINER_FOR_TAG=${CONTAINER/:/-}
     # Replace all forward slashes with '-'
@@ -117,7 +65,7 @@ run_one () {
 
     TAG="$REPO:$CONTAINER_FOR_TAG-$CC-$BUILDTYPE"
 
-    if [ "${BUILD_IMAGES}" == "1" ]; then
+    if [ "${BUILD_IMAGE}" == "1" ]; then
         echo "Building $TAG"
 
         # Build and tag a Docker image with the given configuration.
@@ -196,20 +144,4 @@ EOF
     fi
 }
 
-# Array indexing follows the syntax of:
-# https://stackoverflow.com/a/61551944
-# to handle potentially empty arrays
-
-for CONTAINER in ${CONTAINERS[@]+"${CONTAINERS[@]}"}; do
-for CC in ${CCS[@]+"${CCS[@]}"}; do
-for BUILDTYPE in ${BUILDTYPES[@]+"${BUILDTYPES[@]}"}; do
-    run_one "$CONTAINER" "$CC" "$BUILDTYPE"
-done
-done
-done
-
-for EXTRA in ${EXTRAS[@]+"${EXTRAS[@]}"}; do
-    # Split on ';'
-    IFS=';' read -ra args <<< "$EXTRA"
-    run_one "${args[0]}" "${args[1]}" "${args[2]}"
-done
+run_one
