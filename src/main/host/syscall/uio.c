41ceef3550ed05ea4a415777c2ce39a4599a3d97
Congyu Liu <liucy1998@outlook.com>
2022-08-25 17:45:05 +0000

    Fix incorrect length in uio
    Fix the length issue in #2379.

--- src/main/host/syscall/uio.c
+++ src/main/host/syscall/uio.c
@@ -166,12 +166,13 @@ _syscallhandler_readvHelper(SysCallHandler* sys, int fd, PluginPtr iovPtr,
             if (result > 0) {
                 totalBytesWritten += (size_t)result;
             } else {
-                if (result == -EWOULDBLOCK && totalBytesWritten > 0) {
-                    result = totalBytesWritten;
-                }
                 break;
             }
         }
+    }
+    
+    if (result >= 0 || (result == -EWOULDBLOCK && totalBytesWritten > 0)) {
+        result = totalBytesWritten;
     }
 
     if (result == -EWOULDBLOCK && !(legacyfile_getFlags(desc) & O_NONBLOCK)) {
@@ -278,11 +279,11 @@ _syscallhandler_writevHelper(SysCallHandler* sys, int fd, PluginPtr iovPtr,
             if (result > 0) {
                 totalBytesWritten += (size_t)result;
             } else {
-                if (result == -EWOULDBLOCK && totalBytesWritten > 0) {
-                    result = totalBytesWritten;
-                }
                 break;
             }
+        }
+        if (result >= 0 || (result == -EWOULDBLOCK && totalBytesWritten > 0)) {
+            result = totalBytesWritten;
         }
     }
 
