a7bc03a741ea8625a504edd1081612f1144c3917
Jim Newsome <jnewsome@torproject.org>
2022-11-14 16:08:31 -0600

    Tsc: move tests for Rust code to Rust

--- src/lib/tsc/tsc_test.c
+++ src/lib/tsc/tsc_test.c
@@ -21,35 +21,6 @@ static uint64_t _getEmulatedCycles(void (*emulate_fn)(const Tsc* tsc, uint64_t* 
     return (rdx << 32) | rax;
 }
 
-void emulateGivesExpectedCycles(void* unusedFixture, gconstpointer user_data) {
-    void (*emulate_fn)(
-        const Tsc* tsc, uint64_t* rax, uint64_t* rdx, uint64_t* rip, uint64_t nanos) = user_data;
-    const uint64_t cyclesPerSecondForOneGHz = 1000000000;
-
-    // Single ns granularity @ 1 GHz
-    g_assert_cmpint(_getEmulatedCycles(emulate_fn, cyclesPerSecondForOneGHz, 1), ==, 1);
-
-    // 1000x clock rate
-    g_assert_cmpint(_getEmulatedCycles(emulate_fn, 1000 * cyclesPerSecondForOneGHz, 1), ==, 1000);
-
-    // 1000x nanos
-    g_assert_cmpint(_getEmulatedCycles(emulate_fn, cyclesPerSecondForOneGHz, 1000), ==, 1000);
-
-    // Correct (no overflow) for 1 year @ 10 GHz
-    const uint64_t oneYearInSeconds = 1L     // years
-                                      * 365L // days
-                                      * 24L  // hours
-                                      * 60L  // minutes
-                                      * 60L; // seconds
-    uint64_t expectedCycles;
-    gboolean ok =
-        g_uint64_checked_mul(&expectedCycles, oneYearInSeconds, 10 * cyclesPerSecondForOneGHz);
-    g_assert(ok);
-    g_assert_cmpint(_getEmulatedCycles(
-                        emulate_fn, 10L * cyclesPerSecondForOneGHz, oneYearInSeconds * 1000000000L),
-                    ==, expectedCycles);
-}
-
 static struct timespec _timespecsub(const struct timespec* t1, const struct timespec* t2) {
     struct timespec res;
     res.tv_sec = t1->tv_sec - t2->tv_sec;
@@ -157,11 +128,6 @@ int main(int argc, char* argv[]) {
         g_test_add("/tsc/rdtscpIsCloseToNative", void, &_emulateRdtscpWrapper, NULL,
                    closeToNativeRdtsc, NULL);
     }
-
-    g_test_add("/tsc/emulateRdtscGivesExpectedCycles", void, &Tsc_emulateRdtsc, NULL,
-               emulateGivesExpectedCycles, NULL);
-    g_test_add("/tsc/emulateRdtscpGivesExpectedCycles", void, &_emulateRdtscpWrapper, NULL,
-               emulateGivesExpectedCycles, NULL);
 
     return g_test_run();
 }
