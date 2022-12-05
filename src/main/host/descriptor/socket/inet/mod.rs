702d66add8af3c26594fe92b1f879fcdeb711aa5
Steven Engler <opara@cs.georgetown.edu>
2022-12-05 18:40:17 -0500

    Added `InetSocket` enum and placeholder `TcpSocket` struct

--- src/main/host/descriptor/socket/inet/mod.rs
+++ src/main/host/descriptor/socket/inet/mod.rs
@@ -10,59 +10,49 @@ use crate::utility::callback_queue::CallbackQueue;
 use crate::utility::sockaddr::SockaddrStorage;
 use crate::utility::HostTreePointer;
 
-use inet::{InetSocket, InetSocketRef, InetSocketRefMut};
-use unix::UnixSocket;
+use tcp::TcpSocket;
 
-pub mod abstract_unix_ns;
-pub mod inet;
-pub mod unix;
+pub mod tcp;
 
 #[derive(Clone)]
-pub enum Socket {
-    Unix(Arc<AtomicRefCell<UnixSocket>>),
-    Inet(InetSocket),
+pub enum InetSocket {
+    Tcp(Arc<AtomicRefCell<TcpSocket>>),
 }
 
-impl Socket {
-    pub fn borrow(&self) -> SocketRef {
+impl InetSocket {
+    pub fn borrow(&self) -> InetSocketRef {
         match self {
-            Self::Unix(ref f) => SocketRef::Unix(f.borrow()),
-            Self::Inet(ref f) => SocketRef::Inet(f.borrow()),
+            Self::Tcp(ref f) => InetSocketRef::Tcp(f.borrow()),
         }
     }
 
-    pub fn try_borrow(&self) -> Result<SocketRef, atomic_refcell::BorrowError> {
+    pub fn try_borrow(&self) -> Result<InetSocketRef, atomic_refcell::BorrowError> {
         Ok(match self {
-            Self::Unix(ref f) => SocketRef::Unix(f.try_borrow()?),
-            Self::Inet(ref f) => SocketRef::Inet(f.try_borrow()?),
+            Self::Tcp(ref f) => InetSocketRef::Tcp(f.try_borrow()?),
         })
     }
 
-    pub fn borrow_mut(&self) -> SocketRefMut {
+    pub fn borrow_mut(&self) -> InetSocketRefMut {
         match self {
-            Self::Unix(ref f) => SocketRefMut::Unix(f.borrow_mut()),
-            Self::Inet(ref f) => SocketRefMut::Inet(f.borrow_mut()),
+            Self::Tcp(ref f) => InetSocketRefMut::Tcp(f.borrow_mut()),
         }
     }
 
-    pub fn try_borrow_mut(&self) -> Result<SocketRefMut, atomic_refcell::BorrowMutError> {
+    pub fn try_borrow_mut(&self) -> Result<InetSocketRefMut, atomic_refcell::BorrowMutError> {
         Ok(match self {
-            Self::Unix(ref f) => SocketRefMut::Unix(f.try_borrow_mut()?),
-            Self::Inet(ref f) => SocketRefMut::Inet(f.try_borrow_mut()?),
+            Self::Tcp(ref f) => InetSocketRefMut::Tcp(f.try_borrow_mut()?),
         })
     }
 
     pub fn canonical_handle(&self) -> usize {
         match self {
-            Self::Unix(f) => Arc::as_ptr(f) as usize,
-            Self::Inet(ref f) => f.canonical_handle(),
+            Self::Tcp(f) => Arc::as_ptr(f) as usize,
         }
     }
 
     pub fn bind(&self, addr: Option<&SockaddrStorage>, rng: impl rand::Rng) -> SyscallResult {
         match self {
-            Self::Unix(socket) => UnixSocket::bind(socket, addr, rng),
-            Self::Inet(socket) => InetSocket::bind(socket, addr, rng),
+            Self::Tcp(socket) => TcpSocket::bind(socket, addr, rng),
         }
     }
 
@@ -72,17 +62,15 @@ impl Socket {
         cb_queue: &mut CallbackQueue,
     ) -> Result<(), SyscallError> {
         match self {
-            Self::Unix(socket) => UnixSocket::connect(socket, addr, cb_queue),
-            Self::Inet(socket) => InetSocket::connect(socket, addr, cb_queue),
+            Self::Tcp(socket) => TcpSocket::connect(socket, addr, cb_queue),
         }
     }
 }
 
-impl std::fmt::Debug for Socket {
+impl std::fmt::Debug for InetSocket {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Unix(_) => write!(f, "Unix")?,
-            Self::Inet(_) => write!(f, "Inet")?,
+            Self::Tcp(_) => write!(f, "Tcp")?,
         }
 
         if let Ok(file) = self.try_borrow() {
@@ -98,152 +86,144 @@ impl std::fmt::Debug for Socket {
     }
 }
 
-pub enum SocketRef<'a> {
-    Unix(atomic_refcell::AtomicRef<'a, UnixSocket>),
-    Inet(InetSocketRef<'a>),
+pub enum InetSocketRef<'a> {
+    Tcp(atomic_refcell::AtomicRef<'a, TcpSocket>),
 }
 
-pub enum SocketRefMut<'a> {
-    Unix(atomic_refcell::AtomicRefMut<'a, UnixSocket>),
-    Inet(InetSocketRefMut<'a>),
+pub enum InetSocketRefMut<'a> {
+    Tcp(atomic_refcell::AtomicRefMut<'a, TcpSocket>),
 }
 
 // file functions
-impl SocketRef<'_> {
-    enum_passthrough!(self, (), Unix, Inet;
+impl InetSocketRef<'_> {
+    enum_passthrough!(self, (), Tcp;
         pub fn state(&self) -> FileState
     );
-    enum_passthrough!(self, (), Unix, Inet;
+    enum_passthrough!(self, (), Tcp;
         pub fn mode(&self) -> FileMode
     );
-    enum_passthrough!(self, (), Unix, Inet;
+    enum_passthrough!(self, (), Tcp;
         pub fn get_status(&self) -> FileStatus
     );
-    enum_passthrough!(self, (), Unix, Inet;
+    enum_passthrough!(self, (), Tcp;
         pub fn has_open_file(&self) -> bool
     );
-    enum_passthrough!(self, (), Unix, Inet;
+    enum_passthrough!(self, (), Tcp;
         pub fn supports_sa_restart(&self) -> bool
     );
 }
 
 // socket-specific functions
-impl SocketRef<'_> {
+impl InetSocketRef<'_> {
     pub fn getpeername(&self) -> Result<Option<SockaddrStorage>, SyscallError> {
         match self {
-            Self::Unix(socket) => socket.getpeername().map(|opt| opt.map(Into::into)),
-            Self::Inet(socket) => socket.getpeername().map(|opt| opt.map(Into::into)),
+            Self::Tcp(socket) => socket.getpeername().map(|opt| opt.map(Into::into)),
         }
     }
 
     pub fn getsockname(&self) -> Result<Option<SockaddrStorage>, SyscallError> {
         match self {
-            Self::Unix(socket) => socket.getsockname().map(|opt| opt.map(Into::into)),
-            Self::Inet(socket) => socket.getsockname().map(|opt| opt.map(Into::into)),
+            Self::Tcp(socket) => socket.getsockname().map(|opt| opt.map(Into::into)),
         }
     }
 
-    enum_passthrough!(self, (), Unix, Inet;
+    enum_passthrough!(self, (), Tcp;
         pub fn address_family(&self) -> nix::sys::socket::AddressFamily
     );
 }
 
 // file functions
-impl SocketRefMut<'_> {
-    enum_passthrough!(self, (), Unix, Inet;
+impl InetSocketRefMut<'_> {
+    enum_passthrough!(self, (), Tcp;
         pub fn state(&self) -> FileState
     );
-    enum_passthrough!(self, (), Unix, Inet;
+    enum_passthrough!(self, (), Tcp;
         pub fn mode(&self) -> FileMode
     );
-    enum_passthrough!(self, (), Unix, Inet;
+    enum_passthrough!(self, (), Tcp;
         pub fn get_status(&self) -> FileStatus
     );
-    enum_passthrough!(self, (), Unix, Inet;
+    enum_passthrough!(self, (), Tcp;
         pub fn has_open_file(&self) -> bool
     );
-    enum_passthrough!(self, (val), Unix, Inet;
+    enum_passthrough!(self, (val), Tcp;
         pub fn set_has_open_file(&mut self, val: bool)
     );
-    enum_passthrough!(self, (), Unix, Inet;
+    enum_passthrough!(self, (), Tcp;
         pub fn supports_sa_restart(&self) -> bool
     );
-    enum_passthrough!(self, (cb_queue), Unix, Inet;
+    enum_passthrough!(self, (cb_queue), Tcp;
         pub fn close(&mut self, cb_queue: &mut CallbackQueue) -> Result<(), SyscallError>
     );
-    enum_passthrough!(self, (status), Unix, Inet;
+    enum_passthrough!(self, (status), Tcp;
         pub fn set_status(&mut self, status: FileStatus)
     );
-    enum_passthrough!(self, (request, arg_ptr, memory_manager), Unix, Inet;
+    enum_passthrough!(self, (request, arg_ptr, memory_manager), Tcp;
         pub fn ioctl(&mut self, request: u64, arg_ptr: PluginPtr, memory_manager: &mut MemoryManager) -> SyscallResult
     );
-    enum_passthrough!(self, (ptr), Unix, Inet;
+    enum_passthrough!(self, (ptr), Tcp;
         pub fn add_legacy_listener(&mut self, ptr: HostTreePointer<c::StatusListener>)
     );
-    enum_passthrough!(self, (ptr), Unix, Inet;
+    enum_passthrough!(self, (ptr), Tcp;
         pub fn remove_legacy_listener(&mut self, ptr: *mut c::StatusListener)
     );
 
-    enum_passthrough_generic!(self, (bytes, offset, cb_queue), Unix, Inet;
+    enum_passthrough_generic!(self, (bytes, offset, cb_queue), Tcp;
         pub fn read<W>(&mut self, bytes: W, offset: libc::off_t, cb_queue: &mut CallbackQueue) -> SyscallResult
         where W: std::io::Write + std::io::Seek
     );
 
-    enum_passthrough_generic!(self, (source, offset, cb_queue), Unix, Inet;
+    enum_passthrough_generic!(self, (source, offset, cb_queue), Tcp;
         pub fn write<R>(&mut self, source: R, offset: libc::off_t, cb_queue: &mut CallbackQueue) -> SyscallResult
         where R: std::io::Read + std::io::Seek
     );
 }
 
 // socket-specific functions
-impl SocketRefMut<'_> {
+impl InetSocketRefMut<'_> {
     pub fn getpeername(&self) -> Result<Option<SockaddrStorage>, SyscallError> {
         match self {
-            Self::Unix(socket) => socket.getpeername().map(|opt| opt.map(Into::into)),
-            Self::Inet(socket) => socket.getpeername().map(|opt| opt.map(Into::into)),
+            Self::Tcp(socket) => socket.getpeername().map(|opt| opt.map(Into::into)),
         }
     }
 
     pub fn getsockname(&self) -> Result<Option<SockaddrStorage>, SyscallError> {
         match self {
-            Self::Unix(socket) => socket.getsockname().map(|opt| opt.map(Into::into)),
-            Self::Inet(socket) => socket.getsockname().map(|opt| opt.map(Into::into)),
+            Self::Tcp(socket) => socket.getsockname().map(|opt| opt.map(Into::into)),
         }
     }
 
-    enum_passthrough!(self, (), Unix, Inet;
+    enum_passthrough!(self, (), Tcp;
         pub fn address_family(&self) -> nix::sys::socket::AddressFamily
     );
 
-    enum_passthrough_generic!(self, (source, addr, cb_queue), Unix, Inet;
+    enum_passthrough_generic!(self, (source, addr, cb_queue), Tcp;
         pub fn sendto<R>(&mut self, source: R, addr: Option<SockaddrStorage>, cb_queue: &mut CallbackQueue)
             -> SyscallResult
         where R: std::io::Read + std::io::Seek
     );
 
-    enum_passthrough_generic!(self, (bytes, cb_queue), Unix, Inet;
+    enum_passthrough_generic!(self, (bytes, cb_queue), Tcp;
         pub fn recvfrom<W>(&mut self, bytes: W, cb_queue: &mut CallbackQueue)
             -> Result<(SysCallReg, Option<SockaddrStorage>), SyscallError>
         where W: std::io::Write + std::io::Seek
     );
 
-    enum_passthrough!(self, (backlog, cb_queue), Unix, Inet;
+    enum_passthrough!(self, (backlog, cb_queue), Tcp;
         pub fn listen(&mut self, backlog: i32, cb_queue: &mut CallbackQueue) -> Result<(), SyscallError>
     );
 
-    pub fn accept(&mut self, cb_queue: &mut CallbackQueue) -> Result<Socket, SyscallError> {
+    pub fn accept(&mut self, cb_queue: &mut CallbackQueue) -> Result<InetSocket, SyscallError> {
         match self {
-            Self::Unix(socket) => socket.accept(cb_queue).map(Socket::Unix),
-            Self::Inet(socket) => socket.accept(cb_queue).map(Socket::Inet),
+            Self::Tcp(socket) => socket.accept(cb_queue).map(InetSocket::Tcp),
         }
     }
 }
 
-impl std::fmt::Debug for SocketRef<'_> {
+impl std::fmt::Debug for InetSocketRef<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Unix(_) => write!(f, "Unix")?,
-            Self::Inet(_) => write!(f, "Inet")?,
+            Self::Tcp(_) => write!(f, "Tcp")?,
         }
 
         write!(
@@ -255,11 +235,10 @@ impl std::fmt::Debug for SocketRef<'_> {
     }
 }
 
-impl std::fmt::Debug for SocketRefMut<'_> {
+impl std::fmt::Debug for InetSocketRefMut<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Unix(_) => write!(f, "Unix")?,
-            Self::Inet(_) => write!(f, "Inet")?,
+            Self::Tcp(_) => write!(f, "Tcp")?,
         }
 
         write!(
