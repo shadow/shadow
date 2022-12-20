1952b6e71addb78d02f55d056b209f5f935536e6
Steven Engler <opara@cs.georgetown.edu>
2022-12-20 10:33:12 -0500

    Updated `log_syscall` macro to use `quote!` macro

--- src/lib/syscall-logger/src/lib.rs
+++ src/lib/syscall-logger/src/lib.rs
@@ -43,25 +43,30 @@ use quote::ToTokens;
 /// ```
 #[proc_macro_attribute]
 pub fn log_syscall(args: TokenStream, input: TokenStream) -> TokenStream {
-    let mut item: syn::Item = syn::parse(input).unwrap();
-    let mut fn_item = match &mut item {
-        syn::Item::Fn(fn_item) => fn_item,
-        _ => panic!("expected fn"),
-    };
+    let mut input: syn::Item = syn::parse(input).unwrap();
 
-    let syscall_name = fn_item.sig.ident.to_string();
+    let syscall_name;
+    let syscall_name_original;
 
-    // rename the function
-    fn_item.sig.ident = syn::Ident::new(
-        &(syscall_name.clone() + "_original"),
-        fn_item.sig.ident.span(),
-    );
+    // modify the original function
+    {
+        let mut input_fn = match &mut input {
+            syn::Item::Fn(input_fn) => input_fn,
+            _ => panic!("Expected Item::Fn"),
+        };
 
-    // we assume the syscall handler is public for simplicity
-    assert!(std::matches!(fn_item.vis, syn::Visibility::Public(_)));
+        syscall_name = input_fn.sig.ident.clone();
+        syscall_name_original = quote::format_ident!("{}_original", syscall_name);
 
-    // make the function non-public
-    fn_item.vis = syn::Visibility::Inherited;
+        // rename the function
+        input_fn.sig.ident = syscall_name_original.clone();
+
+        // we assume the syscall handler is public for simplicity
+        assert!(std::matches!(input_fn.vis, syn::Visibility::Public(_)));
+
+        // make the function non-public
+        input_fn.vis = syn::Visibility::Inherited;
+    }
 
     let mut rv_type = vec![];
     let mut arg_types = vec![];
@@ -81,59 +86,65 @@ pub fn log_syscall(args: TokenStream, input: TokenStream) -> TokenStream {
         }
     }
 
-    let x = format!(
-        r#"
-        pub fn {syscall_name}(
+    let arg_types: Vec<proc_macro2::TokenStream> = arg_types
+        .into_iter()
+        .map(|x| TokenStream::from(x).into())
+        .collect();
+    let rv_type: Vec<proc_macro2::TokenStream> = rv_type
+        .into_iter()
+        .map(|x| TokenStream::from(x).into())
+        .collect();
+
+    let syscall_name_str = syscall_name.to_string();
+
+    let syscall_wrapper = quote::quote! {
+        pub fn #syscall_name(
             &self,
             ctx: &mut crate::host::context::ThreadContext,
             args: &crate::host::syscall_types::SysCallArgs,
-        ) -> crate::host::syscall_types::SyscallResult {{
-            let Some(strace_fmt_options) = ctx.process.strace_logging_options() else {{
+        ) -> crate::host::syscall_types::SyscallResult {
+            let Some(strace_fmt_options) = ctx.process.strace_logging_options() else {
                 // exit early if strace logging is not enabled
-                return self.{syscall_name}_original(ctx, args);
-            }};
+                return self.#syscall_name_original(ctx, args);
+            };
 
             // make sure to include the full path to all used types
             use crate::core::worker::Worker;
-            use crate::host::syscall::format::{{SyscallArgsFmt, SyscallResultFmt, write_syscall}};
+            use crate::host::syscall::format::{SyscallArgsFmt, SyscallResultFmt, write_syscall};
 
-            let syscall_args = {{
+            let syscall_args = {
                 let memory = ctx.process.memory_borrow();
-                let syscall_args = <SyscallArgsFmt::<{syscall_args}>>::new(args, strace_fmt_options, &*memory);
+                let syscall_args = <SyscallArgsFmt::<#(#arg_types)*>>::new(args, strace_fmt_options, &*memory);
                 // need to convert to a string so that we read the plugin's memory before we potentially
                 // modify it during the syscall
-                format!("{{}}", syscall_args)
-            }};
+                format!("{}", syscall_args)
+            };
 
             // make the syscall
-            let rv = self.{syscall_name}_original(ctx, args);
+            let rv = self.#syscall_name_original(ctx, args);
 
             // format the result (returns None if the syscall didn't complete)
             let memory = ctx.process.memory_borrow();
-            let syscall_rv = SyscallResultFmt::<{syscall_rv}>::new(&rv, strace_fmt_options, &*memory);
+            let syscall_rv = SyscallResultFmt::<#(#rv_type)*>::new(&rv, strace_fmt_options, &*memory);
 
-            if let Some(ref syscall_rv) = syscall_rv {{
-                ctx.process.with_strace_file(|file| {{
+            if let Some(ref syscall_rv) = syscall_rv {
+                ctx.process.with_strace_file(|file| {
                     write_syscall(
                         file,
                         &Worker::current_time().unwrap(),
                         ctx.thread.id(),
-                        "{syscall_name}",
+                        #syscall_name_str,
                         &syscall_args,
                         syscall_rv,
                     ).unwrap();
-                }});
-            }}
+                });
+            }
 
             rv
-        }}
-        "#,
-        syscall_name = &syscall_name,
-        syscall_rv = TokenStream::from_iter(rv_type),
-        syscall_args = TokenStream::from_iter(arg_types),
-    );
+        }
+    };
 
-    let mut s: TokenStream = x.parse().unwrap();
-    s.extend(TokenStream::from(item.into_token_stream()));
+    let mut s: TokenStream = syscall_wrapper.into();
+    s.extend(TokenStream::from(input.into_token_stream()));
     s
 }
