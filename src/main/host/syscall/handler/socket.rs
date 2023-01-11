08ac39e0d5404154a20c046eb00542204d3e643c
Steven Engler <opara@cs.georgetown.edu>
2023-01-10 19:33:40 -0500

    Moved `read_sockaddr` and `write_sockaddr`
    Also made them public.

--- src/main/host/syscall/handler/socket.rs
+++ src/main/host/syscall/handler/socket.rs
@@ -1,5 +1,3 @@
-use std::mem::MaybeUninit;
-
 use crate::cshadow as c;
 use crate::host::context::ThreadContext;
 use crate::host::descriptor::socket::inet::tcp::TcpSocket;
@@ -9,8 +7,7 @@ use crate::host::descriptor::socket::Socket;
 use crate::host::descriptor::{
     CompatFile, Descriptor, DescriptorFlags, File, FileState, FileStatus, OpenFile,
 };
-use crate::host::memory_manager::MemoryManager;
-use crate::host::syscall::handler::SyscallHandler;
+use crate::host::syscall::handler::{read_sockaddr, write_sockaddr, SyscallHandler};
 use crate::host::syscall::type_formatting::SyscallBufferArg;
 use crate::host::syscall::Trigger;
 use crate::host::syscall_condition::SysCallCondition;
@@ -920,86 +917,4 @@ impl SyscallHandler {
         );
         Err(Errno::ENOSYS.into())
     }
-}
-
-fn write_sockaddr(
-    mem: &mut MemoryManager,
-    addr: Option<&SockaddrStorage>,
-    plugin_addr: PluginPtr,
-    plugin_addr_len: TypedPluginPtr<libc::socklen_t>,
-) -> Result<(), SyscallError> {
-    let addr = match addr {
-        Some(x) => x,
-        None => {
-            mem.copy_to_ptr(plugin_addr_len, &[0])?;
-            return Ok(());
-        }
-    };
-
-    let from_addr_slice = addr.as_slice();
-    let from_len: u32 = from_addr_slice.len().try_into().unwrap();
-
-    // get the provided address buffer length, and overwrite it with the real address length
-    let plugin_addr_len = {
-        let mut plugin_addr_len = mem.memory_ref_mut(plugin_addr_len)?;
-        let plugin_addr_len_value = plugin_addr_len.get_mut(0).unwrap();
-
-        // keep a copy before we change it
-        let plugin_addr_len_copy = *plugin_addr_len_value;
-
-        *plugin_addr_len_value = from_len;
-
-        plugin_addr_len.flush()?;
-        plugin_addr_len_copy
-    };
-
-    // return early if the address length is 0
-    if plugin_addr_len == 0 {
-        return Ok(());
-    }
-
-    // the minimum of the given address buffer length and the real address length
-    let len_to_copy = std::cmp::min(from_len, plugin_addr_len).try_into().unwrap();
-
-    let plugin_addr = TypedPluginPtr::new::<MaybeUninit<u8>>(plugin_addr, len_to_copy);
-    mem.copy_to_ptr(plugin_addr, &from_addr_slice[..len_to_copy])?;
-
-    Ok(())
-}
-
-fn read_sockaddr(
-    mem: &MemoryManager,
-    addr_ptr: PluginPtr,
-    addr_len: libc::socklen_t,
-) -> Result<Option<SockaddrStorage>, SyscallError> {
-    if addr_ptr.is_null() {
-        return Ok(None);
-    }
-
-    let addr_len_usize: usize = addr_len.try_into().unwrap();
-
-    // this won't have the correct alignment, but that's fine since `SockaddrStorage::from_bytes()`
-    // doesn't require alignment
-    let mut addr_buf = [MaybeUninit::new(0u8); std::mem::size_of::<libc::sockaddr_storage>()];
-
-    // make sure we will not lose data when we copy
-    if addr_len_usize > std::mem::size_of_val(&addr_buf) {
-        warn!(
-            "Shadow does not support the address length {}, which is larger than {}",
-            addr_len,
-            std::mem::size_of_val(&addr_buf),
-        );
-        return Err(Errno::EINVAL.into());
-    }
-
-    let addr_buf = &mut addr_buf[..addr_len_usize];
-
-    mem.copy_from_ptr(
-        addr_buf,
-        TypedPluginPtr::new::<MaybeUninit<u8>>(addr_ptr, addr_len_usize),
-    )?;
-
-    let addr = unsafe { SockaddrStorage::from_bytes(addr_buf).ok_or(Errno::EINVAL)? };
-
-    Ok(Some(addr))
 }
