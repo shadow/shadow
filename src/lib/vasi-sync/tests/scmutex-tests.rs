28ec78d789905cecf7276616e5fed7a7dead2730
Jim Newsome <jnewsome@torproject.org>
2023-02-21 17:30:04 -0600

    vasi-sync: create a tests sync module

--- src/lib/vasi-sync/tests/scmutex-tests.rs
+++ src/lib/vasi-sync/tests/scmutex-tests.rs
@@ -5,44 +5,7 @@
 //! [loom]: <https://docs.rs/loom/latest/loom/>
 use vasi_sync::scmutex::{SelfContainedMutex, SelfContainedMutexGuard};
 
-mod sync {
-    #[cfg(loom)]
-    pub fn model<F>(f: F)
-    where
-        F: Fn() + Sync + Send + 'static,
-    {
-        loom::model(move || {
-            f();
-            vasi_sync::sync::loom_reset();
-        });
-    }
-    #[cfg(not(loom))]
-    pub fn model<F>(f: F)
-    where
-        F: Fn() + Sync + Send + 'static,
-    {
-        f()
-    }
-
-    #[cfg(loom)]
-    pub use loom::sync::Arc;
-    #[cfg(not(loom))]
-    pub use std::sync::Arc;
-
-    #[cfg(loom)]
-    pub use loom::thread;
-    #[cfg(not(loom))]
-    pub use std::thread;
-
-    #[cfg(loom)]
-    pub fn rand_sleep() {}
-    #[cfg(not(loom))]
-    pub fn rand_sleep() {
-        std::thread::sleep(std::time::Duration::from_nanos(
-            rand::random::<u64>() % 10_000_000,
-        ));
-    }
-}
+mod sync;
 
 mod tests {
     use super::*;
---
+++ src/lib/vasi-sync/tests/sync/mod.rs
@@ -0,0 +1,41 @@
+//! This file contains utilities that can be reused across integration tests.
+//! It's in a subdirectory of tests to avoid being interpreted as an integration
+//! test itself. See
+//! https://doc.rust-lang.org/book/ch11-03-test-organization.html#submodules-in-integration-tests
+
+#[cfg(loom)]
+pub fn model<F>(f: F)
+where
+    F: Fn() + Sync + Send + 'static,
+{
+    loom::model(move || {
+        f();
+        vasi_sync::sync::loom_reset();
+    });
+}
+#[cfg(not(loom))]
+pub fn model<F>(f: F)
+where
+    F: Fn() + Sync + Send + 'static,
+{
+    f()
+}
+
+#[cfg(loom)]
+pub use loom::sync::Arc;
+#[cfg(not(loom))]
+pub use std::sync::Arc;
+
+#[cfg(loom)]
+pub use loom::thread;
+#[cfg(not(loom))]
+pub use std::thread;
+
+#[cfg(loom)]
+pub fn rand_sleep() {}
+#[cfg(not(loom))]
+pub fn rand_sleep() {
+    std::thread::sleep(std::time::Duration::from_nanos(
+        rand::random::<u64>() % 10_000_000,
+    ));
+}
