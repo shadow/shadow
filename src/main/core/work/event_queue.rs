df3f65294c22cf14d0fde4e5f654bb673c667c6b
Steven Engler <opara@cs.georgetown.edu>
2023-02-28 12:47:11 -0500

    Removed unused event queue ffi code

--- src/main/core/work/event_queue.rs
+++ src/main/core/work/event_queue.rs
@@ -1,6 +1,5 @@
 use std::cmp::Reverse;
 use std::collections::binary_heap::BinaryHeap;
-use std::sync::Mutex;
 
 use shadow_shim_helper_rs::emulated_time::EmulatedTime;
 
@@ -96,75 +95,5 @@ impl<T: PartialOrd + Eq> std::ops::Deref for PanickingOrd<T> {
 impl<T: PartialOrd + Eq> std::ops::DerefMut for PanickingOrd<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
-    }
-}
-
-/// A wrapper for [`EventQueue`] that uses interior mutability to make the ffi simpler.
-#[derive(Debug)]
-pub struct ThreadSafeEventQueue(pub Mutex<EventQueue>);
-
-mod export {
-    use super::*;
-    use shadow_shim_helper_rs::emulated_time::CEmulatedTime;
-
-    use std::sync::Arc;
-
-    #[no_mangle]
-    pub unsafe extern "C" fn eventqueue_new() -> *const ThreadSafeEventQueue {
-        Arc::into_raw(Arc::new(ThreadSafeEventQueue(
-            Mutex::new(EventQueue::new()),
-        )))
-    }
-
-    #[no_mangle]
-    pub unsafe extern "C" fn eventqueue_drop(queue: *const ThreadSafeEventQueue) {
-        assert!(!queue.is_null());
-        unsafe { Arc::from_raw(queue) };
-    }
-
-    #[no_mangle]
-    pub unsafe extern "C" fn eventqueue_cloneArc(
-        queue_ptr: *const ThreadSafeEventQueue,
-    ) -> *const ThreadSafeEventQueue {
-        assert!(!queue_ptr.is_null());
-
-        let queue_arc = unsafe { Arc::from_raw(queue_ptr) };
-        let queue_dup = Arc::clone(&queue_arc);
-
-        assert_eq!(Arc::into_raw(queue_arc), queue_ptr);
-        Arc::into_raw(queue_dup)
-    }
-
-    /// Takes ownership of the event.
-    #[no_mangle]
-    pub unsafe extern "C" fn eventqueue_push(
-        queue: *const ThreadSafeEventQueue,
-        event: *mut Event,
-    ) {
-        assert!(!event.is_null());
-        let queue = unsafe { queue.as_ref() }.unwrap();
-        let event = unsafe { Box::from_raw(event) };
-        queue.0.lock().unwrap().push(*event);
-    }
-
-    #[no_mangle]
-    pub unsafe extern "C" fn eventqueue_pop(queue: *const ThreadSafeEventQueue) -> *mut Event {
-        let queue = unsafe { queue.as_ref() }.unwrap();
-        queue
-            .0
-            .lock()
-            .unwrap()
-            .pop()
-            .map(Box::new)
-            .map(Box::into_raw)
-            .unwrap_or(std::ptr::null_mut())
-    }
-
-    #[no_mangle]
-    pub unsafe extern "C" fn eventqueue_nextEventTime(
-        queue: *const ThreadSafeEventQueue,
-    ) -> CEmulatedTime {
-        let queue = unsafe { queue.as_ref() }.unwrap();
-        EmulatedTime::to_c_emutime(queue.0.lock().unwrap().next_event_time())
     }
 }
