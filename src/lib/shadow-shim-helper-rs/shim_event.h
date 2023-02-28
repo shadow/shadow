88dde2bfc7a9437da850d21dfbd42593f50cb542
Jim Newsome <jnewsome@torproject.org>
2023-02-28 13:54:23 -0600

    Convert ShimEvent to Rust

--- src/lib/shadow-shim-helper-rs/shim_event.h
+++ src/lib/shadow-shim-helper-rs/shim_event.h
@@ -8,35 +8,4 @@
 #include "lib/shmem/shmem_allocator.h"
 #include "main/host/syscall/kernel_types.h"
 
-typedef struct _ShimEvent {
-    ShimEventID event_id;
-
-    union {
-        struct {
-            // We wrap this in the surrounding struct in case there's anything
-            // else we end up needing in the message besides the literal struct
-            // we're going to pass to the syscall handler.
-            SysCallArgs syscall_args;
-        } syscall;
-
-        struct {
-            SysCallReg retval;
-            // Whether the syscall is eligible to be restarted. Only applicable
-            // when retval is -EINTR. See signal(7).
-            bool restartable;
-        } syscall_complete;
-
-        struct {
-            ShMemBlockSerialized serial;
-            PluginPtr plugin_ptr;
-            size_t n;
-        } shmem_blk;
-
-        struct {
-            ShMemBlockSerialized ipc_block;
-        } add_thread_req;
-    } event_data;
-
-} ShimEvent;
-
 #endif // SHD_SHIM_SHIM_EVENT_H_
--- src/lib/shadow-shim-helper-rs/src/shim_event.rs
+++ src/lib/shadow-shim-helper-rs/src/shim_event.rs
@@ -1,4 +1,8 @@
-/// cbindgen:prefix-with-name=false
+use shadow_shmem::allocator::ShMemBlockSerialized;
+
+use crate::syscall_types::{PluginPtr, SysCallArgs, SysCallReg};
+
+#[derive(Copy, Clone, Debug)]
 #[repr(C)]
 pub enum ShimEventID {
     Null = 0,
@@ -17,4 +21,49 @@ pub enum ShimEventID {
     Block = 10,
     AddThreadReq = 11,
     AddThreadParentRes = 12,
+}
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+pub struct ShimEventSyscall {
+    pub syscall_args: SysCallArgs,
+}
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+pub struct ShimEventSyscallComplete {
+    pub retval: SysCallReg,
+    // Whether the syscall is eligible to be restarted. Only applicable
+    // when retval is -EINTR. See signal(7).
+    pub restartable: bool,
+}
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+pub struct ShimEventShmemBlk {
+    pub serial: ShMemBlockSerialized,
+    pub plugin_ptr: PluginPtr,
+    pub n: usize,
+}
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+pub struct ShimEventAddThreadReq {
+    pub ipc_block: ShMemBlockSerialized,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub union ShimEventData {
+    pub syscall: ShimEventSyscall,
+    pub syscall_complete: ShimEventSyscallComplete,
+    pub shmem_blk: ShimEventShmemBlk,
+    pub add_thread_req: ShimEventAddThreadReq,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct ShimEvent {
+    pub event_id: ShimEventID,
+    pub event_data: ShimEventData,
 }
