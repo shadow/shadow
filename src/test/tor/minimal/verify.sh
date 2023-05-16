1e57a2deea42436a3e5a3f5d8608281b320240a3
Jim Newsome <jnewsome@torproject.org>
2023-05-16 17:57:58 -0500

    tor-minimal-test verify.sh: clean up
    Check for bash errors (`set -euo pipefail`) and add some constants.

--- src/test/tor/minimal/verify.sh
+++ src/test/tor/minimal/verify.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+EXPECTED_BOOTSTRAP_COUNT=11
 bootstrapped_count="$(grep -r --include="tor.*.stdout" "Bootstrapped 100" | wc -l)"
-printf "Bootstrapped count: ${bootstrapped_count}/11\n"
+printf "Bootstrapped count: ${bootstrapped_count}/$EXPECTED_BOOTSTRAP_COUNT\n"
 
-if [ "${bootstrapped_count}" != "11" ]; then
+if [ "${bootstrapped_count}" != "$EXPECTED_BOOTSTRAP_COUNT" ]; then
     printf "Verification ${RED}failed${NC}: Not all tor processes bootstrapped :(\n"
     exit 1
 fi
 
+EXPECTED_STREAM_COUNT=80
 stream_count="$(grep -r --include="tgen.*.stdout" "stream-success" | wc -l)"
-printf "Successful tgen stream count: ${stream_count}/80\n"
+printf "Successful tgen stream count: ${stream_count}/$EXPECTED_STREAM_COUNT\n"
 
-if [ "${stream_count}" != "80" ]; then
+if [ "${stream_count}" != "$EXPECTED_STREAM_COUNT" ]; then
     printf "Verification ${RED}failed${NC}: Not all tgen streams were successful :(\n"
     exit 1
 fi
