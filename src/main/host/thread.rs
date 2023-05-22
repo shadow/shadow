856e362f00e28462cf13dd916b3f5666c53aad4e
Jim Newsome <jnewsome@torproject.org>
2023-05-22 10:54:32 -0500

    Merge Thread::run into Thread::spawn

--- src/main/host/thread.rs
+++ src/main/host/thread.rs
@@ -295,9 +295,18 @@ impl Thread {
         host: &Host,
         process_id: ProcessId,
         thread_id: ThreadId,
+        plugin_path: &CStr,
+        argv: Vec<CString>,
+        envv: Vec<CString>,
+        working_dir: &CStr,
+        strace_fd: Option<RawFd>,
+        log_path: &CStr,
     ) -> RootedRc<RootedRefCell<Self>> {
+        let mut mthread = ManagedThread::new();
+        mthread.run(plugin_path, argv, envv, working_dir, strace_fd, log_path);
+
         let thread = Self {
-            mthread: RefCell::new(ManagedThread::new()),
+            mthread: RefCell::new(mthread),
             syscallhandler: unsafe {
                 SendPointer::new(c::syscallhandler_new(
                     host.id(),
@@ -364,20 +373,6 @@ impl Thread {
     /// Shared memory for this thread.
     pub fn shmem(&self) -> &ShMemBlock<ThreadShmem> {
         &self.shim_shared_memory
-    }
-
-    pub fn run(
-        &self,
-        plugin_path: &CStr,
-        argv: Vec<CString>,
-        envv: Vec<CString>,
-        working_dir: &CStr,
-        strace_fd: Option<RawFd>,
-        log_path: &CStr,
-    ) {
-        self.mthread
-            .borrow_mut()
-            .run(plugin_path, argv, envv, working_dir, strace_fd, log_path);
     }
 
     pub fn resume(&self, ctx: &ProcessContext) -> ResumeResult {
