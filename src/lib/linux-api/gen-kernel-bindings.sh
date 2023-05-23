d1a7670d9e52c0ef83751a52b4667236d1000f8b
Jim Newsome <jnewsome@torproject.org>
2023-05-23 11:00:40 -0500

    Create initial linux-api crate
    Mostly setting up binding generation and structure. Initially we just
    generate bindings for some signal-related definitions.

---
+++ src/lib/linux-api/gen-kernel-bindings.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Checks out and builds Linux kernel headers,
+# and uses `bindgen` to generate Rust bindings.
+#
+# We don't do this from the crate build script to avoid requiring a checkout of
+# the Linux kernel source to build Shadow.
+#
+# We check out the kernel source and build at a fixed version for reproducible
+# results. While the kernel ABI is stable, there are source-level changes across
+# versions that can change the generated bindings.
+
+set -euo pipefail
+
+# Generally it makes sense to use the most recent stable version of the kernel,
+# since the ABI is stable. i.e. managed programs compiled against an older
+# version of the kernel headers should still get ABI-compatible definitions.
+LINUX_TAG=v6.2
+
+LINUX_REPO=https://github.com/torvalds/linux.git
+ARCH=x86_64
+
+BUILDDIR="$(pwd)/bindings-build"
+LINUX_SRC_DIR="$BUILDDIR/linux-src"
+LINUX_INSTALL_DIR="$BUILDDIR/linux-install"
+
+if [ ! -d "$BUILDDIR" ]; then
+  mkdir "$BUILDDIR"
+fi
+
+if [ ! -d "$LINUX_SRC_DIR" ]; then
+  git clone -b "$LINUX_TAG" --depth=1 "$LINUX_REPO" "$LINUX_SRC_DIR"
+fi
+git -C "$LINUX_SRC_DIR" checkout "$LINUX_TAG"
+
+# https://www.kernel.org/doc/html/latest/kbuild/headers_install.html
+make -C "$LINUX_SRC_DIR" headers_install ARCH="$ARCH" INSTALL_HDR_PATH="$LINUX_INSTALL_DIR"
+
+bindgen_flags=()
+
+# Use core instead of std, for no_std compatibility.
+bindgen_flags+=("--use-core")
+
+# We seem to also need this to prevent using ctypes from std.
+bindgen_flags+=("--ctypes-prefix=::core::ffi")
+
+# Metadata about how we're generating the file
+bindgen_flags+=("--raw-line=/* Build script: $0 */")
+bindgen_flags+=("--raw-line=/* Kernel tag: $LINUX_TAG */")
+
+# Signal names
+bindgen_flags+=("--allowlist-var=SIG.*")
+
+# Signal types
+bindgen_flags+=("--allowlist-type=sigset_t")
+bindgen_flags+=("--allowlist-type=siginfo_t")
+
+# Output
+bindgen_flags+=("-o" "src/bindings.rs")
+
+# Input
+bindgen_flags+=("./bindings-wrapper.h")
+
+# Following flags are passed to clang
+bindgen_flags+=("--")
+
+# Override the system's linux headers (if any) with our own.
+bindgen_flags+=("-I" "$LINUX_INSTALL_DIR/include")
+
+bindgen "${bindgen_flags[@]}"
---
+++ src/lib/linux-api/src/lib.rs
@@ -0,0 +1,20 @@
+//! Type definitions and utilities for interacting with the Linux API.
+//! Does not depend on the `std` crate (i.e. is `no_std`) nor libc.
+
+#![no_std]
+// https://github.com/rust-lang/rfcs/blob/master/text/2585-unsafe-block-in-unsafe-fn.md
+#![deny(unsafe_op_in_unsafe_fn)]
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
+#![allow(clippy::enum_variant_names)]
+#![allow(clippy::too_many_arguments)]
+
+/// cbindgen:ignore
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+// https://github.com/rust-lang/rust/issues/66220
+#[allow(improper_ctypes)]
+#[allow(unsafe_op_in_unsafe_fn)]
+#[allow(clippy::all)]
+#[allow(unused)]
+mod bindings;
