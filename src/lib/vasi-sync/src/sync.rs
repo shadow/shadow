789f9f247cf9a87c55518cf735725001891f4f5d
Jim Newsome <jnewsome@torproject.org>
2023-05-30 20:19:39 -0500

    vasi-sync: avoid touching errno
    errno is a libc dependency, and is a thread-local. Removing its usage
    here is progress towards being able to operate without native thread
    local storage (#1559) and without libc in general (#2919).

--- src/lib/vasi-sync/src/sync.rs
+++ src/lib/vasi-sync/src/sync.rs
@@ -31,85 +31,106 @@ loom::lazy_static! {
 }
 
 #[cfg(not(loom))]
-pub fn futex_wait(futex_word: &AtomicU32, val: u32) -> nix::Result<i64> {
-    nix::errno::Errno::result(unsafe {
-        libc::syscall(
-            libc::SYS_futex,
-            futex_word as *const _,
-            libc::FUTEX_WAIT,
-            val,
-            core::ptr::null() as *const libc::timespec,
-            core::ptr::null_mut() as *mut u32,
-            0u32,
-        )
-    })
-}
-#[cfg(loom)]
-pub fn futex_wait(futex_word: &AtomicU32, val: u32) -> nix::Result<i64> {
-    // From futex(2):
-    //   This load, the comparison with the expected value, and starting to
-    //   sleep are performed atomically and totally ordered with
-    //   respect to other futex operations on the same futex word.
-    //
-    // We hold a lock on our FUTEXES to represent this.
-    // TODO: If we want to run loom tests with multiple interacting locks,
-    // we should have per-futex mutexes here, and not hold a lock over the
-    // whole list the whole time.
-    let mut hashmap = FUTEXES.lock().unwrap();
-    let futex_word_val = futex_word.load(Ordering::Relaxed);
-    if futex_word_val != val {
-        return Err(nix::errno::Errno::EAGAIN);
-    }
-    let condvar = hashmap
-        .entry(futex_word as *const _ as usize)
-        .or_insert(Arc::new(Condvar::new()))
-        .clone();
-    // We could get a spurious wakeup here, but that's ok.
-    // Futexes are subject to spurious wakeups too.
-    condvar.wait(hashmap).unwrap();
-    Ok(0)
-}
-
-#[cfg(not(loom))]
-pub fn futex_wake(futex_word: &AtomicU32) -> nix::Result<()> {
-    // The kernel just checks for equality of the value at `futex_word`,
-    // which it interprets as `* const u32`.
-    //
-    // This is safe since `AtomicU32` ["has the same in-memory
-    // representation as the underlying integer type,
-    // u32"](https://doc.rust-lang.org/std/sync/atomic/struct.AtomicU32.html#).
-    //
-    // TODO: Consider using
-    // [`as_mut_ptr`](https://doc.rust-lang.org/std/sync/atomic/struct.AtomicU32.html#method.as_mut_ptr)
-    // here once it's stabilized.
+unsafe fn futex(futex_word: &AtomicU32, futex_operation: i32, val: u32) -> nix::Result<i64> {
     type SourceT = AtomicU32;
     type TargetT = u32;
     let futex_word: &SourceT = futex_word;
     static_assertions::assert_eq_size!(SourceT, TargetT);
     static_assertions::assert_eq_align!(SourceT, TargetT);
+    // TODO: Consider using
+    // [`as_mut_ptr`](https://doc.rust-lang.org/std/sync/atomic/struct.AtomicU32.html#method.as_mut_ptr)
+    // here once it's stabilized.
     let futex_word: *const TargetT = futex_word as *const SourceT as *const TargetT;
 
-    nix::errno::Errno::result(unsafe {
-        libc::syscall(
-            libc::SYS_futex,
-            futex_word,
-            libc::FUTEX_WAKE,
-            1,
-            core::ptr::null() as *const libc::timespec,
-            core::ptr::null_mut() as *mut u32,
-            0u32,
-        )
-    })?;
-    Ok(())
+    #[cfg(not(miri))]
+    {
+        use linux_syscall::Result64;
+
+        // We use linux_syscall here to avoid the libc dependency, and touching
+        // libc's `errno` in particular.
+        let raw_res = unsafe {
+            linux_syscall::syscall!(
+                linux_syscall::SYS_futex,
+                futex_word,
+                futex_operation,
+                val,
+                core::ptr::null() as *const libc::timespec,
+                core::ptr::null_mut() as *mut u32,
+                0u32,
+            )
+        };
+        raw_res
+            .try_i64()
+            .map_err(|e| nix::errno::Errno::from_i32(e.into()))
+    }
+    // Miri doesn't understands raw syscall assembly, but does understand
+    // futex syscalls made through the libc syscall wrapper.
+    // It's ok to depend on libc for miri testing.
+    #[cfg(miri)]
+    {
+        nix::errno::Errno::result(unsafe {
+            libc::syscall(
+                libc::SYS_futex,
+                futex_word,
+                futex_operation,
+                val,
+                core::ptr::null() as *const libc::timespec,
+                core::ptr::null_mut() as *mut u32,
+                0u32,
+            )
+        })
+    }
 }
-#[cfg(loom)]
+
+pub fn futex_wait(futex_word: &AtomicU32, val: u32) -> nix::Result<i64> {
+    // In "production" we use linux_syscall to avoid going through libc, and to
+    // avoid touching libc's `errno` in particular.
+    #[cfg(not(loom))]
+    {
+        unsafe { futex(futex_word, libc::FUTEX_WAIT, val) }
+    }
+    #[cfg(loom)]
+    {
+        // From futex(2):
+        //   This load, the comparison with the expected value, and starting to
+        //   sleep are performed atomically and totally ordered with
+        //   respect to other futex operations on the same futex word.
+        //
+        // We hold a lock on our FUTEXES to represent this.
+        // TODO: If we want to run loom tests with multiple interacting locks,
+        // we should have per-futex mutexes here, and not hold a lock over the
+        // whole list the whole time.
+        let mut hashmap = FUTEXES.lock().unwrap();
+        let futex_word_val = futex_word.load(Ordering::Relaxed);
+        if futex_word_val != val {
+            return Err(nix::errno::Errno::EAGAIN);
+        }
+        let condvar = hashmap
+            .entry(futex_word as *const _ as usize)
+            .or_insert(Arc::new(Condvar::new()))
+            .clone();
+        // We could get a spurious wakeup here, but that's ok.
+        // Futexes are subject to spurious wakeups too.
+        condvar.wait(hashmap).unwrap();
+        Ok(0)
+    }
+}
+
 pub fn futex_wake(futex_word: &AtomicU32) -> nix::Result<()> {
-    let hashmap = FUTEXES.lock().unwrap();
-    let Some(condvar) = hashmap.get(&(futex_word as *const _ as usize)) else {
-        return Ok(());
-    };
-    condvar.notify_one();
-    Ok(())
+    #[cfg(not(loom))]
+    {
+        unsafe { futex(futex_word, libc::FUTEX_WAKE, 1) }.map(|_| ())
+    }
+    // loom doesn't understand syscalls; emulate via loom primitives.
+    #[cfg(loom)]
+    {
+        let hashmap = FUTEXES.lock().unwrap();
+        let Some(condvar) = hashmap.get(&(futex_word as *const _ as usize)) else {
+            return Ok(());
+        };
+        condvar.notify_one();
+        Ok(())
+    }
 }
 
 #[cfg(not(loom))]
