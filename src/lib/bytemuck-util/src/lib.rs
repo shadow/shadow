028989e744c4e7c271bb3f50866dbfeb20f38b6b
Jim Newsome <jnewsome@torproject.org>
2023-06-06 12:05:36 -0500

    bytemuck-util: add crate-level docs

--- src/lib/bytemuck-util/src/lib.rs
+++ src/lib/bytemuck-util/src/lib.rs
@@ -1,3 +1,12 @@
+//! Utilities related to the bytemuck crate
+//!
+//! This crate provides a temporary reimplementation of the `bytemuck::AnyBitPattern`
+//! crate (formerly `shadow_rs::util::pod::Pod`), which will eventually become an alias
+//! for `bytemuck::AnyBitPattern` or removed altogether.
+//!
+//! It also provides some extra helper functions related to `AnyBitPattern` and
+//! potentially other bytemuck types.
+
 mod anybitpattern;
 
 pub use anybitpattern::{
