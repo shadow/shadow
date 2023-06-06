18df3891dd602463b5c359ff2a61555abe6c85f2
Jim Newsome <jnewsome@torproject.org>
2023-06-06 09:27:06 -0500

    shim_seccomp.h: delete obsolete fn declaration

--- src/lib/shim/shim_seccomp.h
+++ src/lib/shim/shim_seccomp.h
@@ -11,8 +11,4 @@
 // Initialize the seccomp filter and syscall signal handler function.
 void shim_seccomp_init();
 
-// Gets and resets the instruction pointer to which the child should resume
-// execution after a clone syscall.
-ucontext_t* shim_parent_thread_ctx();
-
 #endif // SRC_LIB_SHIM_SHIM_SECCOMP_H_
