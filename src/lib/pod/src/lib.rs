0794016075d1e2c9eb67cbc80f6fca81415d31cf
Jim Newsome <jnewsome@torproject.org>
2023-06-07 12:19:28 -0500

    Make pod no_std

--- src/lib/pod/src/lib.rs
+++ src/lib/pod/src/lib.rs
@@ -1,4 +1,6 @@
-use std::mem::MaybeUninit;
+#![no_std]
+
+use core::mem::MaybeUninit;
 
 /// Marker trait that the given type is Plain Old Data; i.e. that it is safe to
 /// interpret any pattern of bits as a value of this type.
@@ -24,9 +26,9 @@ where
 {
     // SAFETY: Any value and alignment is safe for u8.
     unsafe {
-        std::slice::from_raw_parts(
+        core::slice::from_raw_parts(
             slice.as_ptr() as *const MaybeUninit<u8>,
-            slice.len() * std::mem::size_of::<MaybeUninit<T>>(),
+            slice.len() * core::mem::size_of::<MaybeUninit<T>>(),
         )
     }
 }
@@ -38,7 +40,7 @@ pub fn as_u8_slice<T>(x: &T) -> &[MaybeUninit<u8>]
 where
     T: Pod,
 {
-    to_u8_slice(std::slice::from_ref(x))
+    to_u8_slice(core::slice::from_ref(x))
 }
 
 /// Convert to a mut slice of raw bytes.
@@ -55,9 +57,9 @@ where
 {
     // SAFETY: Any value and alignment is safe for u8.
     unsafe {
-        std::slice::from_raw_parts_mut(
+        core::slice::from_raw_parts_mut(
             slice.as_mut_ptr() as *mut MaybeUninit<u8>,
-            slice.len() * std::mem::size_of::<MaybeUninit<T>>(),
+            slice.len() * core::mem::size_of::<MaybeUninit<T>>(),
         )
     }
 }
@@ -73,7 +75,7 @@ pub unsafe fn as_u8_slice_mut<T>(x: &mut T) -> &mut [MaybeUninit<u8>]
 where
     T: Pod,
 {
-    unsafe { to_u8_slice_mut(std::slice::from_mut(x)) }
+    unsafe { to_u8_slice_mut(core::slice::from_mut(x)) }
 }
 
 /// Create a value of type `T`, with contents initialized to 0s.
@@ -82,7 +84,7 @@ where
     T: Pod,
 {
     // SAFETY: Any value is legal for Pod.
-    unsafe { std::mem::zeroed() }
+    unsafe { core::mem::zeroed() }
 }
 
 // Integer primitives
@@ -103,7 +105,7 @@ unsafe impl Pod for usize {}
 // No! `char` must be a valid unicode value.
 // impl !Pod for char {}
 
-unsafe impl<T> Pod for std::mem::MaybeUninit<T> where T: Pod {}
+unsafe impl<T> Pod for core::mem::MaybeUninit<T> where T: Pod {}
 unsafe impl<T, const N: usize> Pod for [T; N] where T: Pod {}
 
 // libc types
