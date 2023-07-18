fec347302cc0385dbcd8b423acdf6c8f25c84c72
Jim Newsome <jnewsome@torproject.org>
2023-07-17 21:02:06 -0500

    shim do_clone: split out set_context helper function

--- src/lib/shim/src/clone.rs
+++ src/lib/shim/src/clone.rs
@@ -9,6 +9,76 @@ fn sigcontext_offset_of(base: &sigcontext, field: &u64) -> usize {
     field - base
 }
 
+/// Helper for `do_clone`. Restores all general purpose registers, stack pointer,
+/// and instruction pointer from `ctx` except for `rax`, which is set to 0.
+///
+/// # Safety
+///
+/// `ctx` must be safe to restore.
+///
+/// This is difficult to characterize in a general sense, but e.g. minimally the
+/// stack and instruction pointers must be valid, and other register values must
+/// correspond to "sound" values of whatever state they correspond to at that
+/// instruction pointer.
+unsafe extern "C" fn set_context(ctx: &sigcontext) -> ! {
+    // These offsets are hard-coded into the asm format string below.
+    // TODO: turn these into const parameters to the asm block when const
+    // asm parameters are stabilized.
+    // https://github.com/rust-lang/rust/issues/93332
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.r8), 0);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.r9), 0x8);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.r10), 0x10);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.r11), 0x18);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.r12), 0x20);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.r13), 0x28);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.r14), 0x30);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.r15), 0x38);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.rsi), 0x48);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.rdi), 0x40);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.rbx), 0x58);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.rdx), 0x60);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.rbp), 0x50);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.rip), 0x80);
+    debug_assert_eq!(sigcontext_offset_of(ctx, &ctx.rsp), 0x78);
+
+    unsafe {
+        core::arch::asm!(
+            // Restore general purpose registers.
+            // Offsets are validated in assertions above.
+            "mov r8, [rax+0x0]",
+            "mov r9, [rax+0x8]",
+            "mov r10, [rax+0x10]",
+            "mov r11, [rax+0x18]",
+            "mov r12, [rax+0x20]",
+            "mov r13, [rax+0x28]",
+            "mov r14, [rax+0x30]",
+            "mov r15, [rax+0x38]",
+            "mov rsi, [rax+0x48]",
+            "mov rdi, [rax+0x40]",
+            "mov rbx, [rax+0x58]",
+            "mov rdx, [rax+0x60]",
+            "mov rbp, [rax+0x50]",
+            "mov rsp, [rax+0x78]",
+
+            // Push `ctx`'s `rip` to stack
+            "mov rax, [rax+0x80]",
+            "push rax",
+
+            // Not restored:
+            // - `rax`: stores the result of the syscall, which we set below.
+            // - Floating point and other special registers: hopefully not needed.
+
+            // Set `rax` to 0
+            "mov rax, 0",
+
+            // Ret to ctx's `rip`
+            "ret",
+            in("rax") ctx as *const _,
+            options(noreturn)
+        )
+    };
+}
+
 /// Execute a native `clone` syscall. The newly created child thread will
 /// resume execution from `ctx`, which should be the point where the managed
 /// code originally made a `clone` syscall (but was intercepted by seccomp).
@@ -60,26 +130,6 @@ pub unsafe fn do_clone(
     let child_sigctx = unsafe { child_sigcontext.as_mut().unwrap() };
     child_sigctx.rsp = child_stack as u64;
 
-    // These offsets are hard-coded into the asm format string below.
-    // TODO: turn these into const parameters to the asm block when const
-    // asm parameters are stabilized.
-    // https://github.com/rust-lang/rust/issues/93332
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.r8), 0);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.r9), 0x8);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.r10), 0x10);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.r11), 0x18);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.r12), 0x20);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.r13), 0x28);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.r14), 0x30);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.r15), 0x38);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.rsi), 0x48);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.rdi), 0x40);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.rbx), 0x58);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.rdx), 0x60);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.rbp), 0x50);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.rip), 0x80);
-    debug_assert_eq!(sigcontext_offset_of(child_sigctx, &child_sigctx.rsp), 0x78);
-
     let rv: i64;
     // SAFETY:
     // * There's currently no way to tell Rust than an asm block "returns twice",
@@ -93,38 +143,14 @@ pub unsafe fn do_clone(
             // `2`). https://doc.rust-lang.org/rust-by-example/unsafe/asm.html#labels
             "cmp rax, 0",
             "jne 2f",
+
             // Initialize state for this thread
             "call {shim_ensure_init}",
-            // Restore general purpose registers.
-            // Offsets are validated in assertions above.
-            "mov r8, [rsp+0x0]",
-            "mov r9, [rsp+0x8]",
-            "mov r10, [rsp+0x10]",
-            "mov r11, [rsp+0x18]",
-            "mov r12, [rsp+0x20]",
-            "mov r13, [rsp+0x28]",
-            "mov r14, [rsp+0x30]",
-            "mov r15, [rsp+0x38]",
-            "mov rsi, [rsp+0x48]",
-            "mov rdi, [rsp+0x40]",
-            "mov rbx, [rsp+0x58]",
-            "mov rdx, [rsp+0x60]",
-            "mov rbp, [rsp+0x50]",
-            // Store `rip` in `rax`, to be pushed after
-            // restoring `rsp`
-            "mov rax, [rsp+0x80]",
-            // Set rsp last
-            "mov rsp, [rsp+0x78]",
-            // Push original `rip` onto the stack
-            "push rax",
-            // Not restored:
-            // - RAX: stores the result of the syscall, which we set below.
-            // - Floating point and other special registers: hopefully not needed.
 
-            // Set return value of clone syscall
-            "mov rax, 0",
-            // Ret to original RIP
-            "ret",
+            // Set CPU state from ctx
+            "mov rdi, r12",
+            "call {set_context}",
+
             "2:",
             // clone syscall number in, rv out
             inout("rax") libc::SYS_clone => rv,
@@ -139,6 +165,9 @@ pub unsafe fn do_clone(
             // clone syscall arg5
             in("r8") newtls,
             shim_ensure_init = sym crate::bindings::shim_ensure_init,
+            // callee-saved register
+            in("r12") child_sigcontext as * const _,
+            set_context = sym set_context,
         )
     }
     unsafe { crate::bindings::shim_newThreadFinish() };
