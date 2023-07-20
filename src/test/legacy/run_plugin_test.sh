d478941477f335e19e10eb9586e3e477dcc0d38a
Jim Newsome <jnewsome@torproject.org>
2023-07-20 18:10:12 -0500

    run_plugin_test.sh: tell shellcheck to allow word splitting

--- src/test/legacy/run_plugin_test.sh
+++ src/test/legacy/run_plugin_test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-gcc `pkg-config --cflags --libs glib-2.0` -I/home/rob/.shadow/include -shared -Wl,-soname,testplugin.so -fPIC -o testplugin.so shd-test-plugin.c
+# We *want* word splitting
+# shellcheck disable=SC2046
+gcc $(pkg-config --cflags --libs glib-2.0) -I/home/rob/.shadow/include -shared -Wl,-soname,testplugin.so -fPIC -o testplugin.so shd-test-plugin.c
 cp testplugin.so /tmp/testplugin1.so
 cp testplugin.so /tmp/testplugin2.so
 cp testplugin.so /tmp/testplugin3.so
 cp testplugin.so /tmp/testplugin4.so
-gcc `pkg-config --cflags --libs glib-2.0 gthread-2.0 gmodule-2.0` -ldl shd-test-module.c -o shd-test-module
+# We *want* word splitting
+# shellcheck disable=SC2046
+gcc $(pkg-config --cflags --libs glib-2.0 gthread-2.0 gmodule-2.0) -ldl shd-test-module.c -o shd-test-module
 ./shd-test-module
 rm /tmp/testplugin*
