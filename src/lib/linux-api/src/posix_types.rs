b4c0f7dd2b4104d8157053a98b1a3b59f797bdd9
Jim Newsome <jnewsome@torproject.org>
2023-08-10 16:36:36 -0500

    Add linux_api::posix_types::Pid

--- src/lib/linux-api/src/posix_types.rs
+++ src/lib/linux-api/src/posix_types.rs
@@ -1,3 +1,5 @@
+use core::num::NonZeroI32;
+
 use crate::bindings;
 
 pub use bindings::linux___kernel_pid_t;
@@ -15,3 +17,27 @@ pub type kernel_ulong_t = bindings::linux___kernel_ulong_t;
 pub use bindings::linux___kernel_off_t;
 #[allow(non_camel_case_types)]
 pub type kernel_off_t = linux___kernel_off_t;
+
+/// Type-safe wrapper around [`kernel_pid_t`]. Value is strictly positive.
+/// Interface inspired by `rustix::process::Pid`.
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub struct Pid(NonZeroI32);
+
+impl Pid {
+    pub fn from_raw(pid: kernel_pid_t) -> Option<Self> {
+        if pid < 0 {
+            None
+        } else {
+            Some(Self(NonZeroI32::new(pid).unwrap()))
+        }
+    }
+
+    /// Returns a stricly positive integer for `Some`, or 0 for `None`.
+    pub fn as_raw(this: Option<Self>) -> kernel_pid_t {
+        this.map(|x| kernel_pid_t::from(x.0)).unwrap_or(0)
+    }
+
+    pub fn as_raw_nonzero(self) -> NonZeroI32 {
+        self.0
+    }
+}
