3742cec447ea46f9f1f84961da5bb22112978a51
Jim Newsome <jnewsome@torproject.org>
2023-08-10 16:50:41 -0500

    Add linux_api::exit_group

--- src/lib/linux-api/src/exit.rs
+++ src/lib/linux-api/src/exit.rs
@@ -14,3 +14,16 @@ pub fn exit(val: i8) -> ! {
     exit_raw(val.into()).unwrap();
     unreachable!()
 }
+
+/// Exits the process, setting `val & 0xff` as the exit code.
+pub fn exit_group_raw(val: i32) -> Result<(), Errno> {
+    unsafe { linux_syscall::syscall!(linux_syscall::SYS_exit_group, val) }
+        .check()
+        .map_err(Errno::from)
+}
+
+/// Exits the current process, setting `val` as the exit code.
+pub fn exit_group(val: i8) -> ! {
+    exit_group_raw(val.into()).unwrap();
+    unreachable!()
+}
