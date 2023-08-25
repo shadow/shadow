5263d7aca998bc74da0f4425d4613e46db97778e
Rob Jansen <jansen@cs.umn.edu>
2023-08-25 16:48:30 -0400

    Make TGen validation test more progressive

--- src/test/tgen/fixed_duration/verify.sh
+++ src/test/tgen/fixed_duration/verify.sh
@@ -31,23 +31,28 @@ client_Bps_read="$(grep -r --include="tgen.*.stdout" "driver-heartbeat" ./hosts/
 echo "Server bytes per second=${server_Bps_read}"
 echo "Client bytes per second=${client_Bps_read}"
 
-# Test passes if tgen application achieves >= 85% of configured speed, allowing
-# for TCP overhead. We use a 89% threshold since it's the highest that allowed
-# all tgen tests to pass at time of writing. We can increase this in the future
-# as we improve our stack.
-#
-# The 111250 value below comes from:
-#   1Mbit/s = 125000 bytes/s
-#   125000 bytes/s * 0.85 = 106250 bytes/s
+# Test passes if tgen application achieves >= P% of configured speed, allowing for TCP overhead. TCP
+# overhead includes non-payload bytes from packet headers and bytes from retransmitted packets. The
+# latter tends to increase on very slow congested networks due to retransmission, meaning that TCP
+# is less efficient when retransmitting many packets. Thus, our validation test progressively
+# requires higher efficiency on faster networks where we expect fewer retransmissions.
 expected=0
 if [[ "${network}" == "1mbit"* ]]; then
-    expected=106250
+    # 1 Mbit/s = 125000 bytes/s
+    # 125000 bytes/s * 0.84 = 105000 bytes/s
+    expected=105000
 elif [[ "${network}" == "10mbit"* ]]; then
-    expected=1062500
+    # 10 Mbit/s = 1250000 bytes/s
+    # 1250000 bytes/s * 0.88 = 1100000 bytes/s
+    expected=1100000
 elif [[ "${network}" == "100mbit"* ]]; then
-    expected=10625000
+    # 100 Mbit/s = 12500000 bytes/s
+    # 12500000 bytes/s * 0.92 = 11500000 bytes/s
+    expected=11500000
 elif [[ "${network}" == "1gbit"* ]]; then
-    expected=106250000
+    # 1 Gbit/s = 125000000 bytes/s
+    # 125000000 bytes/s * 0.96 = 120000000 bytes/s
+    expected=120000000
 else
     printf "Verification %bfailed%b: unable to determine expected connection speed\n" "$RED" "$NC"
     exit 1
