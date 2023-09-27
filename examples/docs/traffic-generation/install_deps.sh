a89ebed619bdaff818c43738768069a5a1b4f138
Jim Newsome <jnewsome@torproject.org>
2023-09-27 15:37:21 -0500

    extra_tests: install deps for new tor example scripts

--- examples/docs/traffic-generation/install_deps.sh
+++ examples/docs/traffic-generation/install_deps.sh
@@ -2,12 +2,16 @@
 
 apt-get install -y cmake libglib2.0-dev libigraph-dev git
 
-cd /tmp/ || exit
-git clone https://github.com/shadow/tgen.git
-cd tgen || exit
+if type tgen; then
+    echo tgen already installed
+else
+    cd /tmp/ || exit
+    git clone https://github.com/shadow/tgen.git
+    cd tgen || exit
 
-# assumes that `$HOME/.local/bin` is in PATH
-mkdir build && cd build || exit
-cmake .. -DCMAKE_INSTALL_PREFIX="$HOME/.local"
-make
-make install
+    # assumes that `$HOME/.local/bin` is in PATH
+    mkdir build && cd build || exit
+    cmake .. -DCMAKE_INSTALL_PREFIX="$HOME/.local"
+    make
+    make install
+fi
