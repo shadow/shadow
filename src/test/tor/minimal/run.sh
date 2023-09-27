db38f8f188aa19f30879de36e2592ed812956a7a
Jim Newsome <jnewsome@torproject.org>
2023-09-26 21:37:59 -0500

    Update Tor network example
    Adds `obfs4proxy` dependency. Also tweaks to fix how `verify.sh` is run,
    partly fixing https://github.com/shadow/shadow/issues/3158. We should
    still do a more thorough rework though to inline the `run.sh` and
    `verify.sh` scripts in the documenation, and test them
    (https://github.com/shadow/shadow/issues/3153)

--- src/test/tor/minimal/run.sh
+++ src/test/tor/minimal/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Run the Tor minimal test and store output in shadow.log
-shadow --template-directory shadow.data.template tor-minimal.yaml > shadow.log
+shadow --template-directory shadow.data.template --use-memory-manager=false tor-minimal.yaml > shadow.log
