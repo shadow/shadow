df60f95918a4476012e21fd2566a898867ba386a
Steven Engler <opara@cs.georgetown.edu>
2023-12-09 19:56:04 -0500

    Made `linux_dirent` and `linux_dirent64` definitions opaque
    We don't actually use the structs in Shadow, we just pass the pointers on to
    Linux.

--- src/main/host/syscall/kernel_types.h
+++ src/main/host/syscall/kernel_types.h
@@ -35,29 +35,11 @@
 #endif
 
 #if HAVE_STRUCT_LINUX_DIRENT == 0
-struct linux_dirent {
-   unsigned long  d_ino;     /* Inode number */
-   unsigned long  d_off;     /* Offset to next linux_dirent */
-   unsigned short d_reclen;  /* Length of this linux_dirent */
-   char           d_name[];  /* Filename (null-terminated) */
-                     /* length is actually (d_reclen - 2 -
-                        offsetof(struct linux_dirent, d_name)) */
-   /*
-   char           pad;       // Zero padding byte
-   char           d_type;    // File type (only since Linux
-                             // 2.6.4); offset is (d_reclen - 1)
-   */
-};
+struct linux_dirent;
 #endif
 
 #if HAVE_STRUCT_LINUX_DIRENT64 == 0
-struct linux_dirent64 {
-   ino64_t        d_ino;    /* 64-bit inode number */
-   off64_t        d_off;    /* 64-bit offset to next structure */
-   unsigned short d_reclen; /* Size of this dirent */
-   unsigned char  d_type;   /* File type */
-   char           d_name[]; /* Filename (null-terminated) */
-};
+struct linux_dirent64;
 #endif
 
 #endif /* SRC_MAIN_HOST_SYSCALL_KERNEL_TYPES_H_ */
