7e5c7e6d19c21b9680ff7c8ef62486c867536f08
Steven Engler <opara@cs.georgetown.edu>
2024-01-03 17:44:26 -0500

    Changed simple latch `Errno` from linux-api to nix

--- src/lib/scheduler/src/sync/simple_latch.rs
+++ src/lib/scheduler/src/sync/simple_latch.rs
@@ -1,7 +1,7 @@
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Arc;
 
-use linux_api::errno::Errno;
+use nix::errno::Errno;
 
 /// A simple reusable latch. Multiple waiters can wait for the latch to open. After opening the
 /// latch with [`open()`](Self::open), you must not open the latch again until all waiters have
@@ -157,7 +157,7 @@ pub fn libc_futex(
     } else {
         let errno = unsafe { *libc::__errno_location() };
         debug_assert_eq!(rv, -1);
-        Err(Errno::try_from(errno).unwrap())
+        Err(Errno::from_i32(errno))
     }
 }
 
