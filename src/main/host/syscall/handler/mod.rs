shadow/shadow

commit 0412e0af74f59790515a2cc6b7653005a98a4a63

syscall handler mod: fix double-warn race condition (#3338)
Detect when another thread has raced to warn about a missing syscall
number before us, ensuring we really do only warn once about a
particular missing syscall number.

This doesn't seem like a substantial problem in practice, but looking at
this code again it seems silly not to do this slightly better handling.

src/main/host/syscall/handler/mod.rs
@@ -581,6 +581,7 @@ impl SyscallHandler {
                 // only show a warning the first time we encounter this unsupported syscall
                 static WARNED_SET: RwLock<Option<HashSet<SyscallNum>>> = RwLock::new(None);
 
+                // First check with a (cheap) read-lock.
                 let has_already_warned = WARNED_SET
                     .read()
                     .unwrap()
@@ -588,16 +589,15 @@ impl SyscallHandler {
                     .map(|x| x.contains(&syscall))
                     .unwrap_or(false);
 
-                if !has_already_warned {
-                    // `insert()` returns `false` if the syscall num was already in the set.
-                    // This can happen if another thread added the syscall after we released
-                    // the read-lock, above, and took the write lock, below.
-                    WARNED_SET
+                // If it looks like we haven't already warned, add this to the
+                // already-warned set. Also detect the (rare) case that another
+                // thread already warned after we released the read-lock above.
+                let has_already_warned = has_already_warned
+                    || WARNED_SET
                         .write()
                         .unwrap()
                         .get_or_insert_with(HashSet::new)
                         .insert(syscall);
-                }
 
                 let level = if has_already_warned {
                     log::Level::Debug

commit 722f36d84904338bc3cc2f60b029f301a8f90ead

syscall handler: fix inverted "warn once" logic
Fixes a bug introduced in 0412e0af74f59790515a2cc6b7653005a98a4a63.

src/main/host/syscall/handler/mod.rs
@@ -593,7 +593,7 @@ impl SyscallHandler {
                 // already-warned set. Also detect the (rare) case that another
                 // thread already warned after we released the read-lock above.
                 let has_already_warned = has_already_warned
-                    || WARNED_SET
+                    || !WARNED_SET
                         .write()
                         .unwrap()
                         .get_or_insert_with(HashSet::new)