9202e465713ab41cb73f7f1670ef65a1c8c2f8af
Jim Newsome <jnewsome@torproject.org>
2024-07-12 13:46:48 -0500

    Add tests for mkdirat

--- src/test/unistd/test_unistd.rs
+++ src/test/unistd/test_unistd.rs
@@ -56,6 +56,7 @@ fn main() {
     test_getpgrp();
     test_getsid();
     test_mkdir();
+    test_mkdirat();
 }
 
 /// Tests that the results are plausible, but can't really validate that it's our
@@ -190,6 +191,88 @@ fn test_mkdir() {
         }),
         Err(linux_api::errno::Errno::ENOENT)
     );
+}
+
+fn test_mkdirat() {
+    // Create an outer directory first, using AT_FDCWD (equivalent to `mkdir`).
+    let outerdirname = c"./outerdir";
+    {
+        let mode = libc::S_IRWXU;
+        linux_api::errno::Errno::result_from_libc_errno(-1, unsafe {
+            libc::mkdirat(libc::AT_FDCWD, outerdirname.as_ptr(), mode)
+        })
+        .unwrap();
+    }
+
+    let dirfd = linux_api::errno::Errno::result_from_libc_errno(-1, unsafe {
+        libc::open(outerdirname.as_ptr(), libc::O_DIRECTORY)
+    })
+    .unwrap();
+    let basename = c"innerdir";
+    let mode = libc::S_IRWXU;
+    linux_api::errno::Errno::result_from_libc_errno(-1, unsafe {
+        libc::mkdirat(dirfd, basename.as_ptr(), mode)
+    })
+    .unwrap();
+
+    // Validate that the directory exists with the expected properties.
+    let mut stat: libc::stat = unsafe { std::mem::zeroed() };
+    linux_api::errno::Errno::result_from_libc_errno(-1, unsafe {
+        libc::stat(c"./outerdir/innerdir".as_ptr(), &mut stat)
+    })
+    .unwrap();
+    // Validate that it has the expected permissions.
+    assert_eq!(stat.st_mode, libc::S_IFDIR | mode);
+
+    // Trying to create it again should fail.
+    assert_eq!(
+        linux_api::errno::Errno::result_from_libc_errno(-1, unsafe {
+            libc::mkdirat(dirfd, basename.as_ptr(), 0)
+        }),
+        Err(linux_api::errno::Errno::EEXIST)
+    );
+
+    // Clean up.
+    std::fs::remove_dir("./outerdir/innerdir").unwrap();
+    std::fs::remove_dir("./outerdir").unwrap();
+
+    // Creating a directory with an invalid path component should fail.
+    assert_eq!(
+        linux_api::errno::Errno::result_from_libc_errno(-1, unsafe {
+            libc::mkdirat(dirfd, c"nonexistent/foo".as_ptr(), 0)
+        }),
+        Err(linux_api::errno::Errno::ENOENT)
+    );
+
+    linux_api::errno::Errno::result_from_libc_errno(-1, unsafe { libc::close(dirfd) }).unwrap();
+
+    // Creating a directory with an invalid file descriptor should fail.
+    assert_eq!(
+        linux_api::errno::Errno::result_from_libc_errno(-1, unsafe {
+            libc::mkdirat(-1, basename.as_ptr(), 0)
+        }),
+        Err(linux_api::errno::Errno::EBADF)
+    );
+
+    // Using a non-directory fd should fail.
+    let tmpfd = {
+        linux_api::errno::Errno::result_from_libc_errno(-1, unsafe {
+            libc::open(
+                c"regfile".as_ptr(),
+                libc::O_CREAT | libc::O_RDWR,
+                libc::S_IRWXU,
+            )
+        })
+        .unwrap()
+    };
+    assert_eq!(
+        linux_api::errno::Errno::result_from_libc_errno(-1, unsafe {
+            libc::mkdirat(tmpfd, basename.as_ptr(), 0)
+        }),
+        Err(linux_api::errno::Errno::ENOTDIR)
+    );
+    linux_api::errno::Errno::result_from_libc_errno(-1, unsafe { libc::close(tmpfd) }).unwrap();
+    std::fs::remove_file("regfile").unwrap();
 }
 
 fn gethostname_with_short_buffer() -> libc::c_int {
