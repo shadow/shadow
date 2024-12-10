ead8218c16d2f0615e44a31aaa4f4eaf20273699
Rob Jansen <jansen@cs.umn.edu>
2024-12-10 16:21:55 -0500

    Replace C DNS with a Rust impl

--- src/main/host/descriptor/regular_file.c
+++ src/main/host/descriptor/regular_file.c
@@ -296,11 +296,12 @@ int regularfile_openat(RegularFile* file, RegularFile* dir, const char* pathname
         file->type = FILE_TYPE_RANDOM;
     } else if (!strcmp("/etc/hosts", abspath)) {
         file->type = FILE_TYPE_HOSTS;
-        char* hostspath = worker_getHostsFilePath();
+        const char* hostspath = worker_getHostsFilePath();
         if (hostspath && abspath) {
             free(abspath);
-            abspath = hostspath;
+            abspath = strdup(hostspath);
         }
+        worker_freeHostsFilePath(hostspath);
     } else if (!strcmp("/etc/localtime", abspath)) {
         file->type = FILE_TYPE_LOCALTIME;
         if (abspath) {
