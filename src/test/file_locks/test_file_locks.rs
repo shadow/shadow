d79f544a1724dfba4af9a51d841ba3d298b78f80
Jim Newsome <jnewsome@torproject.org>
2026-09-13 10:06:33 -0500

    test_file_locks: loosen requirements on block-duration test
    Fixes #3788

--- src/test/file_locks/test_file_locks.rs
+++ src/test/file_locks/test_file_locks.rs
@@ -1450,9 +1450,12 @@ fn test_block_on_pid_locks(setlk_cmd: FcntlPosixSetlkUncontestedCommand) -> anyh
 
     let dt = t1 - t0;
 
-    // Should have blocked for roughly `sleep_duration`.
-    // Be pretty lenient here for busy machines.
-    ensure_ord!(dt.abs_diff(sleep_duration), <=, std::time::Duration::from_millis(100));
+    // Shouldn't have been able to take the lock until after `sleep_duration`.
+    ensure_ord!(dt, >=, sleep_duration);
+    // Check that we blocked for < 10x `sleep_duration`. This should be large enough to
+    // avoid flakiness, and small enough to catch the most typical sorts of errors that might
+    // crop up in shadow (order-of-magnitude conversion mistakes).
+    ensure_ord!(dt, <, 10*sleep_duration);
 
     Ok(())
 }
