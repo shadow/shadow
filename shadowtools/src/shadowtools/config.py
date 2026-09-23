569e928472e9c05cb1450f7c139914fe59039a09
Jim Newsome <jnewsome@torproject.org>
2026-09-23 15:35:52 -0500

    strace_logging_mode: add 'long' option
    This option was added in 93d3c32cc3201bfaa99366c754c79de63f10de0e

--- shadowtools/src/shadowtools/config.py
+++ shadowtools/src/shadowtools/config.py
@@ -92,7 +92,7 @@ class Experimental(TypedDict, total=False):
     socket_send_autotune: bool
     socket_send_buffer: Union[str, int]
     strace_logging_mode: Union[
-        Literal["off"], Literal["standard"], Literal["deterministic"]
+        Literal["off"], Literal["standard"], Literal["deterministic"], Literal["long"]
     ]
     unblocked_syscall_latency: str
     unblocked_vdso_latency: str
